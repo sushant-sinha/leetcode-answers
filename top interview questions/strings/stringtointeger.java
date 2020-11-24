18c66b5882477665634513923834a14dfaf9085c
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-24 14:36:45 +0530

    new addition

---
+++ top interview questions/strings/stringtointeger.java
@@ -0,0 +1,18 @@
+// SUSHANT SINHA
+class Solution {
+    public int myAtoi(String s) {
+        String arr[]=s.split(" ");
+        if
+        for(String a:arr){
+            if(a==" ")continue;
+            else if(a)
+            {
+                
+            int ans=Integer.parseInt(a);
+                
+            System.out.println(a);}
+            return Integer.parseInt(a);
+        }
+        return 0;
+    }
+}

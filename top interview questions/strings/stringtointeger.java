2fe4dc93edf2565a408d08bda90eb9c54c4a6e17
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-24 22:26:03 +0530

    new addition

--- top interview questions/strings/stringtointeger.java
+++ top interview questions/strings/stringtointeger.java
@@ -1,18 +1,31 @@
 // SUSHANT SINHA
 class Solution {
-    public int myAtoi(String s) {
-        String arr[]=s.split(" ");
-        if
-        for(String a:arr){
-            if(a==" ")continue;
-            else if(a)
-            {
-                
-            int ans=Integer.parseInt(a);
-                
-            System.out.println(a);}
-            return Integer.parseInt(a);
+    public int myAtoi(String str) {
+        if(str == null || str.isEmpty())
+            return 0;
+        
+        int index=0;
+        while(index < str.length() && str.charAt(index) == ' ') {
+            index++;
         }
-        return 0;
+        
+        if(index >= str.length())
+            return 0;
+        
+        int sign = 1;
+        if(str.charAt(index) == '-' || str.charAt(index) == '+') {
+            sign = (str.charAt(index++) == '-') ? -1 : 1;
+        }
+        
+        long output = 0;
+        while(index < str.length() && str.charAt(index) >= 48 && str.charAt(index) <= 57) {
+            output = (output * 10) + (str.charAt(index++) - 48);
+            
+            if((output * sign) >= Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if((output * sign) <= Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int)output * sign;
     }
 }

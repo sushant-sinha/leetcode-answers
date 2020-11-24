615a03f911c409ce22131705906f9d28d6db2497
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-24 22:52:49 +0530

    new addition

---
+++ top interview questions/linked list/deletenodeinalinkedlist.java
@@ -0,0 +1,8 @@
+// SUSHANT SINHA
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        ListNode temp = node.next;
+        node.next = temp.next;
+    }
+}

ca8a19b300e3c1de3f0b2e7bcba9b8ac021d2a55
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-24 15:07:59 +0530

    new addition

---
+++ top interview questions/strings/validanagram.java
@@ -0,0 +1,20 @@
+// SUSHANT SINHA
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] a = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            a[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            a[t.charAt(i) - 'a']--;
+        }
+        for (int i : a) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}

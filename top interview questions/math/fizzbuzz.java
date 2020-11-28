9f914bf76e6c46efc83704d4c5c5f5d05ba4d83c
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-29 00:12:54 +0530

    new addition

--- top interview questions/math/fizzbuzz.java
+++ top interview questions/math/fizzbuzz.java
@@ -2,15 +2,34 @@
 
 //General approach
 
-// class Solution {
-//     public List<String> fizzBuzz(int n) {
-//         ArrayList<String> ans=new ArrayList<String>();
-//         for(int i=1;i<=n;i++){
-//             if(i%3==0 && i%5!=0){ans.add("Fizz"); continue;}
-//             if(i%5==0 && i%3!=0){ans.add("Buzz");continue;}
-//             if(i%3==0 && i%5==0){ans.add("FizzBuzz");continue;}
-//             else ans.add(""+i);
-//         }
-//         return ans;
-//     }
-// }
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> ans=new ArrayList<String>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5!=0){ans.add("Fizz"); continue;}
+            if(i%5==0 && i%3!=0){ans.add("Buzz");continue;}
+            if(i%3==0 && i%5==0){ans.add("FizzBuzz");continue;}
+            else ans.add(""+i);
+        }
+        return ans;
+    }
+}
+
+//using StringBuilder
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> ans=new ArrayList<String>();
+        for(int i=1;i<=n;i++){
+            int x=0;
+            StringBuilder s=new StringBuilder();
+            if(i%3==0) {++x;s.append("Fizz");System.out.println(""+s);}
+            
+            if(i%5==0) {++x;s.append("Buzz");}
+            
+            if(x>0)ans.add(s.toString());
+            else ans.add(""+i);
+        }
+        return ans;
+    }
+}

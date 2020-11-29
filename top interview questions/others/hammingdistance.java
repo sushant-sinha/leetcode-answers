4dd348829cbe59fb636b63f4cb3e07bc45670b22
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-29 14:22:57 +0530

    updated

--- top interview questions/others/hammingdistance.java
+++ top interview questions/others/hammingdistance.java
@@ -1,9 +1,10 @@
 // SUSHANT SINHA
 class Solution {
     public int hammingDistance(int x, int y) {
+       
+    	// if we XOR we will get 1 where there are different bits.
+
         x=x^y;
-        
-        // if we XOR we will get 1 where there are different bits.
         
         //int ans=0;
         // for(int i=0;i<32;i++){

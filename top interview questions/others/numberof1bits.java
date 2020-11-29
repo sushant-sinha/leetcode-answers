c6f6388b17522ab4ef14f89b185a1d609a29fc7f
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-29 13:27:44 +0530

    new addition

---
+++ top interview questions/others/numberof1bits.java
@@ -0,0 +1,22 @@
+// SUSHANT SINHA
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        // METHOD USING BIT OPERATIONS ......... Faster way
+        
+        int ans=0;
+        for(int i=0;i<32;i++){
+            if((n&1)==1)ans++;
+            n>>=1;
+        }
+        return ans;
+        
+        // METHOD USING Integer.bitCOunt(n)
+        
+        // System.out.println(Integer.toBinaryString(n));
+        
+        // return Integer.bitCount(n);
+        
+    }
+}

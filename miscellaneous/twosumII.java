43a9038ae4c378e7f35ae04705e5b29e2b804725
Sushant Sinha <sinhasushant04@gmail.com>
2020-11-30 23:42:05 +0530

    new addition

---
+++ miscellaneous/twosumII.java
@@ -0,0 +1,37 @@
+ // SUSHANT SINHA
+
+ // class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int i=0,j=0;
+//         while(i<(numbers.length-1)){
+//             j=i+1;
+//             while(j<numbers.length){
+//                 if(numbers[i]+numbers[j]==target){
+//                     return new int[]{++i,++j};
+//                 }
+//                 ++j;
+//             }
+//             ++i;
+//         }
+        
+//         return new int[0];
+//     }
+// }
+
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum > target)
+                r--;
+            else if (sum < target)
+                l++;
+            else
+                return new int[] { l + 1, r + 1 };
+        }
+        return new int[0];
+    }
+}

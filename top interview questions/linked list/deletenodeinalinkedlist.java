47efec9fe13081049fafb5696f4455d679dd6126
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-05 15:15:21 +0530

    new addition

--- top interview questions/linked list/deletenodeinalinkedlist.java
+++ top interview questions/linked list/deletenodeinalinkedlist.java
@@ -1,4 +1,21 @@
 // SUSHANT SINHA
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
 class Solution {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;

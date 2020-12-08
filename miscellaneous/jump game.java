1408246ff7d5ad7c71fe356162761d2232c107e7
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-08 19:17:55 +0530

    new addition

--- miscellaneous/jump game.java
+++ miscellaneous/jump game.java
@@ -30,3 +30,23 @@ class Solution {
         return lastPos == 0;
     }
 }
+
+
+/*
+
+My own version
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = lastPos - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
+
+
+*/

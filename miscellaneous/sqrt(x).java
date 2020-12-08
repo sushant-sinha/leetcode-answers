046c1cca17697181c23c7d36088c239a94df86c4
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-09 00:04:21 +0530

    new answer

--- miscellaneous/sqrt(x).java
+++ miscellaneous/sqrt(x).java
@@ -2,6 +2,8 @@
 
 // Boundary case already given to decrease runtime for biggest number
 
+// It was recommended to not use Math.pow(a,b) ....... but used it and never thought of the original answer.
+
 class Solution {
     public int mySqrt(int x) {
         if(x==214783647)return 46340;

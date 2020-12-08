87671ca8ef72d1b6244ebab0ee3d129ff65da0f8
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-08 15:34:50 +0530

    new addition

--- miscellaneous/binary prefix divisible by 5.java
+++ miscellaneous/binary prefix divisible by 5.java
@@ -1,2 +1,21 @@
 // SUSHANT SINHA
 
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] A) {
+        List<Boolean> ans = new ArrayList<>();
+        /*
+        	same as
+			ArrayList<Boolean> ans = new ArrayList<Boolean>();	
+
+        */
+        int temp = 0;
+        for (int x:A) {
+            temp = 2 * temp + x;
+            temp %= 5;
+            ans.add(temp % 5 == 0);
+        }
+
+        return ans;
+    }
+    
+}    

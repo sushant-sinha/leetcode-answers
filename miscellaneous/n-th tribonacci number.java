bd251e7baabfe08410e2def423d6126bcf6784cb
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-09 16:19:54 +0530

    new addition

---
+++ miscellaneous/n-th tribonacci number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int tribonacci(int n) {
+        
+        int [] ans=new int[4];
+        ans[0]=0;
+        ans[1]=1;
+        ans[2]=1;
+        ans[3]=2;
+        
+        if(n<4)return ans[n];
+        
+        for(int i=4;i<=n;i++){
+            
+            ans[0]=ans[3]+ans[2]+ans[1];
+            
+            ans[1]=ans[2];
+            ans[2]=ans[3];
+            ans[3]=ans[0];
+        }
+        return ans[0];
+    }
+}

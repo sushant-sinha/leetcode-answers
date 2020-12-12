0e98c287b42124dc4699c7de819e0f8f5793d3df
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-13 00:53:57 +0530

    new addition

--- miscellaneous/transpose matrix.java
+++ miscellaneous/transpose matrix.java
@@ -3,6 +3,9 @@
 class Solution {
     public int[][] transpose(int[][] A) {
         int row = A.length, column = A[0].length;
+
+        // could replace coulmn and row .... to decrease space
+
         int[][] ans = new int[column][row];
         for (int r = 0; r < row; ++r){
             for (int c = 0; c < column; ++c) {

28797486a06793a23242093180315081a247094e
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-13 00:52:09 +0530

    new addition

---
+++ miscellaneous/transpose matrix.java
@@ -0,0 +1,14 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int row = A.length, column = A[0].length;
+        int[][] ans = new int[column][row];
+        for (int r = 0; r < row; ++r){
+            for (int c = 0; c < column; ++c) {
+                ans[c][r] = A[r][c];
+            }
+        }
+        return ans;
+    }
+}

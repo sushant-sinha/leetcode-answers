c50a37c5bd2c2726ee8ac0d52778ef331c21b921
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-13 00:35:41 +0530

    new addition

--- miscellaneous/smallest subtree with all the deepest nodes.java
+++ miscellaneous/smallest subtree with all the deepest nodes.java
@@ -1,5 +1,20 @@
 // SUSHANT SINHA
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
         return dfs(root).node;

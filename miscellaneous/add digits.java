66b25cc18df704a6ead691ecf88b65ab2e4d4b82
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-12 15:02:40 +0530

    new addition

--- miscellaneous/add digits.java
+++ miscellaneous/add digits.java
@@ -30,3 +30,23 @@ class Solution {
     }
 }
 
+// fastest asnwer
+
+class Solution {
+    public int addDigits(int num) {
+        int s=0;
+        while(num!=0)
+        {
+            int r=num%10;
+            s=s+r;
+            //System.out.println(s);
+            num=num/10;
+            if(num==0 && s>=10)
+            {
+                num=s;
+                s=0;
+            }
+        }
+        return s;
+    }
+}

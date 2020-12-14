61c869656ee2525d715016859e7b8e0146385e48
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-14 08:00:36 +0530

    new addition

---
+++ miscellaneous/guess number higher or lower.java
@@ -0,0 +1,9 @@
+// SUSHANT SINHA
+
+
+/*
+    Given a set of range, if we divide the number sequence into two halves.
+    If the number is in range of the first half, we discard the rest half.
+    Repeat it untill we get three numbers x-1 (x) x+1 .... here the mid is the answer and thus returned.
+
+*/

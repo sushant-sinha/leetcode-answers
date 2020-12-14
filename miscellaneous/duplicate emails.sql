7a5a82e9e5a899becd26de6ec329b4198f0e160a
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-14 07:45:59 +0530

    new addition

--- miscellaneous/duplicate emails.sql
+++ miscellaneous/duplicate emails.sql
@@ -3,3 +3,7 @@
 select distinct P1.Email
 from Person as P1, Person as P2
 where P1.Id != P2.Id and P1.Email = P2.Email
+
+/* or can use this too */
+
+select email from person group by email having count(id)>1;

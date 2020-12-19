9e5180fd6083808972ebd8c0b5e618b0d4214a78
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-19 14:08:40 +0530

    new addition

--- miscellaneous/factorial trailing zeroes.java
+++ miscellaneous/factorial trailing zeroes.java
@@ -1,7 +1,27 @@
 // SUSHANT SINHA
 
+// Least space
+
 class Solution {
     public int trailingZeroes(int n) {
         return n == 0 ? 0 : n/5 + trailingZeroes(n/5);
+    }
+}
+
+// Fastest
+
+class Solution {
+    // Time Complexity: O(log5(n)) because we are using base 5.
+    // Space Complexity: O(1)
+
+    public int trailingZeroes(int n) {
+        int count = 0;
+        
+        while (n > 4) {
+            n /= 5;
+            count += n;
+        }
+        
+        return count;
     }
 }

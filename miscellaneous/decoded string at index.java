c553d5f7d3ab33e9a0bfda31e9664b707df4433e
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-20 16:25:47 +0530

    new addition

--- miscellaneous/decoded string at index.java
+++ miscellaneous/decoded string at index.java
@@ -1,5 +1,37 @@
 // SUSHANT SINHA
 
+// Given in LeetCode solution 
+
+class Solution {
+    public String decodeAtIndex(String S, int K) {
+        long size = 0;
+        int N = S.length();
+
+        // Find size = length of decoded string
+        for (int i = 0; i < N; ++i) {
+            char c = S.charAt(i);
+            if (Character.isDigit(c))
+                size *= c - '0';
+            else
+                size++;
+        }
+
+        for (int i = N-1; i >= 0; --i) {
+            char c = S.charAt(i);
+            K %= size;
+            if (K == 0 && Character.isLetter(c))
+                return Character.toString(c);
+
+            if (Character.isDigit(c))
+                size /= c - '0';
+            else
+                size--;
+        }
+
+        throw null;
+    }
+}
+
 // took from https://github.com/gouthampradhan/leetcode/blob/master/problems/src/math/DecodedStringAtIndex.java
 
 class Solution {

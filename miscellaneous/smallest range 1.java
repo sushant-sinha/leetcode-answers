3e415894676039b5008b81126dd43ef282bbfe4d
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-21 17:35:06 +0530

    new addition

---
+++ miscellaneous/smallest range 1.java
@@ -0,0 +1,15 @@
+// SUSHANT SINHA
+
+// Fast Solution
+
+class Solution {
+    public int smallestRangeI(int[] A, int K) {
+        // int min = A[0], max = A[0];
+        // for (int x: A) {
+        //     min = Math.min(min, x);
+        //     max = Math.max(max, x);
+        // }
+        Arrays.sort(A);
+        return Math.max(0, A[A.length-1] - A[0] - 2*K);
+    }
+}

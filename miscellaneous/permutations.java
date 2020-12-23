acbbb476e6bf11c331f2541708f2bc9a70e10e3b
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-23 15:42:11 +0530

    new addition

--- miscellaneous/permutations.java
+++ miscellaneous/permutations.java
@@ -1,5 +1,39 @@
 // SUSHANT SINHA
 
+// fastest solution
+
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(nums , new int[nums.length] , ans , 1);
+        return ans;
+    }
+    
+    public void generate(int a[] , int visited[] , List<List<Integer>> ans , int count){
+        if(count == a.length+1){
+            List<Integer> now = new ArrayList<>();
+            for(int i=0;i<a.length;i++)
+                now.add(0);
+            for(int i=0;i<a.length;i++){
+                now.set(visited[i]-1 , a[i]);
+            }
+            ans.add(now);
+            return;
+        }
+        
+        for(int i=0;i<a.length;i++){
+            if(visited[i] != 0)
+                continue;
+            visited[i] = count;
+            generate(a , visited , ans , count+1);
+            visited[i] = 0;
+        }
+        
+    }
+    
+}
+
 public class Solution {
     
     public List<List<Integer>> permute(int[] nums) {
@@ -71,34 +105,3 @@ public class Solution {
 }
 
 
-
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        generate(nums , new int[nums.length] , ans , 1);
-        return ans;
-    }
-    
-    public void generate(int a[] , int visited[] , List<List<Integer>> ans , int count){
-        if(count == a.length+1){
-            List<Integer> now = new ArrayList<>();
-            for(int i=0;i<a.length;i++)
-                now.add(0);
-            for(int i=0;i<a.length;i++){
-                now.set(visited[i]-1 , a[i]);
-            }
-            ans.add(now);
-            return;
-        }
-        
-        for(int i=0;i<a.length;i++){
-            if(visited[i] != 0)
-                continue;
-            visited[i] = count;
-            generate(a , visited , ans , count+1);
-            visited[i] = 0;
-        }
-        
-    }
-    
-}

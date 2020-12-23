fe0a5c232ae1cf5b2cd2509811c7be2d005bdcaf
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-23 15:41:46 +0530

    new addition

--- miscellaneous/permutations.java
+++ miscellaneous/permutations.java
@@ -71,3 +71,34 @@ public class Solution {
 }
 
 
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        generate(nums , new int[nums.length] , ans , 1);
+        return ans;
+    }
+    
+    public void generate(int a[] , int visited[] , List<List<Integer>> ans , int count){
+        if(count == a.length+1){
+            List<Integer> now = new ArrayList<>();
+            for(int i=0;i<a.length;i++)
+                now.add(0);
+            for(int i=0;i<a.length;i++){
+                now.set(visited[i]-1 , a[i]);
+            }
+            ans.add(now);
+            return;
+        }
+        
+        for(int i=0;i<a.length;i++){
+            if(visited[i] != 0)
+                continue;
+            visited[i] = count;
+            generate(a , visited , ans , count+1);
+            visited[i] = 0;
+        }
+        
+    }
+    
+}

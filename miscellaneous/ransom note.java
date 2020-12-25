be4095efd8de3a827adaac92c08dfefc9ad9d07c
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-25 12:52:57 +0530

    new addition

--- miscellaneous/ransom note.java
+++ miscellaneous/ransom note.java
@@ -33,3 +33,45 @@ class Solution {
     }
 }
 
+// Fastest solution
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int alphaCount[] = new int[26];
+        for (char c : ransomNote.toCharArray()) {
+            int index = magazine.indexOf(c, alphaCount[c - 'a']);
+            if(index == -1) {
+                return false;
+            }
+            alphaCount[c - 'a'] = index+1;
+        }
+        return true;
+    }
+}
+
+// Shortest solution
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        if(ransomNote.length() > magazine.length()) return false;
+        if(ransomNote == null || magazine == null) return false;
+        
+        int[] temp = new int[26];
+        
+        for(int i = 0; i < magazine.length(); i++) {
+            temp[magazine.charAt(i) - 'a']++;
+        }
+        
+        for(int j = 0; j < ransomNote.length(); j++) {
+            
+            if(temp[ransomNote.charAt(j) - 'a'] == 0) {
+                return false;
+            } else {
+                temp[ransomNote.charAt(j) - 'a']--;
+            }
+        }
+        
+        return true;
+    }
+}

c0bf695ff800a5f028e701e78e68dbc5e2d9a9f3
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-25 13:21:42 +0530

    new addition

--- miscellaneous/number complement.java
+++ miscellaneous/number complement.java
@@ -1,5 +1,13 @@
 // SUSHANT SINHA
 
+/*
+
+An efficient approach to this problem is as follows:
+1. Find the number of bits in the given integer
+2. XOR the given integer with 2^number_of_bits-1
+
+*/
+
 class Solution {
     public int findComplement(int n) {
          int number_of_bits = (int)(Math.floor(Math.log(n)/Math.log(2))) + 1; 

4aed7f00bd2d2dd32ca307e9b8bd34b566b6d96c
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-28 14:35:11 +0530

    new addition

--- miscellaneous/Maximum 69 Number.java
+++ miscellaneous/Maximum 69 Number.java
@@ -1,5 +1,7 @@
 // SUSHANT SINHA
 
+// My solution 6ms 36.2mb
+
 class Solution {
     public int maximum69Number (int num) {
         
@@ -18,6 +20,32 @@ class Solution {
         System.out.println(sb);
         
         return Integer.valueOf(sb.toString());
+        
+    }
+}
+
+// Almost similar but fast 0ms
+
+class Solution {
+    public int maximum69Number (int num) {
+        
+        String s = Integer.toString(num);
+        char ch[] = s.toCharArray();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(ch[i] == '6')
+            {
+                ch[i] = '9';
+                break;
+            }
+        }
+        
+        String k = new String(ch);
+        
+        int ans = Integer.parseInt(k);
+        
+        return ans;
         
     }
 }

7fec691a7856bfcea7fda52583bd17d1fec2cf8c
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-29 17:32:31 +0530

    new addition

--- miscellaneous/1365. How Many Numbers Are Smaller Than the Current Number.java
+++ miscellaneous/1365. How Many Numbers Are Smaller Than the Current Number.java
@@ -16,3 +16,37 @@ class Solution {
         
     }
 }
+
+// fastest solution
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        /*      
+        nums = [8,1,2,2,3]
+                
+                0 1 2 3 4 5 6 7 8 9 ...101
+        temp = [0 1 2 1 0 0 0 0 1........ ]
+        ->     [0 1 ]
+        */
+        int[] temp = new int[101];
+        for(int i = 0; i<nums.length; i++){
+            temp[nums[i]]++;
+        }
+        
+        for(int i =1; i<101; i++){
+            temp[i] = temp[i]+temp[i-1];
+        }
+        
+        for(int i = 0; i<nums.length; i++){
+            int index = nums[i];
+            if(index==0){
+                nums[i] =0;
+            }else{
+                nums[i] = temp[index-1];
+            }
+           
+        }
+        
+        return nums;
+    }
+}

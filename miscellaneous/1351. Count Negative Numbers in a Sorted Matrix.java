96132ee294b178041ffb9da2cac422be87e1bd80
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-29 17:16:45 +0530

    new addition

--- miscellaneous/1351. Count Negative Numbers in a Sorted Matrix.java
+++ miscellaneous/1351. Count Negative Numbers in a Sorted Matrix.java
@@ -14,3 +14,21 @@ class Solution {
         return ans;
     }
 }
+
+// best solution
+
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int cnt=0;
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]<0){
+                    cnt+=grid[i].length-j;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+}

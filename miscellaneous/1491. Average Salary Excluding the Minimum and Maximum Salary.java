880201c9b73c6504e92938ec331c382bc0b93932
Sushant Sinha <sinhasushant04@gmail.com>
2020-12-31 21:23:04 +0530

    new addition

---
+++ miscellaneous/1491. Average Salary Excluding the Minimum and Maximum Salary.java
@@ -0,0 +1,12 @@
+// SUSHANT SINHA
+
+
+
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        double ans=0;
+        for(int i=1;i<salary.length-1;i++)ans+=salary[i];
+        return ans/(salary.length-2);
+    }
+}

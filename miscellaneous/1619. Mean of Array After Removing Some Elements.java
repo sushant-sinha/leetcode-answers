62b587f4920165e2c1ebf4018b4fe6ce3aa86d4c
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-03 00:04:09 +0530

    new addition

--- miscellaneous/1619. Mean of Array After Removing Some Elements.java
+++ miscellaneous/1619. Mean of Array After Removing Some Elements.java
@@ -1,7 +1,5 @@
 // SUSHANT SINHA
 
-
-
 // 2 ms(98.99 %)
 
 class Solution {
@@ -20,4 +18,39 @@ class Solution {
         
         return (double)sum/len;
     }
+}
+
+// based on...it had extra arrays...
+
+class Solution {
+public double trimMean(int[] arr) {
+
+	//sorting array
+    Arrays.sort(arr);
+	
+	//dividing array's length by 20
+	//since it's a given multiple of 20
+    int tots = arr.length / 20;
+	
+	//creating a new array to store 
+	//all values between tots 
+    int [] something = new int[arr.length-(tots*2)];
+    int j = 0;
+	
+	//transferring values from arr array to something array
+    for(int i = tots; i < arr.length-tots;i++) {
+        something[j] = arr[i];
+        j++;
+    }
+	
+	//creating another for loop to sum up the array
+    int sum = 0;
+    for(int i = 0; i < something.length;i++) {
+        sum += something[i];
+    }
+    
+	//typecasting the mean to create a double result
+    double mean = (double)sum/(double)something.length;
+    return mean;
+}
 }

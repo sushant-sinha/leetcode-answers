cc1568341b07bb1b13146a4f48a3c7be25d49248
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-02 08:48:06 +0530

    new addition

--- miscellaneous/1640. Check Array Formation Through Concatenation.java
+++ miscellaneous/1640. Check Array Formation Through Concatenation.java
@@ -1,5 +1,7 @@
 // SUSHANT SINHA
 
+// 0ms 38.5mb( 36.68%)
+
 class Solution {
     public boolean canFormArray(int[] arr, int[][] pieces) {
         for (int[] piece : pieces) {
@@ -30,3 +32,26 @@ class Solution {
             return -1;
         }
     }
+
+    // 1 ms 38.4mb( 52.57% )
+
+    class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        StringBuilder sb = new StringBuilder();
+        for(int x : arr){
+            sb.append(x);
+            sb.append("#");
+        }
+        for(int i = 0; i < pieces.length; i++){
+            StringBuilder res = new StringBuilder();
+            for(int j = 0; j < pieces[i].length; j++){
+                res.append(String.valueOf(pieces[i][j]));
+                res.append("#");
+            }
+            if(!sb.toString().contains(res.toString())){
+                return false;
+            }
+        }
+        return true;
+    }
+}

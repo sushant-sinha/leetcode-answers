10a9013d9d8b1c67021e7dd4c3092544d13fc353
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-04 09:56:34 +0530

    new addition

---
+++ miscellaneous/1523. Count Odd Numbers in an Interval Range.java
@@ -0,0 +1,12 @@
+// SUSHANT SINHA
+
+// 0 ms (100%)
+
+class Solution {
+    public int countOdds(int low, int high) {
+        
+        if((low%2==0) && (high%2==0))return (high-low)/2;
+        else return  ((high-low)/2+1);
+        
+    }
+}

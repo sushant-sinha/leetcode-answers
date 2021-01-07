25b18076994f73692e152256c0ff7993bc3a49f5
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-07 17:32:03 +0530

    new addition

--- miscellaneous/628. Maximum Product of Three Numbers.java
+++ miscellaneous/628. Maximum Product of Three Numbers.java
@@ -1,6 +1,26 @@
 // SUSHANT SINHA
 
-// fatest solution 1ms
+// my solution 15 ms
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int temp;
+        if( nums[0] < 0 && nums[1] < 0 && nums[nums.length-1] > 0){
+            
+            temp= nums[0]*nums[1]*nums[nums.length-1];
+            if(temp > nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3])
+                return temp;
+            else return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
+        }
+        
+        return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
+    }
+}
+
+// fastest solution 1 ms
 
 class Solution {
     public int maximumProduct(int[] nums) {
@@ -54,25 +74,5 @@ class Solution {
 
         }
         return Math.max(max1*max2*max3 , min1*min2*max3);
-    }
-}
-
-// my solution 15ms
-
-class Solution {
-    public int maximumProduct(int[] nums) {
-        
-        Arrays.sort(nums);
-        
-        int temp;
-        if( nums[0] < 0 && nums[1] < 0 && nums[nums.length-1] > 0){
-            
-            temp= nums[0]*nums[1]*nums[nums.length-1];
-            if(temp > nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3])
-                return temp;
-            else return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
-        }
-        
-        return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
     }
 }

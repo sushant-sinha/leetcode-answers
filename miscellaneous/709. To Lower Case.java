6b9e9c20a3b893e2b9d35cd0adbcd19172a80b61
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-08 10:12:06 +0530

    new addition

---
+++ miscellaneous/709. To Lower Case.java
@@ -0,0 +1,20 @@
+// SUSHANT SINHA
+
+// original answer
+
+class Solution {
+    public String toLowerCase(String str) {
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if ((int) c <= 90 && (int) c >= 65) {
+                result.append((char)(c+32));
+            } 
+            else {
+            result.append(c);
+            }
+        }
+        
+        return result.toString();
+
+    }
+}

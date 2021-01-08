d45e1c27188f8331042629cb6996e769bd1d064d
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-08 09:58:20 +0530

    new additon

--- miscellaneous/704. Binary Search.java
+++ miscellaneous/704. Binary Search.java
@@ -29,3 +29,24 @@ class Solution {
     }
 }
 
+// less space consuming
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high) {
+            int mid = (low+high)/2;
+            
+            if(nums[mid] == target) return mid;
+            
+            if(nums[mid] < target) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}

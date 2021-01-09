4be2a3d28953e7f4a16bdb25419d605c2d99ffd1
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-09 16:47:33 +0530

    new addition

--- miscellaneous/127. Word Ladder.java
+++ miscellaneous/127. Word Ladder.java
@@ -1,8 +1,54 @@
 // SUSHANT SINHA
 
-// to from fishercoder1534 (hard category)
+// took from fishercoder1534 (hard category) { TRY }
 
-// 20 ms solution
+// 88 ms
+
+class Solution {
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> beginSet = new HashSet<>();
+        Set<String> endSet = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        Set<String> dict = new HashSet<>(wordList);
+        int len = 1;
+
+        beginSet.add(beginWord);
+
+        if (dict.contains(endWord)) {
+            endSet.add(endWord);
+        }
+
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            Set<String> nextBeginSet = new HashSet<>();
+            for (String word : beginSet) {
+                char[] chars = word.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chars[i];
+                        chars[i] = c;
+                        String newWord = new String(chars);
+                        if (endSet.contains(newWord)) {
+                            return len + 1;
+                        }
+
+                        if (!visited.contains(newWord) && dict.contains(newWord)) {
+                            visited.add(newWord);
+                            nextBeginSet.add(newWord);
+                        }
+                        chars[i] = old;
+                    }
+                }
+            }
+
+            beginSet = nextBeginSet;
+            len++;
+        }
+        return 0;
+    }
+}
+
+
+// 20 ms solution (better solution)
 
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
@@ -52,49 +98,3 @@ class Solution {
         return 0;
     }
 }
-
-// 88 ms
-
-class Solution {
-
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Set<String> beginSet = new HashSet<>();
-            Set<String> endSet = new HashSet<>();
-            Set<String> visited = new HashSet<>();
-            Set<String> dict = new HashSet<>(wordList);
-            int len = 1;
-
-            beginSet.add(beginWord);
-
-            if (dict.contains(endWord)) {
-                endSet.add(endWord);
-            }
-
-            while (!beginSet.isEmpty() && !endSet.isEmpty()) {
-                Set<String> nextBeginSet = new HashSet<>();
-                for (String word : beginSet) {
-                    char[] chars = word.toCharArray();
-                    for (int i = 0; i < chars.length; i++) {
-                        for (char c = 'a'; c <= 'z'; c++) {
-                            char old = chars[i];
-                            chars[i] = c;
-                            String newWord = new String(chars);
-                            if (endSet.contains(newWord)) {
-                                return len + 1;
-                            }
-
-                            if (!visited.contains(newWord) && dict.contains(newWord)) {
-                                visited.add(newWord);
-                                nextBeginSet.add(newWord);
-                            }
-                            chars[i] = old;
-                        }
-                    }
-                }
-
-                beginSet = nextBeginSet;
-                len++;
-            }
-            return 0;
-        }
-    }

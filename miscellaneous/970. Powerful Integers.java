7407326d866a94af1d6d376600815fd2712ac387
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-16 00:03:13 +0530

    new addition

---
+++ miscellaneous/970. Powerful Integers.java
@@ -0,0 +1,50 @@
+// SUSHANT SINHA
+
+
+
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        if(bound==0 || (x==1 && y==1 && bound==1))return ans;
+        if(x==1 && y==1){
+            ans.add(2);
+            return ans;
+        }
+        
+        if(y==1){
+            
+            for(int i=0;Math.pow(x,i)+1<=(double)bound;i++){
+                ans.add( (int)Math.pow(x,i) + 1);
+            }
+            return ans;
+            
+        }
+        if(x==1){
+            
+            for(int i=0;Math.pow(y,i)+1<=(double)bound;i++){
+                ans.add( (int)Math.pow(y,i) + 1 );
+            }
+            return ans;
+            
+        }
+        
+        for(int i=0;Math.pow(x,i)<=(double)bound;i++){
+            
+            for(int j=0;Math.pow(y,j)+Math.pow(x,i)<=(double)bound;j++){
+                
+                if(!ans.contains( (int)Math.pow(x,i) + (int)Math.pow(y,j) )){
+                    
+                    ans.add( (int)Math.pow(x,i) + (int)Math.pow(y,j) );
+                    
+                }
+                
+            }
+            
+            
+        }
+        
+        return ans;
+        
+    }
+}

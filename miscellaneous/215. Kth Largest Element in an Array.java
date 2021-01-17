6f400cffcfcea18596c78aa2aafd30246b504747
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-17 07:55:12 +0530

    new addition

---
+++ miscellaneous/215. Kth Largest Element in an Array.java
@@ -0,0 +1,13 @@
+// SUSHANT SINHA
+
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        Arrays.sort(nums);
+        
+        return nums[nums.length-k];
+        
+    }
+}

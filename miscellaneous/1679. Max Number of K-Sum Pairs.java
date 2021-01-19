9e1a685d79d48074f2942ef05e642224ae33ab30
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-19 11:20:22 +0530

    new addition

--- miscellaneous/1679. Max Number of K-Sum Pairs.java
+++ miscellaneous/1679. Max Number of K-Sum Pairs.java
@@ -22,3 +22,30 @@ class Solution {
         return ops;
     }
 }
+
+// 30ms (personal favourite)
+
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        int count = 0;
+        
+        int i = 0;
+        int j = nums.length - 1; 
+        
+        while(i<j) {
+            if(nums[i] + nums[j] > k) {
+                j--;
+            } else if(nums[i] + nums[j] < k) {
+                i++;
+            } else {
+                count++;
+                i++;
+                j--;
+            }  
+        }
+        
+        return count;
+    }
+}

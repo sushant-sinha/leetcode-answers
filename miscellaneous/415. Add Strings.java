ec6cf7911a9048d6ba8c6cd6460aa5cf26f2d960
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-24 17:16:59 +0530

    new addition

--- miscellaneous/415. Add Strings.java
+++ miscellaneous/415. Add Strings.java
@@ -90,3 +90,30 @@ class Solution {
     }
 }
 
+
+
+// best solution 2ms
+
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int a = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int b = j >= 0 ? num2.charAt(j--) - '0' : 0;
+            int sum = a + b + carry;
+            int alg = sum % 10;
+            carry = sum >= 10 ? 1 : 0;
+            res.append(alg);
+        }
+
+        if (carry == 1) {
+            res.append(carry);
+        }
+        
+        return res.reverse().toString();
+    }
+}

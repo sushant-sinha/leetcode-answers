cac6b6f7604abeb08c21c810635e078bcc643b83
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-25 20:19:49 +0530

    new addition

---
+++ miscellaneous/1437. Check If All 1's Are at Least Length K Places Away.java
@@ -0,0 +1,35 @@
+// SUSHANT SINHA
+
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        
+        for(int i=0;i<nums.length;i++){
+            
+            // reset the counter
+            int counter=0;
+            
+            
+            // check the boundary condition whether 1 is the last element or not
+            if(nums[i]==1 && (i+1)!=nums.length){
+                ++i;
+                while(i<nums.length && nums[i]==0){
+                    counter++;
+                    i++;
+                }
+                --i;
+                
+                // check the counter....and also whether the while loop ended because of end of array
+                if(counter<k && i+1!=nums.length){
+                    return false;
+                    }
+                else continue;
+                
+            }
+            
+        }
+        
+        return true;
+        
+    }
+}

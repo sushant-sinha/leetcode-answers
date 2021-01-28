e450b3319793458f5584a96bcd14146885518d86
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-28 13:51:50 +0530

    new addition

--- miscellaneous/507. Perfect Number.java
+++ miscellaneous/507. Perfect Number.java
@@ -15,3 +15,19 @@ class Solution {
         return sum == num;
     }
 }
+
+// 2ms 37.7mb
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num<2)
+            return false;
+        int sum = 1;
+        for(int i=2;i<=(int)Math.sqrt(num);i++){
+            if(num%i==0)
+                sum = sum + i + num/i;
+        }
+        return sum==num;
+    }
+
+}

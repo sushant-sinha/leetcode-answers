dadaa74a321ecb8442ddc3756852fce0e353b114
Sushant Sinha <sinhasushant04@gmail.com>
2021-01-29 15:14:31 +0530

    new addition

--- miscellaneous/551. Student Attendance Record I.java
+++ miscellaneous/551. Student Attendance Record I.java
@@ -26,3 +26,32 @@ class Solution {
         return true;
     }
 }
+
+// 18ms 41.1mb
+
+class Solution {
+    public boolean checkRecord(String s) {
+        
+        if(s.length()==1 || s.length()==0)return true;
+        
+        String split[]=s.split("");
+        int a=0;
+        
+        if(split[split.length-1].equals("A"))a++;
+        if(split[split.length-2].equals("A"))a++;
+        if(a>1)return false;
+        
+        for(int i=0;i<split.length-2;i++){
+        	if(split[i].equals("A"))a++;
+        	if(a>1)return false;
+        	if(split[i].equals("L") && split[i+1].equals("L") && split[i+2].equals("L")){
+                System.out.print(i+" "+" "+(i+1)+" "+(i+2));
+                return false;
+            }
+
+        }
+
+        return true;
+        
+    }
+}   

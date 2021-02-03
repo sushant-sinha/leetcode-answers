4f6107b2c945d195f04562ceeb261ef40eb0ae9b
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-04 00:00:53 +0530

    new addition

---
+++ miscellaneous/693. Binary Number with Alternating Bits.java
@@ -0,0 +1,27 @@
+// SUSHANT SINHA
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        
+        String bin=Integer.toBinaryString(n);
+        char ar[]=bin.toCharArray();
+        if( ar[0]=='0'){
+            for(int i=0;i<ar.length;i++){
+                if(i%2==0 && ar[i]=='0')continue;
+                if(i%2!=0 && ar[i]=='1')continue;
+                else return false;
+            }
+        }
+        
+        if( ar[0]=='1'){
+            for(int i=0;i<ar.length;i++){
+                if(i%2==0 && ar[i]=='1')continue;
+                if(i%2!=0 && ar[i]=='0')continue;
+                else return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}

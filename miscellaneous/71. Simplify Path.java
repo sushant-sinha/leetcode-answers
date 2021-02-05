0822d47e99bc9a2177a38c3470f82b072207f664
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-05 14:20:15 +0530

    new addition

--- miscellaneous/71. Simplify Path.java
+++ miscellaneous/71. Simplify Path.java
@@ -1,15 +1,17 @@
 // SUSHANT SINHA
 
+// 5ms ( 57.36% ) 38.9mb ( 80.53% )
+
 class Solution {
     public String simplifyPath(String path) {
         String[] dirs = path.split("/");
-        Deque<String> q = new ArrayDeque<>();
+        Deque<String> ans = new ArrayDeque<>();
         for(String s: dirs) {
             if(s.equals("")) continue;
             if(s.equals(".")) continue;
-            if(s.equals("..")) q.pollLast();
-            else q.add(s);
+            if(s.equals("..")) ans.pollLast();
+            else ans.add(s);
         }
-        return "/" + String.join("/", q);
+        return "/" + String.join("/", ans);
     }
 }

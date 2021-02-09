50c706cd5b3e492c0c53d7979f19798cfb10cadc
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-09 18:09:26 +0530

    new addition

--- miscellaneous/804. Unique Morse Code Words.java
+++ miscellaneous/804. Unique Morse Code Words.java
@@ -110,3 +110,20 @@ class Solution {
     }
 }
 
+// best solution
+// 1ms ( 100% ) 36.9mb ( 83.03% )
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> set = new HashSet<>();
+        for(String word:words){
+            StringBuilder sb = new StringBuilder();
+            for(char c: word.toCharArray()){
+                sb.append(morse[c-'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}

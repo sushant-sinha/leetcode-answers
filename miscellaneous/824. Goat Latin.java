9bbfd3a4cef51c230d2518843193544d69162b38
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-12 19:02:01 +0530

    new addition

--- miscellaneous/824. Goat Latin.java
+++ miscellaneous/824. Goat Latin.java
@@ -43,3 +43,43 @@ class Solution {
         
     }
 }
+
+// best solution 
+
+// 1ms ( 99.66% ) 37.7mb ( 66.14% )
+
+class Solution {
+    public String toGoatLatin(String S) {
+        
+        String[] arr = S.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < arr.length; i++){
+            if(res.length() > 0)
+                res.append(" ");
+            res.append(modifyString(arr[i], i));
+        }
+        
+        return res.toString();
+    }
+    
+    String modifyString(String s, int index){
+        
+        StringBuilder sb = new StringBuilder();
+        if(s.charAt(0) != 'a' && s.charAt(0) != 'e' && s.charAt(0) != 'i'
+            && s.charAt(0) != 'o' && s.charAt(0) != 'u' &&
+            s.charAt(0) != 'A' && s.charAt(0) != 'E' && s.charAt(0) != 'I'
+            && s.charAt(0) != 'O' && s.charAt(0) != 'U'){
+            
+            char ch = s.charAt(0);
+            sb.append(s.substring(1)).append(ch);
+        }else
+            sb.append(s);
+        
+        sb.append("ma");
+        
+        for(int i = 0; i <= index; i++)
+            sb.append('a');
+        
+        return sb.toString();
+    }
+}

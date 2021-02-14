2e9cc32f513c0145b2b3d909c55815100aede02a
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-14 20:17:30 +0530

    new addition

--- miscellaneous/844. Backspace String Compare.java
+++ miscellaneous/844. Backspace String Compare.java
@@ -52,3 +52,93 @@ class Solution {
     }
     
 }
+
+// best solution given
+// 0ms 37.4mb ( 41.74% )
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length()-1;
+        int j = T.length()-1;
+        char c1;
+        char c2;
+        int count;
+        while(i!=-1 && j!=-1){
+            c1 = S.charAt(i);
+            if(c1=='#'){
+                count = 1;
+                i--;
+                while(count!=0 || S.charAt(i) =='#'){
+                    c1 = S.charAt(i);
+                    if(c1 == '#'){
+                        count++;
+                    }
+                    else{
+                        count--;
+                    }
+                    i--;
+                    if(i==-1){
+                        break;
+                    }
+                }
+            }
+            c2 = T.charAt(j);
+            if(c2=='#'){
+                count = 1;
+                j--;
+                while(count!=0 || T.charAt(j) =='#'){
+                    c2 = T.charAt(j);
+                    if(c2 == '#'){
+                        count++;
+                    }
+                    else{
+                        count--;
+                    }
+                    j--;
+                    if(j==-1){
+                       break;
+                    }
+                }
+            }
+            
+            if(i==-1 || j==-1){
+                break;
+            }
+            c1 = S.charAt(i);
+            c2 = T.charAt(j);
+            
+            
+            if(c1 != c2){
+                return false;
+            }
+            i--;
+            j--;
+        }
+        count = 0;
+        while(i!=-1 && (count!=0 || S.charAt(i) =='#') ){
+            c1 = S.charAt(i);
+            if(c1 == '#'){
+                count++;
+            }
+            else{
+                count--;
+            }
+            i--;
+        }
+        
+        count = 0;
+        while(j!=-1 && (count!=0 || T.charAt(j) =='#')){
+            c2 = T.charAt(j);
+            if(c2 == '#'){
+                count++;
+            }
+            else{
+                count--;
+            }
+            j--;
+        }
+        
+        if(i==-1 && j==-1) return true;
+        return false;
+    }
+}

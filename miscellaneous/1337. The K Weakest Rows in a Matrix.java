4aa06b70f14315eb86e8f55a3a8ab6f2350d5175
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-15 20:20:56 +0530

    new addition

--- miscellaneous/1337. The K Weakest Rows in a Matrix.java
+++ miscellaneous/1337. The K Weakest Rows in a Matrix.java
@@ -69,3 +69,34 @@ class Solution {
         return mat[0];
     }
 }
+
+// best solution given
+// 0ms 40.2mb ( 48.25% )
+
+class Solution {
+    public int sumRow(int[] arr){
+        int s = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==1){
+            s += arr[i];
+            }else
+                break;
+        }
+        
+        return s;
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int[] fa = new int[mat.length];
+        int[] res = new int[k];
+        for(int i=0; i<mat.length; i++){
+            int sum = sumRow(mat[i])*1000 + i;
+            fa[i] = sum;
+        }
+        Arrays.sort(fa);
+        
+        for(int i=0; i<k; i++){
+            res[i] = fa[i]%1000;
+        }
+        return res;
+    }   
+}

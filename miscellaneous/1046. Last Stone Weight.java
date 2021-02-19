c4ea8c7b90f35d6cd46ffdbe30600dfc2a7915f0
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-19 19:07:42 +0530

    new addition

---
+++ miscellaneous/1046. Last Stone Weight.java
@@ -0,0 +1,35 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        ArrayList<Integer> ar=new ArrayList<>();
+        
+        for(int x:stones)
+            ar.add(x);
+        
+        while(ar.size()>1){
+            
+            Collections.sort(ar);
+            
+            int max=ar.get(ar.size()-1);
+            int max1=ar.get(ar.size()-2);
+            
+            if(max>max1){
+                ar.remove(ar.size()-1);
+                ar.remove(ar.size()-1);
+                ar.add(max-max1);
+            }
+            if(max==max1){
+                ar.remove(ar.size()-1);
+                ar.remove(ar.size()-1);
+            }
+            
+        }
+        
+        if(ar.size()!=0)return ar.get(0);
+        
+        return 0;
+        
+    }
+}

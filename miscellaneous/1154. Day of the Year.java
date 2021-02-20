4a7bf9c76218c812d8d2d7fcdcc1f9e1aa06c9bc
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-20 22:42:23 +0530

    new addition

--- miscellaneous/1154. Day of the Year.java
+++ miscellaneous/1154. Day of the Year.java
@@ -1,5 +1,8 @@
 // SUSHANT SINHA
 
+// took reference 
+// 1ms ( 96.76% ) 36.8mb ( 98.23% )
+
 class Solution {
     boolean isLeapYear(int num){
         return num%400==0 ? true : num%4==0 && num%100!=0;
@@ -14,5 +17,40 @@ class Solution {
         int month=Integer.parseInt(date.substring(5,7))-1;
         int ans=Integer.parseInt(date.substring(8,10))+(month-1<0 ? 0 : dp[month-1]);
         return ans;  
+    }
+}
+
+class Solution {
+    public int dayOfYear(String date) {
+        String[] temp = date.split("-");
+        int[] normal = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        int[] leap = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+        int sum = 0;
+        Boolean isLeap = false;
+        if (Integer.parseInt(temp[0]) % 100 == 0) {
+            if (Integer.parseInt(temp[0]) % 400 == 0) {
+                isLeap = true;
+            }
+        }
+        else if (Integer.parseInt(temp[0]) % 4 == 0) {
+            isLeap = true;
+        }
+        if (isLeap) {
+            int month = Integer.parseInt(temp[1]);
+            int day = Integer.parseInt(temp[2]);
+            sum += day;
+            for (int i = 0; i < month; i++) {
+                sum += leap[i];
+            }
+        }
+        else {
+            int month = Integer.parseInt(temp[1]);
+            int day = Integer.parseInt(temp[2]);
+            sum += day;
+            for (int i = 0; i < month; i++) {
+                sum += normal[i];
+            }
+        }
+        return sum;
     }
 }

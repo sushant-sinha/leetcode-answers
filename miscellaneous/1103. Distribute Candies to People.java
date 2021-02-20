38729c7df0b28f5a7a0e95855f0d98573739a7e1
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-20 22:21:39 +0530

    new addition

---
+++ miscellaneous/1103. Distribute Candies to People.java
@@ -0,0 +1,32 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        
+        int ans[]=new int[num_people];
+        
+        int i=0,cur=1;
+        
+        while(candies>=0){
+            
+            if(i==num_people)i=0;
+            
+            if(candies-cur<=0){
+                ans[i++]+=candies;
+                candies-=cur;
+                cur++;
+            }
+            else{
+                
+                ans[i++]+=cur;
+                candies-=cur;
+                cur++;
+                
+            }                
+            
+        }
+        
+        return ans;
+        
+    }
+}

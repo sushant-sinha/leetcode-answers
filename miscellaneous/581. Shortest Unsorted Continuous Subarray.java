08b30bac9dc66aa118da25d69ac1f398b3f359c4
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-25 14:01:33 +0530

    new addition

---
+++ miscellaneous/581. Shortest Unsorted Continuous Subarray.java
@@ -0,0 +1,33 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        
+        if(nums.length==1)return 0;
+        
+        int start=0,end=0;
+        int sorted[]=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++)
+            sorted[i]=nums[i];
+        
+        Arrays.sort(sorted);
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=sorted[i]){
+                start=i;
+                break;
+            }
+        }
+        
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]!=sorted[i]){
+                end=i;
+                break;
+            }
+        }
+        if(end==start)return 0;
+        return end-start+1;
+        
+    }
+}

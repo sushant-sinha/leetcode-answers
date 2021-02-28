bb2c732430e13bd3bdf73e9eb1560986375f6f6e
Sushant Sinha <sinhasushant04@gmail.com>
2021-02-28 08:19:29 +0530

    new addition

---
+++ miscellaneous/1252. Cells with Odd Values in a Matrix.java
@@ -0,0 +1,31 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int oddCells(int n, int m, int[][] indices) {
+        
+        int mat[][]=new int[n][m];
+        
+        for(int i=0;i<indices.length;i++){
+            
+            int row=indices[i][0];
+            int column=indices[i][1];
+            
+            for(int j=0;j<m;j++)
+                mat[row][j]++;
+            
+            for(int j=0;j<n;j++)
+                mat[j][column]++;
+            
+        }
+        
+        int ans=0;
+        
+        for(int x[]:mat)
+            for(int y:x)
+                if(y%2!=0)
+                    ans++;
+        
+        return ans;
+        
+    }
+}

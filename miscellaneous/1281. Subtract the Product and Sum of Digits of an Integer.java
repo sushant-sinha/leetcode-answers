bc4e43312958ec967972e6c0d611e5ce1cce2810
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-01 09:28:17 +0530

    new addition

--- miscellaneous/1281. Subtract the Product and Sum of Digits of an Integer.java
+++ miscellaneous/1281. Subtract the Product and Sum of Digits of an Integer.java
@@ -16,3 +16,17 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        while(n > 0) {
+            int val = n % 10;
+            product *= val;
+            sum += val;
+            n = n / 10;
+        }
+        return product - sum;
+    }
+}

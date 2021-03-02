8a3acf0623f8ee91bb9a403281acb5abcbb0c8fc
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-02 22:14:21 +0530

    new addition

--- miscellaneous/1266. Minimum Time Visiting All Points.java
+++ miscellaneous/1266. Minimum Time Visiting All Points.java
@@ -13,3 +13,29 @@ class Solution {
         return time;
     }
 }
+
+// 1ms ( 38.65% ) 38.3mb ( 90.93% )
+
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int time = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            int[] point1 = points[i];
+            int[] point2 = points[i + 1];
+            int x1 = point1[0], y1 = point1[1];
+            int x2 = point2[0], y2 = point2[1];
+            int hSteps = x2 - x1;
+            int vSteps = y2 - y1;
+
+            int diagonal = Math.min(Math.abs(vSteps), Math.abs(hSteps)); // diagonal travel
+            time += diagonal;
+            x1 += Math.signum(hSteps) * diagonal;
+            y1 += Math.signum(vSteps) * diagonal;
+
+            // after travelling from point1 diagonally, travel remainder to point 2 vertically or horizontally
+            int remainingLinear = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+            time += remainingLinear;
+        }
+        return time;
+    }
+}

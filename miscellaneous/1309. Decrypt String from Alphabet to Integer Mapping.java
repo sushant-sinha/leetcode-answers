cbdbfecba5eabc81030dce5bfee7ca9e5c508f5a
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-04 10:47:41 +0530

    new addition

---
+++ miscellaneous/1309. Decrypt String from Alphabet to Integer Mapping.java
@@ -0,0 +1,34 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String freqAlphabets(String s) {
+        
+        String ans="";
+        
+        for(int i=s.length()-1; i>=0 ;i--){
+            if(s.charAt(i)=='#'){
+                
+                int x=Integer.parseInt(s.substring(i-2,i))+96;
+                char temp=(char)x;
+                ans+=Character.valueOf(temp);
+                i-=2;
+                
+            }
+            else{
+                
+                int x=Integer.parseInt(s.substring(i,i+1))+96;
+                char temp=(char)x;
+                ans+=Character.valueOf(temp);
+                
+            }
+        }
+        
+        String print="";
+        
+        for(int i=ans.length()-1;i>=0;--i)
+            print+=ans.substring(i,i+1);
+        
+        return print;
+        
+    }
+}

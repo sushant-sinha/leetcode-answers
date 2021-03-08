f171ff91585ee485c643700c8b4090b392958df6
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-08 17:32:18 +0530

    new addition

---
+++ miscellaneous/1408. String Matching in an Array.java
@@ -0,0 +1,39 @@
+// SUSHANT SINHA
+
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        
+        List<String> ans=new ArrayList<>();
+        
+        for(int i=0;i<words.length-1;i++){
+            
+            for(int j=i+1;j<words.length;j++){
+                
+                String small=words[i].length()>=words[j].length() ? words[j] : words[i];
+                String big=words[i].length()<words[j].length() ? words[j] : words[i];
+                                
+                if(issub(small,big)){
+                    if(!ans.contains(small))
+                    ans.add(small);
+                    
+                }
+                
+            }
+            
+            
+        }
+        
+        return ans;
+        
+    }
+    
+    public boolean issub(String small, String big){
+        
+        for(int i=0;i<big.length()-small.length()+1;i++){
+            if(small.equals(big.substring(i,i+small.length())))
+                return true;
+        }
+        
+        return false;
+    }
+}

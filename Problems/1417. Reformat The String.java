97119956ff74b385c9001e10a99e2e2ac0d8c144
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-12 22:10:45 +0530

    new addition

---
+++ Problems/1417. Reformat The String.java
@@ -0,0 +1,54 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String reformat(String s) {
+        
+        ArrayList<String> num=new ArrayList<>();
+        ArrayList<String> str=new ArrayList<>();
+        
+        String ans="";
+        
+        for(char x:s.toCharArray()){
+            
+            if(Character.isDigit(x))
+                num.add(""+x);
+            else
+                str.add(""+x);
+            
+        }
+        
+        if(num.size()-str.size()==1){
+            
+            for(int i=0;i<str.size();i++){
+                ans+=num.get(i);
+                ans+=str.get(i);
+            }
+            ans+=num.get(num.size()-1);
+            
+        }
+        
+        else if(str.size()-num.size()==1){
+            
+            for(int i=0;i<num.size();i++){
+                ans+=str.get(i);
+                ans+=num.get(i);
+            }
+            ans+=str.get(str.size()-1);
+            
+        }
+        
+        else if(num.size()==str.size()){
+            
+            for(int i=0;i<num.size();i++){
+                ans+=str.get(i);
+                ans+=num.get(i);
+            }
+            
+        }
+        
+        else return "";
+        
+        return ans;
+        
+    }
+}

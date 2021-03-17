8a546afe16106f22475e831a27de7881abcb179a
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-17 13:25:51 +0530

    new addition

--- Problems/1460. Make Two Arrays Equal by Reversing Sub-arrays.java
+++ Problems/1460. Make Two Arrays Equal by Reversing Sub-arrays.java
@@ -17,3 +17,21 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int test[] = new int[1001];
+        
+        for(int i=0;i<target.length;i++){
+            test[target[i]]++;
+            test[arr[i]]--;
+        }
+        
+        for(int i=0;i<test.length;i++){
+            if(test[i]!=0)
+                return false;
+        }
+        
+        return true;
+    }
+}

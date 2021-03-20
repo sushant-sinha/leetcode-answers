3acb0a2ed115377e6e142e0e2d874d56e0f95096
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-20 09:49:53 +0530

    new addition

--- Problems/1456. Maximum Number of Vowels in a Substring of Given Length.java
+++ Problems/1456. Maximum Number of Vowels in a Substring of Given Length.java
@@ -39,3 +39,25 @@ class Solution {
     }
 }
 
+
+// solutions given in the discuss tab
+// 8ms ( 95.49% ) 39.5mb ( 73.25% )
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        int vowel = 0;
+        for(int i =0; i< k; i++)
+            if(isVowel(s.charAt(i))) vowel++;
+        int max = vowel;
+        for(int i = k; i < s.length(); i++){
+            if(isVowel(s.charAt(i))) vowel++;
+            if(isVowel(s.charAt(i-k))) vowel--;
+            max = Math.max(max, vowel);
+        }
+        return max;
+    }
+    private boolean isVowel(char c){
+        if((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) return true;
+        return false;
+    }
+}

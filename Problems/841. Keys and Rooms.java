469b76c7a02ed6ea5124dc6fe4a01e0fa320553e
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-20 11:37:42 +0530

    new addition

---
+++ Problems/841. Keys and Rooms.java
@@ -0,0 +1,30 @@
+// SUSHANT SINHA
+
+// tired using ArrayList instead of stack
+
+// given solution
+// 1ms ( 85.85% ) 38.7mb ( 85.76% )
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] seen = new boolean[rooms.size()];
+        seen[0] = true;
+        Stack<Integer> stack = new Stack();
+        stack.push(0);
+
+        //At the beginning, we have a todo list "stack" of keys to use.
+        //'seen' represents at some point we have entered this room.
+        while (!stack.isEmpty()) { // While we have keys...
+            int node = stack.pop(); // Get the next key 'node'
+            for (int nei: rooms.get(node)) // For every key in room # 'node'...
+                if (!seen[nei]) { // ...that hasn't been used yet
+                    seen[nei] = true; // mark that we've entered the room
+                    stack.push(nei); // add the key to the todo list
+                }
+        }
+
+        for (boolean v: seen)  // if any room hasn't been visited, return false
+            if (!v) return false;
+        return true;
+    }
+}

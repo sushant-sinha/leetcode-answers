c75a13e3c601f21439fca08f7455569b173d6875
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-20 11:38:47 +0530

    new addition

--- Problems/841. Keys and Rooms.java
+++ Problems/841. Keys and Rooms.java
@@ -28,3 +28,31 @@ class Solution {
         return true;
     }
 }
+
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visitedRooms = new boolean[rooms.size()];
+        moveBetweenRooms(rooms, 0, visitedRooms);
+        
+        for(boolean isRoomVisited: visitedRooms) {
+            if(!isRoomVisited) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private void moveBetweenRooms(List<List<Integer>> rooms, int curRoom, boolean[] visitedRooms) {
+        
+        if(visitedRooms[curRoom]) {
+            return;
+        }
+        visitedRooms[curRoom] = true;
+        List<Integer> accessibleRooms = rooms.get(curRoom);
+        for(int room: accessibleRooms) {
+            moveBetweenRooms(rooms, room, visitedRooms);
+        }
+    }
+}

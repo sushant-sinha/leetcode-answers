dffffa18f83ecd1ab82bbac111400f0b4fcd6a33
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-23 00:05:19 +0530

    new addition

--- Problems/1475. Final Prices With a Special Discount in a Shop.java
+++ Problems/1475. Final Prices With a Special Discount in a Shop.java
@@ -21,3 +21,23 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        for(int i=0;i<prices.length-1;i++){
+            int temp=0;
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j] <= prices[i]){
+                    temp=prices[j];
+                    break;
+                }
+            }
+            
+            prices[i]-=temp;
+        }
+        
+        return prices;
+        
+    }
+}

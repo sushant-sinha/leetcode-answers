58e846bf75ce70ffa14b1bc05ee3c5f4a0d7639b
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-23 00:05:08 +0530

    new addition

---
+++ Problems/1475. Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,23 @@
+// SUSHANT SINHA
+
+// 1ms ( 97.36% ) 39.2mb ( 60.06% )
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        
+        for(int i=0;i<prices.length-1;i++){
+            int temp=0;
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[j] <= prices[i]){
+                    temp=prices[j];
+                    break;
+                }
+            }
+            
+            prices[i]-=temp;
+        }
+        
+        return prices;
+        
+    }
+}
--- Problems/1493. Longest Subarray of 1's After Deleting One Element.java
+++ Problems/1493. Longest Subarray of 1's After Deleting One Element.java
@@ -1,26 +1,5 @@
 // SUSHANT SINHA
 
-// 1ms ( 99.49% ) 47.9mb ( 60.67% )
-
-class Solution {
-    public int longestSubarray(int[] A) {
-        int i = 0, j, k = 1, res = 0;
-        for (j = 0; j < A.length; ++j) {
-            if (A[j] == 0) {
-                k--;
-            }
-            while (k < 0) {
-                if (A[i] == 0) {
-                    k++;
-                }
-                i++;
-            }
-            res = Math.max(res, j - i);
-        }
-        return res;
-    } 
-}
-
 // My solution
 // 492ms ( 5.40% ) 47.6mb ( 89.97% )
 
@@ -47,4 +26,26 @@ class Solution {
         return len;
         
     }
+}
+
+// better solution ( solution tab )
+// 1ms ( 99.49% ) 47.9mb ( 60.67% )
+
+class Solution {
+    public int longestSubarray(int[] A) {
+        int i = 0, j, k = 1, res = 0;
+        for (j = 0; j < A.length; ++j) {
+            if (A[j] == 0) {
+                k--;
+            }
+            while (k < 0) {
+                if (A[i] == 0) {
+                    k++;
+                }
+                i++;
+            }
+            res = Math.max(res, j - i);
+        }
+        return res;
+    } 
 }

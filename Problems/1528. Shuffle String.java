d00b9f760b55cee8d41dfbf3a36165256d0e494a
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-26 11:52:12 +0530

    new addition

--- Problems/1528. Shuffle String.java
+++ Problems/1528. Shuffle String.java
@@ -23,3 +23,19 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        
+        char ans[]= new char[indices.length];
+        
+        for( int i=0; i< indices.length; i++){
+            
+            ans[indices[i]]=s.charAt(i);
+            
+        }
+        
+        return String.valueOf(ans);
+        
+    }
+}

7d285f56cb06034097ab54cb61a0f4ce8986f536
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-27 11:25:14 +0530

    new addition

---
+++ Problems/1544. Make The String Great.java
@@ -0,0 +1,28 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String makeGood(String s) {
+        
+        while(true){
+            
+            boolean ck=true;
+                        
+            for(int i=0;i<s.length()-1;i++){
+                
+                if(('A'-s.charAt(i))==('a'-s.charAt(i+1)) || ('a'-s.charAt(i))==('A'-s.charAt(i+1))){
+                    
+                    
+                    s=s.substring(0,i)+s.substring(i+2,s.length());
+                    ck=false;
+                    break;
+                }
+            }
+            
+            if(ck)break;
+            
+        }
+        
+        return s;
+        
+    }
+}

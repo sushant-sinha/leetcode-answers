cef30ae252f610e480a9dc2b73b4fb92d614135b
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-30 10:34:38 +0530

    new addition

---
+++ Problems/1539. Kth Missing Positive Number.java
@@ -0,0 +1,26 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        
+        int ans[]=new int[k];
+        
+        int index=0;
+        int val=0;
+        int arind=0;
+        
+        while(index!=k){
+            
+            while(arind<arr.length && val+1==arr[arind]){
+                val++;
+                arind++;
+            }
+            
+            ans[index++]=++val;
+            
+        }
+        
+        return ans[--k];        
+        
+    }
+}

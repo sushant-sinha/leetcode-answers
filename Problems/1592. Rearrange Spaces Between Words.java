04c6acea99459efc0d1443d5551cb2c5c38e022f
Sushant Sinha <sinhasushant04@gmail.com>
2021-03-31 11:28:27 +0530

    new addition

---
+++ Problems/1592. Rearrange Spaces Between Words.java
@@ -0,0 +1,50 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String reorderSpaces(String text) {
+        
+        int space=0;
+        for(char x:text.toCharArray())
+            if(x==' ')
+                ++space;
+        
+        
+        String[] words = text.trim().split("\\s+");
+        
+        String ans="";
+        int ar=0;
+        
+        if(words.length==1){
+            
+            ans+=words[0];
+            
+            while(ans.length()!=text.length())
+                ans+=" ";
+            
+            return ans;
+            
+        }
+        
+        int val=(space/(words.length-1));
+        int remainder = space%(words.length-1);
+        
+        for(int i=0;i<space+words.length-remainder;i++){
+            
+            ans+=words[ar++];
+            if(i+1==space+words.length-remainder)
+                break;
+            for(int j=0;j<val;j++)
+                ans+=" ";
+            i+=val;
+            
+        }
+        
+        
+        
+        while(ans.length()!=text.length())
+            ans+=" ";
+        
+        return ans;
+        
+    }
+}

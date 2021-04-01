af86c1fc5f1fde4d6c3b20dce62faede6c62e535
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-01 11:14:06 +0530

    new addition

---
+++ Problems/1588. Sum of All Odd Length Subarrays.java
@@ -0,0 +1,24 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        
+        int ans=0;
+        
+        for(int x:arr)
+            ans+=x;
+        
+        for(int i=0;i<arr.length-2;i++){
+            
+            for(int j=i+2;j<arr.length;j+=2){
+                
+                for(int k=i;k<=j;k++)
+                    ans+=arr[k];
+                
+            }
+        }
+        
+        return ans;
+     
+    }
+}

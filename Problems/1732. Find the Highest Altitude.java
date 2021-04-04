8a320e2d3f1d9641c5386fe9d14b2c4242e1a103
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-04 11:08:24 +0530

    new addition

---
+++ Problems/1732. Find the Highest Altitude.java
@@ -0,0 +1,21 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+               
+        for(int i=1;i<gain.length;i++){
+            
+            gain[i]+=gain[i-1];
+            
+        }
+        
+        int ans=0;
+        
+        for(int x:gain){
+            ans = ans>x ? ans : x;
+        }
+        
+        return ans;
+        
+    }
+}

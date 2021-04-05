488e30ecb6732488411ddd087245a663f4cbbf25
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-06 00:12:00 +0530

    new addition

--- Problems/1752. Check if Array Is Sorted and Rotated.java
+++ Problems/1752. Check if Array Is Sorted and Rotated.java
@@ -45,3 +45,45 @@ class Solution {
     }
     
 }
+
+// using .cclone might give better memory usage, better upto 36mb ( 99.45% )
+
+class Solution {
+    public boolean check(int[] nums) {
+                
+        int sorted[]=nums.clone();
+        Arrays.sort(sorted);
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int first=nums[0];
+            
+            for(int j=0;j<nums.length-1;j++){
+                
+                nums[j]=nums[j+1];
+                
+            }
+            
+            nums[nums.length-1]=first;
+            
+            if(check(sorted,nums))
+                return true;
+            
+        }
+        
+        return false;
+        
+    }
+    
+    boolean check(int[] sort, int[] ip){
+        
+        for(int i=0;i<sort.length;i++){
+            if(sort[i]!=ip[i])
+                return false;
+        }
+        
+        return true;
+        
+    }
+    
+}

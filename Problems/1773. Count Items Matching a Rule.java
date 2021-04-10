f9063c697bc73d26d8fbd19baf5e9839c5c4eecf
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-10 12:37:06 +0530

    new addition

---
+++ Problems/1773. Count Items Matching a Rule.java
@@ -0,0 +1,40 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        
+        
+        if(ruleKey.equals("type")){
+            
+            return check(items,0,ruleValue);
+            
+        }
+        else if(ruleKey.equals("color")){
+            
+            return check(items,1,ruleValue);
+            
+        }
+        else{
+            
+            return check(items,2,ruleValue);
+            
+        }
+        
+        
+    }
+    
+    int check(List<List<String>> items, int k, String ruleValue){
+        
+        int ans=0;
+        
+        for(int i=0;i<items.size();i++){
+            
+            if(items.get(i).get(k).equals(ruleValue))
+                ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

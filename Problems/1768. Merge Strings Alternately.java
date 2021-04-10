356d978f08f0d03f5ad3d08160f0af5003436819
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-11 00:08:58 +0530

    new addition

--- Problems/1768. Merge Strings Alternately.java
+++ Problems/1768. Merge Strings Alternately.java
@@ -42,3 +42,46 @@ class Solution {
         
     }
 }
+
+// using String
+// 9ms ( 5.70% ) 40.5mb ( 5.09% )
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        
+        String ans="";
+        
+        int i=0,j=0;
+        
+        String w1[]=word1.split("");
+        String w2[]=word2.split("");
+        
+        while(true){
+            if(i==w1.length || j==w2.length )
+                break;
+            ans+=w1[i++];
+            ans+=w2[j++];
+            
+        }
+        
+        if(i==word1.length() && j!=word2.length()){
+            
+            while(j!=w2.length)
+                ans+=w2[j++];
+            return ans;
+            
+        }
+        
+        if(i!=word1.length() && j==word2.length()){
+            
+            while(i!=w1.length)
+                ans+=w1[i++];
+            return ans;
+            
+        }
+        
+        else
+            return ans;
+        
+    }
+}

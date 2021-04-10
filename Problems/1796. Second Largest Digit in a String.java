811b3a65ce58f27e7779d46d4756d0cf6d7f420d
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-10 12:49:47 +0530

    new addition

---
+++ Problems/1796. Second Largest Digit in a String.java
@@ -0,0 +1,33 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int secondHighest(String s) {
+        
+        // can use boolean array to check existence of 0-9 number
+        // boolean exist[]=new boolean[11];
+        
+        ArrayList<Integer> exist = new ArrayList<>();
+        
+        for(char x:s.toCharArray()){
+            
+            if(Character.isDigit(x)){
+                if(!exist.contains(Character.getNumericValue(x))){
+                    exist.add(Character.getNumericValue(x));
+                }
+            }
+            
+        }
+        
+        Collections.sort(exist);
+        
+        try{
+            return exist.get(exist.size()-2);
+        }
+        catch(Exception e){
+            return -1;
+        }
+        
+        
+        
+    }
+}

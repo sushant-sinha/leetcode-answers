700c4b3adcdffb7ff4170e84e230fe35d8be4308
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-12 08:04:52 +0530

    new addition

---
+++ Problems/1678. Goal Parser Interpretation.java
@@ -0,0 +1,30 @@
+// SUSHANT SINHA
+
+// 1ms ( 59.15% ) 37.5mb ( 32.22% )
+
+class Solution {
+    public String interpret(String command) {
+        
+        String ans="";
+        
+        for(int i=0;i<command.length();i++){
+            
+            if(command.charAt(i)=='G')
+                ans+="G";
+            
+            else if(i<command.length()-1 && command.substring(i,i+2).equals("()")){
+                ans+="o";
+                i++;
+            }
+            else{
+                ans+="al";
+                i+=3;
+            }
+            
+            
+        }
+        
+        return ans;
+        
+    }
+}

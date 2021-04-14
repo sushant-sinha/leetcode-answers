311e4e048be7a60a5f4ec46e9316f6d6aee43474
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-14 07:59:49 +0530

    new addition

---
+++ Problems/205. Isomorphic Strings.java
@@ -0,0 +1,27 @@
+// SUSHANT SINHA
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char[] sCharList = s.toCharArray();
+        char[] tCharList = t.toCharArray();
+        
+        char[] transRelation = new char[128];
+        boolean[] targetTrans = new boolean[128];
+        
+        for (int i = 0, len = sCharList.length; i < len; i++) {
+            char sChar = sCharList[i];
+            char willChar = transRelation[sChar];
+            if (willChar != 0 && willChar != tCharList[i]) {
+                return false;
+            }
+            if (transRelation[sChar] == 0) {
+                if (targetTrans[tCharList[i]]) {
+                    return false;
+                }
+                transRelation[sChar ] = tCharList[i];
+                targetTrans[tCharList[i]] = true;
+            }
+        }
+        return true;
+    }
+}

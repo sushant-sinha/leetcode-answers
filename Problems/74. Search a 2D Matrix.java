416a7a5213b013b67e3abcae2b4198d27be45a5b
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-15 10:23:24 +0530

    new addition

--- Problems/74. Search a 2D Matrix.java
+++ Problems/74. Search a 2D Matrix.java
@@ -16,3 +16,24 @@ class Solution {
     }
 }
 
+// 0ms( 100% ) 38.7mb ( 15.35% )
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int m=matrix.length;
+        int n=matrix[0].length;
+        
+        for(int i=0;i<m;i++){
+            
+            if(matrix[i][n-1]>=target){
+                for(int j=0;j<n;j++){
+                    if(matrix[i][j]==target)
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}

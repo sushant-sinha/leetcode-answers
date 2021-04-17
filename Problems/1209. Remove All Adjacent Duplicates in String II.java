645f9bc7af56e0d92ca2f67bcb5f041b037ab2f1
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-17 23:23:48 +0530

    new addition

--- Problems/1209. Remove All Adjacent Duplicates in String II.java
+++ Problems/1209. Remove All Adjacent Duplicates in String II.java
@@ -4,6 +4,62 @@
 
 class Solution {
     public String removeDuplicates(String s, int k) {
+        Deque<Item> deque = new LinkedList();
+        
+        //Gready Apprach,
+        //whenever we find the k consecutive same character just remove the dequeu (stack)
+        
+        for(char c : s.toCharArray()){
+            if(deque.isEmpty()){
+                deque.addLast(new Item(c, 1));
+                continue;
+            }
+            
+            //last stored item
+            Item item = deque.peekLast();
+
+            if(item.c != c){
+                deque.addLast(new Item(c, 1));
+                continue;
+            }
+            
+            if(item.count == k - 1){
+                deque.removeLast();
+            }else{
+                item.count++;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!deque.isEmpty()){
+            Item item = deque.removeFirst();
+            
+            while(item.count --> 0){
+                sb.append(item.c);
+            }
+        }
+        
+        //sb.reverse();
+        
+        return sb.toString();
+        
+    }
+}
+
+class Item {
+    char c;
+    int count;
+    
+    public Item(char c, int count){
+        this.c = c;
+        this.count = count;
+    }
+}
+
+// my solution .... TLE
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
         
         ArrayList<Character> ar=new ArrayList<>();
         

ff76b1742ea2b06154aaf07960bb1cb33c9e9be1
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-18 09:41:08 +0530

    new addition

---
+++ Problems/1828. Queries on Number of Points Inside a Circle.java
@@ -0,0 +1,22 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[] countPoints(int[][] points, int[][] queries) {
+        
+        int ans[]=new int[queries.length];
+        
+        for(int i=0;i<queries.length;i++){
+            
+            for(int t[]:points){
+                
+                if(Math.sqrt(Math.pow(t[0]-queries[i][0],2) + Math.pow(t[1]-queries[i][1],2))<=queries[i][2])
+                    ans[i]++;
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

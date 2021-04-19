bb01ad814cbdb1c45bd7851c93577b7c08572efb
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-19 15:25:51 +0530

    new addition

--- Problems/1832. Check if the Sentence Is Pangram.java
+++ Problems/1832. Check if the Sentence Is Pangram.java
@@ -1,7 +1,31 @@
 // SUSHANT SINHA
 
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        
+        boolean[] exist= new boolean[26];
+        
+        for(char c:sentence.toCharArray())
+            exist[c-'a']=true;
+        
+        return all(exist);
+        
+    }
+    
+    boolean all(boolean ar[]){
+        
+        for(boolean c:ar)
+            if(c==false)
+                return false;
+        
+        return true;
+        
+    }
+}
+
+
 // using int
-// 1ms ( 100% ) 38.6mb ( 25.00% )
+// 1ms ( 100% ) 38.1mb ( 25.00% )
 
 class Solution {
     public boolean checkIfPangram(String sentence) {

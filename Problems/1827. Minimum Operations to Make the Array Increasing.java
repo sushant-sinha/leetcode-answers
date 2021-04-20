91a53a2d9081050de193681b369c00d8670e003a
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-21 00:46:26 +0530

    new addition

---
+++ Problems/1827. Minimum Operations to Make the Array Increasing.java
@@ -0,0 +1,22 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int minOperations(int[] nums) {
+        
+        int count=0;
+        
+        for(int i=1;i<nums.length;i++){
+            
+            if(nums[i]<=nums[i-1]){
+                
+                count+=nums[i-1]-nums[i]+1;
+                nums[i]+=nums[i-1]-nums[i]+1;
+                
+            }
+            
+        }
+        
+        return count;
+        
+    }
+}

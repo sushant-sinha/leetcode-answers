07cd10a8ffe3d6d612235e17068865cc700be47d
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-20 18:33:01 +0530

    new addition

--- Problems/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ Problems/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -17,3 +17,26 @@ class Solution {
         return max;
     }
 }
+
+// using integer array
+// 12ms ( 16.90% ) 39.5mb ( 52.31% )
+
+class Solution {
+    public int minPartitions(String n) {
+        
+        int ex[]=new int[10];
+        
+        for(char c:n.toCharArray()){
+            ex[Character.getNumericValue(c)]++;
+            if(ex[9]!=0)
+                return 9;
+        }
+        int max=8;
+        for(;max>0;max--)
+            if(ex[max]!=0)
+                break;
+        
+        return max;
+    }
+}
+

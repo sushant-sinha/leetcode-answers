0c8d7f40e5f883a300591eafe76eca1eead8c53e
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-22 20:03:29 +0530

    new addition

---
+++ Problems/554. Brick Wall.java
@@ -0,0 +1,24 @@
+// from discuss section
+// https://leetcode.com/problems/brick-wall/discuss/1170889/JS-Python-Java-C%2B%2B-or-Easy-Frequency-Map-Solution-w-Explanation
+
+class Solution {
+    
+    int best = 0;
+    Map<Integer, Integer> freq = new HashMap<>();
+    
+    public int leastBricks(List<List<Integer>> wall) {
+        for (int i = 0; i < wall.size(); i++)
+            processRow(wall.get(i));
+        return wall.size() - best;
+    }
+    
+    public void processRow(List<Integer> row) {
+        int rowSum = row.get(0);
+        for (int j = 1; j < row.size(); j++) {
+            int f = freq.getOrDefault(rowSum, 0) + 1;
+            freq.put(rowSum, f);
+            if (f > best) best = f;
+            rowSum += row.get(j);
+        } 
+    }
+}

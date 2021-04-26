1a719863c8fdeb2d05eebb76c5579b3fba1c32a5
Sushant Sinha <sinhasushant04@gmail.com>
2021-04-27 00:04:16 +0530

    new addition

--- Problems/1642. Furthest Building You Can Reach.java
+++ Problems/1642. Furthest Building You Can Reach.java
@@ -1,5 +1,60 @@
 // SUSHANT SINHA
 
+/*
+
+Works fine for small number of testcases.
+TLE
+
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        
+        int ans=0,sum=0,index=heights.length-1;
+        
+        int gap[]=new int[heights.length];
+        
+        for(int i=1;i<heights.length;i++){
+            
+            if(heights[i-1]>=heights[i]){
+                ans=i;
+                continue;
+            }
+            
+            else{
+                
+                gap[index--]=(heights[i]-heights[i-1]);
+                
+                sum+=heights[i]-heights[i-1];
+                
+                if(heights.length-index>ladders){
+        
+                    Arrays.sort(gap);
+                    int temp=sum;
+                    
+                    for(int x=1;x<=ladders;x++)
+                        temp-=gap[heights.length-x];
+
+
+                    if(temp>bricks)
+                        return i-1;
+                    else
+                        ans=i;
+                
+                }
+                
+                else
+                    ans=i;
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}
+
+*/
+
 // discuss tab 
 // 14ms ( 83.98% ) 49.4mb ( 91.93% )
 

7ca7962f1e1610b8044ac4cbffa1f8b1eac72982
Sushant Sinha <sinhasushant04@gmail.com>
2021-05-01 00:09:13 +0530

    new addition

--- Problems/1561. Maximum Number of Coins You Can Get.java
+++ Problems/1561. Maximum Number of Coins You Can Get.java
@@ -20,3 +20,30 @@ class Solution {
     }
 }
 
+// using arraylist
+// 425 ( 5.09% ) 51.5mb ( 10.70% )
+
+class Solution {
+    public int maxCoins(int[] piles) {
+        
+        Arrays.sort(piles);
+        
+        ArrayList<Integer> p=new ArrayList<>();
+        for(int i:piles)
+            p.add(i);
+        
+        int ans=0;
+        
+        for(int i=0;i<(piles.length/3);i++){
+            
+            p.remove(p.size()-1);
+            ans+=p.get(p.size()-1);
+            p.remove(p.size()-1);
+            p.remove(0);
+            
+        }
+        
+        return ans;
+        
+    }
+}

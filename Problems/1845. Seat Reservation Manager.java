a55fe17b731ce6e6310f17e0a444c8a847a219ee
Sushant Sinha <sinhasushant04@gmail.com>
2021-05-03 00:04:16 +0530

    new addition

---
+++ Problems/1845. Seat Reservation Manager.java
@@ -0,0 +1,39 @@
+// SUSHANT SINHA
+
+class SeatManager {
+
+    ArrayList<Integer> unr=new ArrayList<>();
+    int latest=0;
+    
+    public SeatManager(int n) {
+        
+    }
+    
+    public int reserve() {
+        
+        if(unr.size()==0){
+            return ++latest;
+        }
+        
+        else{
+            Collections.sort(unr);
+            int ans=unr.get(0);
+            unr.remove(0);
+            return ans+1;
+        }
+        
+    }
+    
+    public void unreserve(int seatNumber) {
+        
+        unr.add(seatNumber-1);
+        
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */

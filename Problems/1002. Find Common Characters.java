acb18cae41fe475a0903e9e878429f891a2173d8
Sushant Sinha <sinhasushant04@gmail.com>
2021-05-07 00:11:22 +0530

    new addition

---
+++ Problems/1002. Find Common Characters.java
@@ -0,0 +1,40 @@
+// SUSHANT SINHA
+
+class Solution {
+    public List<String> commonChars(String[] A) {
+        
+        int ar[]=new int[26];
+        
+        for(char c:A[0].toCharArray()){
+            ar[c-'a']++;
+        }
+        
+        
+        for(int i=1;i<A.length;i++){
+            
+            int temp[]=new int[26]; 
+            
+            for(char c:A[i].toCharArray()){
+                temp[c-'a']++;
+            }
+            
+            for(int j=0;j<26;j++){
+                ar[j] = ar[j] > temp[j] ? temp[j] : ar[j];
+            }
+            
+        }
+                
+        List<String> ans=new ArrayList<>();
+        
+        for(int i=0;i<ar.length;i++){
+            for(int j=0;j<ar[i];j++){
+                
+                ans.add(Character.toString((char)(i+97)));
+                
+            }
+        }
+        
+        return ans;
+        
+    }
+}

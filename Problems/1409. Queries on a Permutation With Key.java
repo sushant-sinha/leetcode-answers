952acedb8bcd03401d093286b31006738a6a444c
Sushant Sinha <sinhasushant04@gmail.com>
2021-05-10 00:03:48 +0530

    new addition

--- Problems/1409. Queries on a Permutation With Key.java
+++ Problems/1409. Queries on a Permutation With Key.java
@@ -35,3 +35,28 @@ class Solution {
         
     }
 }
+
+
+// slow but better memory usage
+// 11ms ( 42.39% ) 38.5mb ( 98.78% )
+
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        LinkedList<Integer> list = new LinkedList();
+        for(int i = 1; i <=m; i++){
+            list.add(i);   
+        }
+        
+        int[] ans = new int[queries.length];
+        
+        for(int i = 0; i < queries.length; i++){
+            int val = queries[i],
+                index = list.indexOf(val);
+            list.remove(index);
+            list.add(0, val);
+            ans[i] = index;
+        }
+        System.gc();
+        return ans;
+    }
+}

3cb1cc65f17ec9517a18c8b910507082c5d32070
sushant-sinha <sinhasushant04@gmail.com>
2021-05-15 00:00:48 +0530

    new addition

--- Problems/151. Reverse Words in a String.java
+++ Problems/151. Reverse Words in a String.java
@@ -35,3 +35,36 @@ class Solution {
         
     }
 }
+
+// removing one for loop
+// 6ms ( 52.74% ) 38.8mb ( 83.00% )
+
+class Solution {
+    public String reverseWords(String s) {
+                
+        StringBuffer ans=new StringBuffer();
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(s.charAt(i)==' ')
+                continue;
+            
+            else{
+                StringBuffer temp=new StringBuffer();
+                
+                while(i<s.length() && s.charAt(i)!=' '){
+                    temp.insert(0,s.charAt(i));
+                    i++;
+                }
+                
+                ans.append(temp+" ");
+            }
+            
+        }
+        
+        ans.reverse();
+        
+        return ans.substring(1,ans.length());
+        
+    }
+}

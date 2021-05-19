4bb283d27cb83bc461197f2381970b11c00fb463
sushant-sinha <sinhasushant04@gmail.com>
2021-05-19 22:28:00 +0530

    new addition

--- Problems/462. Minimum Moves to Equal Array Elements II.java
+++ Problems/462. Minimum Moves to Equal Array Elements II.java
@@ -1,5 +1,8 @@
 // SUSHANT SINHA
 
+// ~~ symbol is a faster way of flooring a positive number
+// 2ms ( 98.21% ) 39.5mb ( 71.75% )
+
 class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);

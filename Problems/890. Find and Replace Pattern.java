a3ba4b8a1393d08cde7055f80fe30ba7561c4c3b
sushant-sinha <sinhasushant04@gmail.com>
2021-05-21 20:11:48 +0530

    new addition

---
+++ Problems/890. Find and Replace Pattern.java
@@ -0,0 +1,55 @@
+// SUSHANT SINHA
+
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        
+        List<String> ans= new ArrayList<>();
+        
+        for(int i=0;i<words.length;i++){
+            
+            char ar[]= new char[26];
+            // initializing char array with a charater that will be never used
+            for(int x=0;x<26;x++)
+                ar[x]='$';
+            boolean flag=true;
+            
+            for(int j=0;j<pattern.length();j++){
+                
+                if(ar[pattern.charAt(j)-'a']=='$'){
+                    
+                    boolean check=false;
+                    
+                    for(char c:ar){
+                        if(c==words[i].charAt(j)){
+                            check=true;
+                            break;
+                        }
+                    }
+                    
+                    if(check){
+                        flag=false;
+                        break;
+                    }
+                    
+                    ar[pattern.charAt(j)-'a']=words[i].charAt(j);
+                    
+                }
+                else if(ar[pattern.charAt(j)-'a']!=words[i].charAt(j)){
+                    flag=false;
+                    break;
+                }
+                else
+                    continue;
+                
+            }
+            
+            if(flag){
+                ans.add(words[i]);
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

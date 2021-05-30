50f7b5e31398b840274b0d54ed1ef62388f5db6a
sushant-sinha <sinhasushant04@gmail.com>
2021-05-31 00:05:25 +0530

    new addition

--- Problems/1881. Maximum Value after Insertion.java
+++ Problems/1881. Maximum Value after Insertion.java
@@ -1,5 +1,7 @@
 // SUSHANT SINHA
 
+// 8ms ( 100% ) 39.8mb ( 70% )
+
 class Solution {
     public String maxValue(String n, int x) {
 
@@ -37,5 +39,23 @@ class Solution {
 
         return ans.insert(n.length(),x).toString();
 
+    }
+}
+
+// from discuss section
+
+class Solution{
+    public String maxValue(String n, int x) {
+        boolean isNegative = false;
+        if(n.charAt(0) == '-') {
+            isNegative = true;
+            n = n.substring(1);
+        }
+        for(int i=0;i<n.length();i++) {
+            int current = n.charAt(i) - '0';
+            if(!isNegative && current<x || isNegative && current>x) 
+                return (isNegative? "-": "") + n.substring(0, i) + x + n.substring(i);
+        }
+        return (isNegative? "-": "") + n + x;
     }
 }

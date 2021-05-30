a4adb3fdcd78ba435cd87b9de5cada04c878be16
sushant-sinha <sinhasushant04@gmail.com>
2021-05-31 00:04:46 +0530

    new addition

--- Problems/164. Maximum Gap.java
+++ Problems/164. Maximum Gap.java
@@ -1,6 +1,7 @@
 // SUSHANT SINHA
 
 // correct answer but wrong implementation
+// 2ms ( 88.55% ) 39.1mb ( 45.80% )
 
 class Solution {
     public int maximumGap(int[] nums) {
@@ -20,5 +21,36 @@ class Solution {
         return max;
         
         
+    }
+}
+
+// correct implementation
+// from discuss section
+
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2) return 0;
+        int hi = 0, lo = Integer.MAX_VALUE, ans = 0;
+        for (int n : nums) {
+            hi = Math.max(hi, n);
+            lo = Math.min(lo, n);
+        }
+        int bsize = Math.max((hi - lo) / (nums.length - 1), 1);
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (int i = (hi - lo) / bsize; i >= 0; i--)
+            buckets.add(new ArrayList<>());
+        for (int n : nums)
+            buckets.get((n - lo) / bsize).add(n);
+        int currhi = 0;
+        for (List<Integer> b : buckets) {
+            if (b.isEmpty()) continue;
+            int prevhi = currhi > 0 ? currhi : b.get(0), currlo = b.get(0);
+            for (int n : b) {
+                currhi = Math.max(currhi, n);
+                currlo = Math.min(currlo, n);
+            }
+            ans = Math.max(ans, currlo - prevhi);
+        }
+        return ans;
     }
 }

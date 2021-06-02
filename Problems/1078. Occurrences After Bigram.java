3a05f5ed346424d812e2284a12e1b52297304f20
sushant-sinha <sinhasushant04@gmail.com>
2021-06-02 22:24:42 +0530

    new addition

---
+++ Problems/1078. Occurrences After Bigram.java
@@ -0,0 +1,25 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        
+        ArrayList<String> ans = new ArrayList<>();
+        
+        String split[]=text.split(" ");
+        
+        for(int i=0;i<split.length-2;i++){
+            
+            if(split[i].equals(first) && split[i+1].equals(second))
+                ans.add(split[i+2]);
+            
+        }
+        
+        String ret[]=new String[ans.size()];
+        
+        for(int i=0;i<ret.length;i++)
+            ret[i]=ans.get(i);
+        
+        return ret;
+        
+    }
+}

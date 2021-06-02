810e2266fee4c266c92a5454994fd48900259e42
sushant-sinha <sinhasushant04@gmail.com>
2021-06-02 22:29:16 +0530

    new addition

--- Problems/1078. Occurrences After Bigram.java
+++ Problems/1078. Occurrences After Bigram.java
@@ -25,3 +25,25 @@ class Solution {
         
     }
 }
+
+// 0ms ( 100% ) 39mb ( 5.55% )
+
+class Solution {
+    public String[] findOcurrences(String text, String first, String second) {
+        
+        ArrayList<String> ans = new ArrayList<>();
+        
+        String split[]=text.split(" ");
+        
+        for(int i=0;i<split.length-2;i++){
+            
+            if(split[i].equals(first) && split[i+1].equals(second))
+                ans.add(split[i+2]);
+            
+        }
+        
+        // forming the array by iterating makes the code slow... use list.toArray(new String[0])
+        return ans.toArray(new String[0]);
+        
+    }
+}

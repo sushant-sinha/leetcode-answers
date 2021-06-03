2d2ae03e5f3a0522ac322a42323f01c8df613a82
sushant-sinha <sinhasushant04@gmail.com>
2021-06-03 17:55:10 +0530

    new addition

---
+++ Problems/1370. Increasing Decreasing String.java
@@ -0,0 +1,44 @@
+// SUSHANT SINHA
+
+class Solution {
+    public String sortString(String s) {
+        
+        int total=s.length();
+        int ar[]=new int[26];
+        
+        StringBuffer ans=new StringBuffer();
+        
+        for(char c:s.toCharArray()){
+            ar[c-'a']++;
+        }
+        
+        while(total!=0){
+            
+            for(int i=0;i<26;i++){
+                if(ar[i]!=0){
+                    ans.append((char)('a'+i));
+                    ar[i]--;
+                    total--;
+                }
+                if(total==0)
+                    break;
+            }
+            
+            if(total==0)
+                break;
+            
+            for(int i=25;i>=0;i--){
+                if(ar[i]!=0){
+                    ans.append((char)('a'+i));
+                    ar[i]--;
+                    total--;
+                }
+                if(total==0)
+                    break;
+            }
+        }
+        
+        return ans.toString();
+        
+    }
+}

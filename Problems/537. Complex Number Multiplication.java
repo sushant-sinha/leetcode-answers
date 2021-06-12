05af1a6d7573e1e9a5067d7db31648746484f4c1
sushant-sinha <sinhasushant04@gmail.com>
2021-06-12 11:50:37 +0530

    new addition

--- Problems/537. Complex Number Multiplication.java
+++ Problems/537. Complex Number Multiplication.java
@@ -23,3 +23,32 @@ class Solution {
 
     }
 }
+
+// if we use StringBuilder and its append function in the return statement
+
+// 1ms ( 93.06% ) 36.8mb ( 94.34% )
+
+class Solution {
+    public String complexNumberMultiply(String num1, String num2) {
+        
+        String split1[]=num1.split("\\+");
+        String split2[]=num2.split("\\+");
+        
+        int re1=Integer.parseInt(split1[0]);
+        int re2=Integer.parseInt(split2[0]);
+        int img1=Integer.parseInt(split1[1].substring(0,split1[1].length()-1));
+        int img2=Integer.parseInt(split2[1].substring(0,split2[1].length()-1));
+
+        int re, img;
+        re=(re1*re2-(img1*img2));
+        img=(re1*img2)+(re2*img1);
+
+        return new StringBuilder()
+            .append(re)
+            .append("+")
+            .append(img)
+            .append("i")
+            .toString();
+
+    }
+}

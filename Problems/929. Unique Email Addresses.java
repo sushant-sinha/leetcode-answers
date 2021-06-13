887cb2a903fe1d3ad66777ddc0d09eb5710f4007
sushant-sinha <sinhasushant04@gmail.com>
2021-06-13 10:30:42 +0530

    new addition

---
+++ Problems/929. Unique Email Addresses.java
@@ -0,0 +1,45 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        
+        ArrayList<String> dis = new ArrayList<>();
+        int ans=0;
+        
+        for(String s:emails){
+            String em=effective(s);
+            if(!dis.contains(em)){
+                dis.add(em);
+                ans++;
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+    String effective( String s ){
+        
+        String split[]=s.split("@");
+        
+        StringBuilder str= new StringBuilder();
+        
+        for(char c:split[0].toCharArray()){
+            if(c=='.'){
+                continue;
+            }
+            
+            if(c=='+'){
+                break;
+            }
+            
+            else{
+                str.append(c);
+            }
+        }
+        
+        str.append("@"+split[1]);
+        
+        return str.toString();
+    }
+}

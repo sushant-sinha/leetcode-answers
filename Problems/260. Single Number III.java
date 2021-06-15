4a44c975c44c872330e3054239c9b78436e4a6ac
sushant-sinha <sinhasushant04@gmail.com>
2021-06-15 12:18:59 +0530

    new addition

--- Problems/260. Single Number III.java
+++ Problems/260. Single Number III.java
@@ -26,3 +26,40 @@ class Solution {
         
     }
 }
+
+// 1ms ( 96.95% ) 39.7mb ( 38.21 %)
+
+// from discuss tab
+// using bit manipulation
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        int[] result = new int[2];
+        
+        // Step 1: Do XOR of all element
+        int xor = 0;
+        
+        for(int i : nums){
+            xor ^= i;
+        }
+        
+        // Step 2: Find Right Most Set Bit in xor
+        int rsb = xor & (~(xor-1));
+        
+        // Step 3: Do AND of rsb with each element
+        for(int i : nums){
+            
+            if((i&rsb) == 0){
+                result[0] ^= i;
+            }
+            else{
+                result[1] ^= i;
+            }
+            
+        }
+        
+        return result;
+        
+    }
+}

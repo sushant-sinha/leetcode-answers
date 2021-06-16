8668f99b369ed0105f226a26e429bb39c8588dc2
sushant-sinha <sinhasushant04@gmail.com>
2021-06-16 10:09:29 +0530

    new addition

---
+++ Problems/1268. Search Suggestions System.java
@@ -0,0 +1,31 @@
+// SUSHANT SINHA
+
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        
+        List<List<String>> ans = new ArrayList<>();
+
+        Arrays.sort(products);
+        
+        for(int i=0;i<searchWord.length();i++){
+
+        	ArrayList<String> temp = new ArrayList<>();
+
+        	String s=searchWord.substring(0,i+1);
+
+        	for(String x:products){
+        		if(temp.size()==3)
+        			break;
+
+        		if(s.length()<=x.length() && s.equals(x.substring(0,i+1)))
+        			temp.add(x);
+        	}
+
+        	ans.add(temp);
+
+        }
+
+        return ans;
+        
+    }
+}

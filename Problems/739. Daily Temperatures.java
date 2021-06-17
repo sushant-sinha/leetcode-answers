c79b677caebe8081941ecc0ade576374267fd794
sushant-sinha <sinhasushant04@gmail.com>
2021-06-17 14:21:38 +0530

    new addition

--- Problems/739. Daily Temperatures.java
+++ Problems/739. Daily Temperatures.java
@@ -24,3 +24,22 @@ class Solution {
         
     }
 }
+
+// solution using stack
+// 7ms ( 95.59% ) 54.1mb ( 18.19% )
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
+            }
+            stack[++top] = i;
+        }
+        return ret;
+    }
+}

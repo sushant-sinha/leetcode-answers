63222e5da6904d7253d93ca7999b827fcb92e1a8
sushant-sinha <sinhasushant04@gmail.com>
2021-06-17 14:21:15 +0530

    new addition

---
+++ Problems/739. Daily Temperatures.java
@@ -0,0 +1,24 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        for(int i=0;i<temperatures.length;i++){
+            int x=0,in=0;
+            for(int j=i+1;j<temperatures.length;j++){
+                if(temperatures[j]>temperatures[i]){
+                    x++;
+                    in++;
+                    temperatures[i]=x;
+                    break;
+                }
+                x++;
+            }
+            if(in==0)
+                temperatures[i]=0;
+        }
+
+        return temperatures;
+        
+    }
+}

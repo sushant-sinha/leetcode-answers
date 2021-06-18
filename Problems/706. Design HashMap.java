282b74e5f21ad9bc59bc2877a022980323e972a3
sushant-sinha <sinhasushant04@gmail.com>
2021-06-18 22:45:41 +0530

    new addition

--- Problems/706. Design HashMap.java
+++ Problems/706. Design HashMap.java
@@ -87,3 +87,53 @@ class MyHashMap {
  * int param_2 = obj.get(key);
  * obj.remove(key);
  */
+
+
+// using array
+// 18ms ( 63.29% ) 46.7mb ( 42.59% )
+
+class MyHashMap {
+    
+    int val[];
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        
+        val=new int[1000001];
+        
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        
+        // since 0 can be input val, so we store every value incremented by one i.e. 0 is stored as 1.
+        // if the value is found to be 0, this means its blank i.e no value exists for this key ( array index )
+
+        val[key]=value+1;
+        
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        
+        if(val[key]==0)
+            return -1;
+        
+        return val[key]-1;        
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        
+        val[key]=0;
+        
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */

fb86ce748beb14e95f4a70fd8fd1fb684cc6b55a
sushant-sinha <sinhasushant04@gmail.com>
2021-06-21 18:46:24 +0530

    new addition

---
+++ Problems/566. Reshape the Matrix.java
@@ -0,0 +1,28 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        
+        if((mat.length*mat[0].length)!=r*c)
+            return mat;
+        
+        int ans[][]=new int[r][c];
+
+        r=0;c=-1;
+        
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                
+                ans[r][++c]=mat[i][j];
+
+                if(c==ans[0].length-1){
+                    r++;
+                    c=-1;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+}

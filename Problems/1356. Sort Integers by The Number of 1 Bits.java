dc0fcfdac2c9de8f50952778206bee2410cfdb69
sushant-sinha <sinhasushant04@gmail.com>
2021-06-22 08:29:37 +0530

    new addition

---
+++ Problems/1356. Sort Integers by The Number of 1 Bits.java
@@ -0,0 +1,21 @@
+// SUSHANT SINHA
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        Integer[] temp = new Integer[arr.length];
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        Arrays.sort(temp, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.bitCount(o1)-Integer.bitCount(o2);
+            }
+        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+        return arr;
+    }
+}

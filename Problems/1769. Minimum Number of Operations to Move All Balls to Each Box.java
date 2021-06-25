c955312d8c3b1dfc2d1e7556d15998ecef90a774
sushant-sinha <sinhasushant04@gmail.com>
2021-06-25 21:11:49 +0530

    new addition

--- Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.java
+++ Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.java
@@ -1,11 +1,13 @@
 // SUSHANT SINHA
 
+// 152ms ( 38.41% ) 39.5mb ( 71.27% )
+
 class Solution {
     public int[] minOperations(String boxes) {
         
         int ans[]=new int[boxes.length()];
         char ar[]=boxes.toCharArray();
-        
+
         for(int i=0;i<ans.length;i++){
         	ans[i]=count(ar,i);
         }

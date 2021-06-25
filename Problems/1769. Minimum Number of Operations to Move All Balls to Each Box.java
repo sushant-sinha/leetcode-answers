2412a78262159d30c2c7074924e9a153454a9529
sushant-sinha <sinhasushant04@gmail.com>
2021-06-25 21:12:03 +0530

    new addition

--- Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.java
+++ Problems/1769. Minimum Number of Operations to Move All Balls to Each Box.java
@@ -33,3 +33,37 @@ class Solution {
     	return c;
     }
 }
+
+// 2ms ( 98.24% ) approach from 
+
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] ans = new int[n];
+        char[] balls = boxes.toCharArray();
+        int left = balls[0] == '1' ? 1 : 0, right = 0;
+
+        // this for loop sets ans[0] and the rightmost '1'
+        for (int i = 1; i < n; i++) {
+            if (balls[i] == '1') {
+                right++;
+                ans[0] += i;
+            }
+        }
+
+        // this for loop is for all 1 to n elements
+        for (int i = 1; i < n; i++) {
+
+        	// ith position has the answer that of: the previous index value + number of '1' to its left - the number of the '1' to itsa right
+        	// it is same as 1*left - 1*right as the number is being reduced by 1 for each right '1'(getting closer) and increased by 1 for each '1' to its left (getting farther)
+            ans[i] = ans[i - 1] + left - right;
+
+            // if at index i, there was a one , update the left to have one more '1' and right to have one less '1'
+            if (balls[i] == '1') {
+                left++;
+                right--;
+            }
+        }
+        return ans;
+    }
+}

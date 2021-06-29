fe66353e5a0bcb6b134a9add0118cef140aff473
sushant-sinha <sinhasushant04@gmail.com>
2021-06-29 12:57:42 +0530

    new addition

--- Problems/1877. Minimize Maximum Pair Sum in Array.java
+++ Problems/1877. Minimize Maximum Pair Sum in Array.java
@@ -16,3 +16,21 @@ class Solution {
         
     }
 }
+
+// by just replacing nums.length with a variale (n)
+// 49ms ( 97.36% ) 54.7mb ( 85.08% )
+
+class Solution {
+    public int minPairSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int ans=0,n=nums.length;
+        
+        for(int i=0;i<nums.length/2;i++)
+            ans = ans<nums[i]+nums[n-1-i] ? nums[i]+nums[n-1-i] : ans;
+        
+        return ans;
+        
+    }
+}

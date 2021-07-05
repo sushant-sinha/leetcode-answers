9b57b111a3cee498438772c804d09d50e078cb71
sushant-sinha <sinhasushant04@gmail.com>
2021-07-05 19:00:21 +0530

    new addition

--- Problems/1913. Maximum Product Difference Between Two Pairs.java
+++ Problems/1913. Maximum Product Difference Between Two Pairs.java
@@ -13,3 +13,36 @@ class Solution {
     }
 }
 
+// 2ms ( 92.18% ) 38.7mb ( 98.08% )
+// faster solution
+
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int low1 = nums[0], low2 = 10001, high1 = nums[0], high2 = -1;
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]>=high1)
+            {
+                high2 = high1;
+                high1 = nums[i];
+            }
+            else if(nums[i]<high1 && nums[i]>high2)
+            {
+                high2 = nums[i];
+            }
+            
+            if(nums[i]<=low1)
+            {
+                low2 = low1;
+                low1 = nums[i];
+            }
+            else if(nums[i]>low1 && nums[i]<low2)
+            {
+                low2 = nums[i];
+            }
+        }
+        
+        return (high1*high2) - (low1*low2);
+    }
+}

8178950423492137d8fd3db4d5ee052415ec70e4
sushant-sinha <sinhasushant04@gmail.com>
2021-07-11 20:13:26 +0530

    new addition

---
+++ Problems/295. Find Median from Data Stream.java
@@ -0,0 +1,36 @@
+class MedianFinder {
+    
+    ArrayList<Integer> ar=new ArrayList<>();
+    int len=0;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        
+        ar.add(num);
+        len++;
+        
+    }
+    
+    public double findMedian() {
+        
+        Collections.sort(ar);
+        
+        if(len%2==0)
+            return (double)(ar.get(len/2)+ar.get((len/2)-1))/2;
+        
+        else
+            return (double)ar.get(len/2);
+        
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */

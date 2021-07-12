467b3adc469b274be714019e5aeca06392349807
sushant-sinha <sinhasushant04@gmail.com>
2021-07-12 10:47:27 +0530

    new addition

--- Problems/442. Find All Duplicates in an Array.java
+++ Problems/442. Find All Duplicates in an Array.java
@@ -24,3 +24,26 @@ class Solution {
         
     }
 }
+
+// 3ms ( 100% ) 44.6mb ( 100% )
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> ans=new ArrayList<>();
+        
+        int freq[]=new int[nums.length+1];
+        
+        for(int i:nums)
+            freq[i]++;
+        
+        for(int i=0;i<freq.length;i++)
+            if(freq[i]==2)
+                ans.add(i);
+            
+        System.gc();
+        
+        return ans;
+        
+    }
+}

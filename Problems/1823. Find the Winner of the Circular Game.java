e6fa7059bcb13dc800e65e17c492c462c72e478e
sushant-sinha <sinhasushant04@gmail.com>
2021-07-16 13:49:45 +0530

    new addition

--- Problems/1823. Find the Winner of the Circular Game.java
+++ Problems/1823. Find the Winner of the Circular Game.java
@@ -24,3 +24,18 @@ class Solution {
         
     }
 }
+
+// best solution
+// recursive approach
+// 0ms ( 100% ) 35.8mb ( 82.47% )
+
+class Solution {
+    public int findTheWinner(int n, int k) 
+    {
+        if(n==1)
+            return 1;
+        return (findTheWinner(n-1,k)+k-1)%n+1;
+        //return n-k;
+        
+    }
+}

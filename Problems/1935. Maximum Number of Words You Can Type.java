80d26e774e7686886fae0f7067dd534f184cba88
sushant-sinha <sinhasushant04@gmail.com>
2021-07-18 11:09:48 +0530

    new addition

---
+++ Problems/1935. Maximum Number of Words You Can Type.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        
+        int ans=0;        
+        String sp[]=text.split("\\s");
+        
+        ArrayList<Character> br=new ArrayList<>();
+        
+        for(char c:brokenLetters.toCharArray()){
+            
+            br.add(new Character(c));
+            
+        }
+        
+        for(String s:sp){
+            
+            for(char c:s.toCharArray()){
+                if(br.contains(c)){
+                    ans++;
+                    break;
+                }
+            }
+            
+        }
+                
+        return sp.length-ans;
+        
+    }
+}

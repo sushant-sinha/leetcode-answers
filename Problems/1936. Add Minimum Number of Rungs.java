70238bc3ed5199b3c25d041a7511412af5cb0933
sushant-sinha <sinhasushant04@gmail.com>
2021-07-19 22:47:24 +0530

    new addition

---
+++ Problems/1936. Add Minimum Number of Rungs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int addRungs(int[] rungs, int dist) {
+        
+        int ans=0;
+        
+        if(dist<rungs[0]){            
+            ans+=rungs[0]/dist;
+            
+            if(rungs[0]%dist==0)
+                ans--;
+        }
+        
+        
+        for(int i=0;i<rungs.length-1;i++){
+            
+            if((float)(rungs[i+1]-rungs[i])/dist > 1){
+                ans+=(rungs[i+1]-rungs[i])/dist;
+                if((rungs[i+1]-rungs[i])%dist==0){
+                    ans--;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+}

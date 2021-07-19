27180ab39b0f9fdbba4522f35c5164150b90b7ad
sushant-sinha <sinhasushant04@gmail.com>
2021-07-19 22:56:58 +0530

    new addition

--- Problems/1936. Add Minimum Number of Rungs.java
+++ Problems/1936. Add Minimum Number of Rungs.java
@@ -2,6 +2,11 @@
 
 // 2ms ( 28.57% ) 51.7mb ( 28.57% )
 
+/* 
+    hardest part for me was to notice the fact that we have
+    to subtract 1 when the difference is perfectly divisible 
+    by dist.
+*/
 class Solution {
     public int addRungs(int[] rungs, int dist) {
         
@@ -10,6 +15,12 @@ class Solution {
         if(dist<rungs[0]){            
             ans+=rungs[0]/dist;
             
+            /* 
+                if remainder is 0 ( i.e. perfectly divisible by dist )
+                subtract 1;
+
+            */
+
             if(rungs[0]%dist==0)
                 ans--;
         }
@@ -19,6 +30,13 @@ class Solution {
             
             if((float)(rungs[i+1]-rungs[i])/dist > 1){
                 ans+=(rungs[i+1]-rungs[i])/dist;
+
+                /* 
+                    if remainder is 0 ( i.e. perfectly divisible by dist )
+                    subtract 1;
+
+                */
+
                 if((rungs[i+1]-rungs[i])%dist==0){
                     ans--;
                 }

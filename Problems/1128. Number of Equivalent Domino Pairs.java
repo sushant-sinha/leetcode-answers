79f5c3623e83d1d6f8a32eb618d6deb333b43a7f
sushant-sinha <sinhasushant04@gmail.com>
2021-07-21 18:09:17 +0530

    new addition

--- Problems/1128. Number of Equivalent Domino Pairs.java
+++ Problems/1128. Number of Equivalent Domino Pairs.java
@@ -1,5 +1,16 @@
+// SUSHANT SINHA
+
+// 2ms ( 98.35% ) 47.6mb ( 99.40% )
+
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
+        
+        /* 
+            counts the pairs having same sum
+            i.e [1,2] has (1*10)+(2)
+            ans [2,1] has (1*10)+(2)
+        */
+        
         int[] sumcounter = new int[100];
         int ans= 0, index;
         
@@ -10,6 +21,7 @@ class Solution {
         
         for (int i = 0; i < 100; i++)
             if (sumcounter[i] > 1)
+                // for getting the number of pairs from the count
                 ans += (sumcounter[i]*(sumcounter[i]-1))/2;
         
         return ans;

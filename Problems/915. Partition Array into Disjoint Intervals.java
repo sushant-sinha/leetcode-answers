58c9de9cb5631fcbeefb0a3cfbfc0f6759c59df9
sushant-sinha <sinhasushant04@gmail.com>
2021-07-22 23:23:47 +0530

    new addition

---
+++ Problems/915. Partition Array into Disjoint Intervals.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+
+        int max=nums[0];
+
+        for(int i=0;i<nums.length-1;i++)
+        {
+            // can use int flag
+            boolean f=true;
+            max=max>nums[i] ? max : nums[i];
+            
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[j]<max)
+                {
+                    f=false;
+                    break;
+                }
+            }
+            
+            if(f)
+                return i+1;
+        }
+        
+        return 0;
+    }
+}

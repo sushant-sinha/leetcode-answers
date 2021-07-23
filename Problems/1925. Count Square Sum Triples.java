ac2b9a81a32583d896f1a128c38cd23b368a5d47
sushant-sinha <sinhasushant04@gmail.com>
2021-07-23 22:42:56 +0530

    new addition

--- Problems/1925. Count Square Sum Triples.java
+++ Problems/1925. Count Square Sum Triples.java
@@ -27,3 +27,30 @@ class Solution {
     }
     
 }
+
+// best solution from submission details ( innovative )
+
+
+// 0ms ( 100% ) 35.5mb ( 93.84% )
+
+class Solution {
+
+    // for square triples "a^2 + b^2 = c^2" (where c<=250), get
+    // all possible c from https://www.mathsisfun.com/numbers/pythagorean-triples.html
+    
+    final static int [] tripleC={
+        5, 13, 17, 25, 29, 37, 41, 53,61, 65,65,73,85,85, 89,97, 101,109,113, 125,137,
+        145,145,149,157,169,173,181, 185,185,193,197,205,205,221,221,229,233,241
+    };
+    
+    public int countTriples(int n) {
+        int ct=0;
+        for ( int c:tripleC )
+            if (c>n)
+                break;
+            else
+                ct += 2*(n/c);
+        return ct;
+        
+    }
+}

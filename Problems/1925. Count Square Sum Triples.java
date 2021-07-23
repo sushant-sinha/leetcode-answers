340ca49be2a3201bb5affee3efd6f1a89729d877
sushant-sinha <sinhasushant04@gmail.com>
2021-07-23 22:37:39 +0530

    new addition

---
+++ Problems/1925. Count Square Sum Triples.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countTriples(int n) {
+        
+        int ans=0;
+        
+        for(int i=1;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(sq(i,j,n))
+                    ans++;
+            }
+        }
+        
+        return ans*2;
+        
+    }
+    
+    boolean sq(int a, int b, int n){
+                
+        int s=(int)Math.sqrt((a*a)+(b*b));
+                
+        return s<=n && (s*s)==((a*a)+(b*b));
+        
+    }
+    
+}

02ac3eecff027d8383f10a6777eb2d3b0be01185
sushant-sinha <sinhasushant04@gmail.com>
2021-07-25 15:37:13 +0530

    new addition

--- Problems/762. Prime Number of Set Bits in Binary Representation.java
+++ Problems/762. Prime Number of Set Bits in Binary Representation.java
@@ -40,3 +40,46 @@ class Solution {
         
     }
 }
+
+
+// more efficient method of finding ones is used
+
+// 8ms ( 64.73% ) 35.6mb ( 84.57% )
+
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        
+        int prime[]={2,3,5,7,11,13,17,19};
+        
+        int ans=0;
+        
+        while(left!=right+1){
+            
+            int c=isPrime(left);
+            
+            for(int i:prime){
+                if(i==c){
+                    ans++;
+                    break;
+                }
+            }
+            
+            left++;
+        }
+        
+        return ans;
+        
+    }
+    
+    int isPrime(int n){        
+        int one=0;
+        
+        while(n!=0){
+            one++;
+            n&=(n-1);
+        }
+        
+        return one;
+        
+    }
+}

c77e2f4d5c2ccbed81a5986d14ebb7c6e9bffb00
sushant-sinha <sinhasushant04@gmail.com>
2021-07-27 00:26:23 +0530

    new addition

--- Problems/1945. Sum of Digits of String After Convert.java
+++ Problems/1945. Sum of Digits of String After Convert.java
@@ -37,3 +37,26 @@ class Solution {
         
     }
 }
+
+// best solution
+
+// 0ms ( 100% )
+
+class Solution {
+    public int getLucky(String s, int k) {
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            int val = c - 'a' + 1;
+            sum += val % 10 + val / 10;
+        }
+        for (int i = 1; i < k; i++) {
+            int newsum = 0;
+            while (sum > 0) {
+                newsum += sum % 10;
+                sum /= 10;
+            }
+            sum = newsum;
+        }
+        return sum;
+    }
+}

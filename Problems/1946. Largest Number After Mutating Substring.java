eb728bc69782e49b0be73ada703a020c31d4fd48
sushant-sinha <sinhasushant04@gmail.com>
2021-07-31 19:32:46 +0530

    new addition

---
+++ Problems/1946. Largest Number After Mutating Substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String maximumNumber(String num, int[] change) {
+        int start = -1;
+        char[] charArr = num.toCharArray();
+        
+        for(int i = 0; i < charArr.length; i++) {
+            
+            //converting char to string
+            int j = charArr[i] - '0';
+            
+            if(change[j] > j) {
+                
+                if(start < 0) {
+                    start = i;
+                }
+                
+                //converting int to char
+                charArr[i] = (char) (change[j] + '0');
+                
+            }
+            else if(change[j] < j){
+                if(start >= 0)
+                    break;
+            }
+        }
+        
+        return String.valueOf(charArr);
+    }
+}

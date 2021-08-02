83268f5c642299ddd7d0443a72209384598e4cbd
sushant-sinha <sinhasushant04@gmail.com>
2021-08-02 21:42:45 +0530

    new addition

--- Problems/942. DI String Match.java
+++ Problems/942. DI String Match.java
@@ -1,3 +1,37 @@
+// SUSHANT SINHA
+
+// based on solution below this section
+// 2ms ( 95.30% ) 39.8mb ( 93.51% )
+
+class Solution {
+    public int[] diStringMatch(String s) {
+        int[] ans = new int[s.length()+1];
+        int left = 0, right = s.length();
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            ans[i]= 'I'==s.charAt(i) ? left++ : right--;
+            
+        }
+        
+        ans[s.length()]=left;
+        
+        // using System.gc() can give 39.1mb ( 100% )... but the runtime reduces to 3ms ( 31.47% )
+        //System.gc();
+        
+        return ans;
+    }
+}
+
+
+
+// problem statement was not well structured... few hints from the examples were not enough :(
+// had to refer to the discussion section
+
+// solution from https://leetcode.com/problems/di-string-match/discuss/1375270/JAVAororBeats-100-oror-Two-pointer
+
+// 2ms ( 95.30% ) 40mb ( 78.75% )
+
 class Solution {
     public int[] diStringMatch(String s) {
         int[] res = new int[s.length()+1];

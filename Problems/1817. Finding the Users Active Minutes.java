af568b3ff28137de115cde280c929c47b19393c5
sushant-sinha <sinhasushant04@gmail.com>
2021-08-03 22:07:35 +0530

    new addition

--- Problems/1817. Finding the Users Active Minutes.java
+++ Problems/1817. Finding the Users Active Minutes.java
@@ -2,12 +2,14 @@
 
 // my lame brute force 🤣🤣😶😑
 // 1953 ( 5.02% ) 53.7mb ( 20.29% )
+// can be made easier we just throw HashMap
 
 class Solution {
     public int[] findingUsersActiveMinutes(int[][] logs, int k) {
         
         int ans[]=new int[k];
         
+        // for finding all unique IDs
         ArrayList<Integer> all = new ArrayList<>();
         
         for(int i[]:logs){
@@ -16,6 +18,8 @@ class Solution {
         }
         
         for(int i:all){
+
+            // for finding unique time
             ArrayList<Integer> rec =new ArrayList<>();
             
             for(int x[]:logs){
@@ -32,5 +36,36 @@ class Solution {
         
         return ans;
         
+    }
+}
+
+// best solution
+
+// 10ms ( 100% ) 49.7mb ( 64.55% )
+
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+
+        // magic
+        Arrays.sort(logs,(a,b)->a[0]==b[0]?a[1]-b[1]: a[0]-b[0]);
+        int ans[] = new int[k];
+        
+        // in one array traverse we get the answer
+
+        int i=0;
+        while(i<logs.length){
+            int count=0, last=-1;
+            int user=logs[i][0];
+            while(i<logs.length && logs[i][0]==user){
+                if(logs[i][1]!=last){
+                    count++;
+                    last = logs[i][1];
+                }
+                i++;
+            }
+            ans[count-1]++;
+        }
+            
+        return ans;
     }
 }

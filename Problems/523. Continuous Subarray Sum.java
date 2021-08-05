92aeecde396a40a8baa5d89a36aa8bbc78dd4b2e
sushant-sinha <sinhasushant04@gmail.com>
2021-08-05 17:20:25 +0530

    new addition

--- Problems/523. Continuous Subarray Sum.java
+++ Problems/523. Continuous Subarray Sum.java
@@ -1,5 +1,28 @@
 // SUSHANT SINHA
 
+// expected solution
+// 14ms ( 93.78% ) 55.5mb ( 53.14% )
+
+class Solution {
+     public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        
+        int remainder = 0;
+        for (int i = 0; i < nums.length; i++) {
+            remainder = (nums[i] + remainder) % k;
+            if (map.containsKey(remainder)) {
+                Integer prev = map.get(remainder);
+                if (i - prev > 1) return true;
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        
+        return false;
+    }
+}
+
 // brute force....can pass 93/94 test cases
 // the only failed testcase had nums.length=10^5
 

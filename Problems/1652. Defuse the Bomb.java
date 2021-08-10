b50f0746a0a78dfc7528d7b68b5f804ae9b22ba6
sushant-sinha <sinhasushant04@gmail.com>
2021-08-10 20:10:13 +0530

    new addition

---
+++ Problems/1652. Defuse the Bomb.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        
+        int ans[]=new int[code.length];
+        
+        if(k==0){
+            for(int i=0;i<code.length;i++)
+                ans[i]=0;
+        }
+        
+        else if(k>0){
+            for(int i=0;i<code.length;i++){
+                for(int j=i+1;j<=i+k;j++){
+                    ans[i]+=code[j%code.length];
+                }
+            }
+        }
+        
+        else{
+            for(int i=0;i<code.length;i++){
+                int x=k;
+                for(int j=i-1;x!=0;j--,x++){
+                    if(j<0)
+                        ans[i]+=code[code.length+j];
+                    else
+                        ans[i]+=code[j];
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+}

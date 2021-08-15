04f50c2be70a135a71d63bd88cec81ce985b6dcc
sushant-sinha <sinhasushant04@gmail.com>
2021-08-15 17:48:45 +0530

    new addition

---
+++ Problems/1217. Minimum Cost to Move Chips to The Same Position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        
+        int even=0,odd=0;
+        
+        for(int i=0;i<position.length;i++){
+            
+            if(position[i]%2==0)
+                even++;
+            else
+                odd++;
+        }
+        
+        return even>odd ? odd : even;
+    }
+}

c10b6b15fa03df095c9de6915489ebf8d5e63760
sushant-sinha <sinhasushant04@gmail.com>
2021-08-18 23:41:31 +0530

    new addition

---
+++ Problems/1656. Design an Ordered Stream.java
@@ -0,0 +1,21 @@
+class OrderedStream {
+    String[] words = null;
+    int ptr = 0;
+    public OrderedStream(int n) {
+        this.words = new String[n];
+    }
+
+    public List<String> insert(int idKey, String value) {
+        words[idKey-1] = value;
+        List<String> res = new ArrayList<>();
+        if(idKey-1 == ptr)
+        {
+            while(ptr < words.length && words[ptr] != null)
+            {
+                res.add(words[ptr++]);
+            }
+        }
+
+        return res;
+    }
+}

d317455a9954490b930b94c79eb241ae0847ff1d
sushant-sinha <sinhasushant04@gmail.com>
2021-08-21 22:56:31 +0530

    new addition

---
+++ Problems/1974. Minimum Time to Type Word Using Special Typewriter.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minTimeToType(String word) {
+                
+        int ans=0;
+        
+        int temp=0;
+        
+        for(char c:word.toCharArray()){
+            
+            int x=temp-((int)c-'a');
+            
+            x= x<0 ? -1*x : x;
+            
+            ans+= x>12 ? 26-x : x;
+                        
+            ans++;
+            
+            temp=c-'a';
+            
+        }
+        
+        return ans;
+        
+    }
+}

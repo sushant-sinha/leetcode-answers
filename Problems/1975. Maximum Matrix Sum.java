cff3a407ab44d8ec04ece9437cf2436a7fbc6e02
sushant-sinha <sinhasushant04@gmail.com>
2021-08-22 12:55:23 +0530

    new addition

---
+++ Problems/1975. Maximum Matrix Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        
+        long ans = 0, count = 0, min = Integer.MAX_VALUE;
+        
+        for(int i=0;i<matrix.length;i++)
+            
+            for(int j=0;j<matrix[0].length;j++) {
+                
+                if(matrix[i][j] < 0) {
+                    count++;
+                    matrix[i][j] *= -1;
+                }
+                
+                min = min>matrix[i][j] ? matrix[i][j] : min;
+                ans += matrix[i][j];
+                
+            }
+        
+        return count%2!=0 ? ans-min*2 : ans;
+        
+    }
+}

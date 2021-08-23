ddc3ae84d422e6988006da938d8546165c963079
sushant-sinha <sinhasushant04@gmail.com>
2021-08-23 20:37:13 +0530

    new addition

--- Problems/1979. Find Greatest Common Divisor of Array.java
+++ Problems/1979. Find Greatest Common Divisor of Array.java
@@ -23,3 +23,38 @@ class Solution {
         
     }
 }
+
+// using for loop instead of Arrays.sort()
+
+// 0ms ( 100% ) 38.9mb ( 28.57% )
+
+class Solution {
+    public int findGCD(int[] nums) {
+        
+        //Arrays.sort(nums);
+        
+        int min=1001,max=0;
+        
+        for(int i:nums){
+            
+            min= i<min ? i : min;
+            max= i>max ? i : max;
+            
+        }
+        
+        int ans=1;
+        
+        if(min==max || max%min==0)
+            return min;
+        
+        for(int i=1;i<=min/2;i++){
+            
+            if(min%i==0 && max%i==0)
+                ans=i;
+            
+        }
+        
+        return ans;
+        
+    }
+}

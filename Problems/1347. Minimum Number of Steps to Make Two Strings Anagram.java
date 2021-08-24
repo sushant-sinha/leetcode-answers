0b3580184b8e26130a7a8f0dfec890ea8bfca0b7
sushant-sinha <sinhasushant04@gmail.com>
2021-08-24 18:34:55 +0530

    new addition

--- Problems/1347. Minimum Number of Steps to Make Two Strings Anagram.java
+++ Problems/1347. Minimum Number of Steps to Make Two Strings Anagram.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 13ms ( 71.00% ) 39.3mb ( 83.11% )
+
 class Solution {
     public int minSteps(String s, String t) {
         
@@ -7,11 +11,14 @@ class Solution {
         
         for(int i=0;i<s.length();i++){
             
+            // subtracting to nullify same character between s & t
             ar1[s.charAt(i)-'a']++;
             ar1[t.charAt(i)-'a']--;
             
         }
         
+        // can use ans+= i>0 ? (-1)*i : i;........ but then have to divide the answer by 2 because even then the ones being replaced are also counted different :)
+
         for(int i:ar1)
             ans+= i>0 ? i : 0;
         

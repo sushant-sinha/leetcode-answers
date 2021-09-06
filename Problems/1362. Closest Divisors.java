f3a6cb81e9070d7e906302b9c76ce8070c695176
Sushant Sinha <sinhasushant04@gmail.com>
2021-09-06 12:52:25 +0530

    new addition

--- Problems/1362. Closest Divisors.java
+++ Problems/1362. Closest Divisors.java
@@ -1,3 +1,13 @@
+// SUSHANT SINHA
+
+// 0ms ( 100% ) 36.8mb ( 72.07% )
+/*
+
+In https://leetcode.com/problems/closest-divisors/discuss/1365411/easy-java-solution, "i>=0" was used, even though "i" will never be equal to 0.
+But still, divinding numbers by 0 is not good ☹☹.
+
+*/
+
 class Solution {
     public int[] closestDivisors(int num) {
         int s1=num+1, s2=num+2;

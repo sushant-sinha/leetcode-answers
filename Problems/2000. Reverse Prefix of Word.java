16d6e20c8f6e4a8a75b9d8b17a89038b8abb312b
sushant-sinha <sinhasushant04@gmail.com>
2021-09-14 14:41:28 +0530

    new addition

--- Problems/2000. Reverse Prefix of Word.java
+++ Problems/2000. Reverse Prefix of Word.java
@@ -1,5 +1,30 @@
 // SUSHANT SINHA
 
+// 1ms ( 73.36% ) 38.7mb ( 83.93% )
+// using StringBuffer's inbuilt function for reversing the splitted string
+
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        
+        int n=word.indexOf(ch);
+        
+        if(n==-1)
+            return word;
+        
+        StringBuffer sb=new StringBuffer();
+        sb.append(word.substring(0,n+1));
+        
+        sb.reverse();
+        
+        // for(int i=n;i>=0;i--){
+        //     temp+=word.charAt(i);
+        // }
+        
+        return sb+word.substring(n+1);
+        
+    }
+}
+
 // 9ms ( 7.50% ) 39.7mb ( 8.94% )
 // basic
 

c8739645a7dd9de4a1c0bd11d40b8290c55f7274
sushant-sinha <sinhasushant04@gmail.com>
2021-09-17 19:06:25 +0530

    new addition

---
+++ Problems/540. Single Element in a Sorted Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        
+        if(nums.length==1)
+            return nums[0];
+            
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i]==nums[i+1])
+                    i++;
+                else
+                    return nums[i];
+            }
+        
+        return nums[nums.length-1];
+        
+    }
+}

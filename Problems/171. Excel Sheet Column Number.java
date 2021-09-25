dec48044fe66dd2a40e3db44179657b6c8458cc6
sushant-sinha <sinhasushant04@gmail.com>
2021-09-26 00:01:28 +0530

    new addition

--- Problems/171. Excel Sheet Column Number.java
+++ Problems/171. Excel Sheet Column Number.java
@@ -8,6 +8,25 @@ class Solution {
         int ans=0;
                 
         for(int i=0;i<columnTitle.length();i++){
+
+            /* 
+
+                character number (for A=1, B=2......) is multiplied with 26 to the power of number of digits behind it
+                
+                for CABC:
+
+                (C=3): 3*(26^3)
+                        +
+                (A=1): 1*(26^2)
+                        +
+                (B=2): 2*(26^1)
+                        +
+                (C=3): 3*(26^0)
+
+                        = 53459
+
+            */
+
             ans+=(columnTitle.charAt(i)-'A'+1)*Math.pow(26,columnTitle.length()-i-1);
         }
         

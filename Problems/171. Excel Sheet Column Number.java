a97914b9dd22374c552d5d4554bf479ea4c73297
sushant-sinha <sinhasushant04@gmail.com>
2021-09-25 23:54:10 +0530

    new addition

---
+++ Problems/171. Excel Sheet Column Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        
+        int ans=0;
+                
+        for(int i=0;i<columnTitle.length();i++){
+            ans+=(columnTitle.charAt(i)-'A'+1)*Math.pow(26,columnTitle.length()-i-1);
+        }
+        
+        return ans;
+    }
+}

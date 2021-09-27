84647bc2760b6f84c8c54974122368273c117188
sushant-sinha <sinhasushant04@gmail.com>
2021-09-27 10:52:20 +0530

    new addition

--- Problems/1903. Largest Odd Number in String.java
+++ Problems/1903. Largest Odd Number in String.java
@@ -13,3 +13,18 @@ class Solution {
         return "";
     }
 }
+
+// best solution
+// 1ms ( 100% ) 39.7mb ( 82.10% )
+
+class Solution {
+    public String largestOddNumber(String num) {
+                
+        for(int i=num.length()-1;i>=0;i--){
+            if (num.charAt(i)%2==1)
+                return num.substring(0,i+1);
+        }
+        
+        return "";
+    }
+}

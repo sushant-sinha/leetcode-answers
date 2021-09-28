95059ca7fe861effc7c116144dafae5172341f3e
sushant-sinha <sinhasushant04@gmail.com>
2021-09-28 15:11:08 +0530

    new addition

--- Problems/292. Nim Game.java
+++ Problems/292. Nim Game.java
@@ -1,3 +1,13 @@
+// SUSHANT SINHA
+
+/*
+
+expanation:
+https://leetcode.com/problems/nim-game/discuss/1402101/100-JAVA-Solution-w-Explanation-on-How-to-find-pattern-for-these-types-of-questions
+
+*/
+// 0ms ( 100% ) 35.7mb ( 76.75% )
+
 class Solution {
     public boolean canWinNim(int n) {
         

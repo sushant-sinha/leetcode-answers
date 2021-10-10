9c55ca1e6bf71d91f0102aec1113c1c2e71fe12a
sushant-sinha <sinhasushant04@gmail.com>
2021-10-11 00:02:34 +0530

    new addition

---
+++ Problems/201. Bitwise AND of Numbers Range.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int count =0;
+        while(m!=n){
+            m>>=1;
+            n>>=1;
+            count+=1;
+        }
+        return m<<=count;
+    }
+}
+
+/*
+
+public int rangeBitwiseAnd(int left, int right) {
+	var rangeAnd = 0;
+	for (var i = 30; i >= 0; i--) {
+		var leftHighestSet = left & (1 << i);
+		if (leftHighestSet != (right & (1 << i)))
+			break;
+		rangeAnd |= leftHighestSet;
+	}
+	return rangeAnd;
+}
+
+*/

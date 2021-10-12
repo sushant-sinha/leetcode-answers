1a15f3b52d20853f3d59cf73bedafd15990d6640
sushant-sinha <sinhasushant04@gmail.com>
2021-10-12 15:40:38 +0530

    new addition

--- Problems/6. ZigZag Conversion.java
+++ Problems/6. ZigZag Conversion.java
@@ -45,3 +45,31 @@ class Solution {
         
     }
 }
+
+// best solution
+// 3ms ( 89.99% ) 42.1mb ( 34.38% )
+
+class Solution {
+    public String convert(String s, int numRows) {
+         int n = s.length();
+        if(numRows<=1 || numRows>n){
+            return s;
+        }
+        
+        int interval = 2*numRows-2;
+        char[] arr = new char[n];
+        int count =0;
+        for(int i = 0;i< numRows;i++){
+            int step = interval-2*i;
+            for(int j = i;j< n;j+=interval){
+                arr[count++] = s.charAt(j);
+                if(step >0 && step < interval && j+step<n){
+                    arr[count++] = s.charAt(j+step);
+                }
+            }
+        }
+        
+        return String.valueOf(arr);
+        
+    }
+}

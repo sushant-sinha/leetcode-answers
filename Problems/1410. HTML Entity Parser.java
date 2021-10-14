cbc19e2bbd78a96006f3716f8fa91bb9a21135d9
sushant-sinha <sinhasushant04@gmail.com>
2021-10-14 14:41:50 +0530

    new addition

---
+++ Problems/1410. HTML Entity Parser.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String entityParser(String text) {
+        
+        StringBuilder str=new StringBuilder(text);
+        
+        for(int i=0;i<str.length();i++){
+            
+            if(str.charAt(i)=='&'){
+                
+                if(i+4<=str.length()){
+                    
+                    if(str.substring(i+1,i+4).equals("gt;"))
+                        str.replace(i,i+4,">");
+                    
+                    else if(str.substring(i+1,i+4).equals("lt;"))
+                        str.replace(i,i+4,"<");
+                    
+                    else if(i+5<=str.length() && str.substring(i+1,i+5).equals("amp;"))
+                        str.replace(i,i+5,"&");
+                    
+                    else if(i+6<=str.length() && str.substring(i+1,i+6).equals("quot;"))
+                        str.replace(i,i+6,"\"");
+                    
+                    else if(i+6<=str.length() && str.substring(i+1,i+6).equals("apos;"))
+                        str.replace(i,i+6,"'");
+                    
+                    else if(i+7<=str.length() && str.substring(i+1,i+7).equals("frasl;"))
+                        str.replace(i,i+7,"/");
+                    
+                }
+                                
+            }
+            
+        }
+        
+        //System.out.println(str);
+        
+        return str.toString();
+        
+    }
+}

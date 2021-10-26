c9f2ac5c2f943f04f1c16b76e4d72ef0450a5296
sushant-sinha <sinhasushant04@gmail.com>
2021-10-26 13:35:40 +0530

    new addition

--- Problems/228. Summary Ranges.java
+++ Problems/228. Summary Ranges.java
@@ -1,5 +1,56 @@
 // SUSHANT SINHA
 
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        
+        List<String> ans=new ArrayList<>();
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int start=nums[i];
+            
+            //ans.add(""+start);
+            
+            StringBuilder s=new StringBuilder();
+            
+            int counter=0;
+            
+            while(i+counter+1<nums.length && nums[i+counter+1]==start+counter+1){
+                
+                counter++;
+                continue;
+                
+            }
+            
+            //System.out.println(start);
+            
+            if(counter!=0){
+                s.append(start);s.append("->");s.append(nums[i+counter]);
+                ans.add(s.toString());
+            }
+            
+            else{
+                ans.add(s.append(start).toString());
+            }
+            
+            i+=counter;
+            
+        }
+        
+        return ans;
+        
+    }
+}
+
+// original solution
+/*
+    IMPORTANT
+    
+    using + to concatenate string is the solwest method
+
+    USING StringBuilder's append method is the fastest
+
+*/
 // 6ms ( 77.45% ) 37.8mb ( 51.36% )
 
 class Solution {

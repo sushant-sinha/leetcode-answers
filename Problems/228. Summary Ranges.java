7a1fc2f72b25b230822b80120f8202394fcb5277
sushant-sinha <sinhasushant04@gmail.com>
2021-10-26 13:32:28 +0530

    new addition

---
+++ Problems/228. Summary Ranges.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        
+        List<String> ans=new ArrayList<>();
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int start=nums[i];
+            
+            //ans.add(""+start);
+            
+            int counter=0;
+            
+            while(i+counter+1<nums.length && nums[i+counter+1]==start+counter+1){
+                
+                counter++;
+                continue;
+                
+            }
+            
+            //System.out.println(start);
+            
+            if(counter!=0){
+                ans.add(start+"->"+nums[i+counter]);
+            }
+            
+            else{
+                ans.add(""+start);
+            }
+            
+            i+=counter;
+            
+        }
+        
+        return ans;
+        
+    }
+}

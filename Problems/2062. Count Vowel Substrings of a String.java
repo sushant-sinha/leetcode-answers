1c7849e5ed2e6ed2fc84c6be01d8eebd5f0df066
sushant-sinha <sinhasushant04@gmail.com>
2021-11-10 10:45:17 +0530

    new addition

--- Problems/2062. Count Vowel Substrings of a String.java
+++ Problems/2062. Count Vowel Substrings of a String.java
@@ -82,41 +82,45 @@ class Solution {
     }
 }
 
+// votrubac's (op) solution
+// https://leetcode.com/problems/count-vowel-substrings-of-a-string/discuss/1563737/Sliding-Window
 
-// class Solution {
-//     public int countVowelSubstrings(String word) {
+// 3ms ( 90.21% ) 37.1mb ( 96.39% )
 
-//     int j = 0, k = 0, vow = 0, cnt = 0;
+class Solution {
+    public int countVowelSubstrings(String word) {
 
-//     HashMap < Character, Integer > map = new HashMap < > ();
-//     map.put('a', 0);
-//     map.put('e', 0);
-//     map.put('i', 0);
-//     map.put('o', 0);
-//     map.put('u', 0);
+    int j = 0, k = 0, vow = 0, cnt = 0;
 
-//     for (int i = 0; i < word.length(); ++i) {
-//         if (map.get(word.charAt(i)) != null) {
-//             map.put(word.charAt(i), map.get(word.charAt(i)) + 1);
-//             if ((int) map.get(word.charAt(i)) == 1) {
-//                 ++vow;
-//             }
-//             while (vow == 5) {
-//                 map.put(word.charAt(k), map.get(word.charAt(k)) - 1);
-//                 if ((int) map.get(word.charAt(k)) == 0) {
-//                     --vow;
-//                 }
-//                 k++;
-//             }
-//             cnt = cnt + (k - j);
-//         } else {
-//             map.forEach((k1, v) -> {
-//                 map.put(k1, 0);
-//             });
-//             vow = 0;
-//             j = k = i + 1;
-//         }
-//     }
-//     return cnt;
-// }
-// }
+    HashMap < Character, Integer > map = new HashMap < > ();
+    map.put('a', 0);
+    map.put('e', 0);
+    map.put('i', 0);
+    map.put('o', 0);
+    map.put('u', 0);
+
+    for (int i = 0; i < word.length(); ++i) {
+        if (map.get(word.charAt(i)) != null) {
+            map.put(word.charAt(i), map.get(word.charAt(i)) + 1);
+            if ((int) map.get(word.charAt(i)) == 1) {
+                ++vow;
+            }
+            while (vow == 5) {
+                map.put(word.charAt(k), map.get(word.charAt(k)) - 1);
+                if ((int) map.get(word.charAt(k)) == 0) {
+                    --vow;
+                }
+                k++;
+            }
+            cnt = cnt + (k - j);
+        } else {
+            map.forEach((k1, v) -> {
+                map.put(k1, 0);
+            });
+            vow = 0;
+            j = k = i + 1;
+        }
+    }
+    return cnt;
+}
+}

532110c0c546cc57b30d25606f5936fe120ebaf1
sushant-sinha <sinhasushant04@gmail.com>
2021-11-13 20:06:36 +0530

    new addition

--- Problems/16. 3Sum Closest.java
+++ Problems/16. 3Sum Closest.java
@@ -29,3 +29,47 @@ class Solution {
         return ans;
     }
 }
+
+// best solution from the submissions tab
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int closest = findClosest(nums, i, target);
+            if (target == closest) {
+                return target;
+            }
+            if (Math.abs(target-closest) <= Math.abs(target-result)) {
+                result = closest;
+            }
+            while (i < nums.length - 2 && nums[i] == nums[i+1]) {
+                i++;
+            }
+        }
+        return result;
+    }
+    
+    private int findClosest(int[] nums, int index, int target) {
+        int i = index + 1;
+        int j = nums.length - 1;
+        int result = nums[index] + nums[i] + nums[j];
+        while (i < j) {
+            int closest = nums[index] + nums[i] + nums[j];
+            if (Math.abs(target-closest) <= Math.abs(target-result)) {
+                result = closest;
+            }
+            if (closest > target) {
+                j--;
+            }
+            else if (closest < target) {
+                i++;
+            }
+            else {
+                return target;
+            }
+        }
+        return result;
+    }
+}

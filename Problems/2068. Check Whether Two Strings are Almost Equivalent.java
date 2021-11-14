5c937ccd7c7c778993d2284d84aad91c73877777
sushant-sinha <sinhasushant04@gmail.com>
2021-11-14 23:16:02 +0530

    new addition

--- Problems/2068. Check Whether Two Strings are Almost Equivalent.java
+++ Problems/2068. Check Whether Two Strings are Almost Equivalent.java
@@ -1,6 +1,11 @@
+// SUSHANT SINHA
+
+// 0ms ( 100% ) 37.2mb ( 100% )
+
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
         
+        // for storing the frequency of the characters per word
         int fr[]=new int[26];
         
         for(char c:word1.toCharArray())

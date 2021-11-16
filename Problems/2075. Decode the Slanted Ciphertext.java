d10cbe7535bcab4224a200f338ad3768a32910d0
sushant-sinha <sinhasushant04@gmail.com>
2021-11-16 15:34:51 +0530

    new addition

--- Problems/2075. Decode the Slanted Ciphertext.java
+++ Problems/2075. Decode the Slanted Ciphertext.java
@@ -26,3 +26,35 @@ class Solution {
         
     }
 }
+
+// same as votrubac's solution
+// https://leetcode.com/problems/decode-the-slanted-ciphertext/discuss/1576914/Jump-Columns-%2B-1
+
+// using charAt instead of char array
+// 40ms ( 77.69% ) 49mb ( 71.63% )
+
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        
+        //System.out.println(encodedText.length());
+        
+        int colcount=encodedText.length()/rows;
+        
+        //char ar[]=encodedText.toCharArray();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<colcount;i++){
+            for(int j=i;j<encodedText.length();){
+                
+                //System.out.println(ar[j]);
+                sb.append(encodedText.charAt(j));
+                j+=colcount+1;
+                
+            }
+        }
+        // https://stackoverflow.com/questions/16974267/how-to-remove-only-trailing-spaces-of-a-string-in-java-and-keep-leading-spaces/16974310
+        return sb.toString().stripTrailing();
+        
+    }
+}

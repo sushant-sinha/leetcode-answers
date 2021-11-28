2fc88e5649caaa5ff13e551b938b69de498383c8
sushant-sinha <sinhasushant04@gmail.com>
2021-11-28 14:57:37 +0530

    new addition

--- Problems/2089. Find Target Indices After Sorting Array.java
+++ Problems/2089. Find Target Indices After Sorting Array.java
@@ -19,3 +19,55 @@ class Solution {
         
     }
 }
+
+// (should have used my brain previously😅 )
+// since the array is sorted... we should stop when a number greater than target is found
+
+// 1ms ( 85.71% ) 38.6mb ( 100% )
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        
+        List<Integer> ans=new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)
+                ans.add(i);
+            
+            else if(nums[i]>target)
+                break;
+            
+        }
+        
+        return ans;
+                
+        
+    }
+}
+
+// best solution from the submission tab
+// 0ms ( 100% ) 38.8mb ( 100% )
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> result = new ArrayList<>();
+        
+        int tCount = 0;
+        int start = 0;
+        
+        for (int num : nums){
+            if (num == target)
+                ++tCount;
+            else if (num < target)
+                ++start;
+        }
+        
+        while (tCount-- > 0){
+            result.add(start++);
+        }
+        
+        return result;
+    }
+}

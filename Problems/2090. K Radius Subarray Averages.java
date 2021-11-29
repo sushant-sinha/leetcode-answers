6afae9b574ac518ae9f2a1b257702f9a46f18a4b
sushant-sinha <sinhasushant04@gmail.com>
2021-11-30 00:06:02 +0530

    new addition

--- Problems/2090. K Radius Subarray Averages.java
+++ Problems/2090. K Radius Subarray Averages.java
@@ -1,5 +1,29 @@
 // SUSHANT SINHA
 
+// from discuss section
+// 10ms ( 100% ) 58.5mb ( 60.00% )
+
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n =  nums.length;
+        long sum = 0;
+        int[] res = new int[n];
+        Arrays.fill(res,-1);
+        int left = 0, right = 0, windowSize = 2*k+1;
+        while( right  < n ){
+            sum+=nums[right];
+            if(right - left + 1 == windowSize){
+                res[left+k] = (int) (sum/windowSize);
+                sum-=nums[left];
+                left++;
+            }
+            right++;
+        }      
+        return res;
+    }
+}
+
+
 // my code...works for 35/36 test cases
 
 

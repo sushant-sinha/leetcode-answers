a83ac0a25b458dcfac864cc90757abfa8807786d
sushant-sinha <sinhasushant04@gmail.com>
2021-11-30 00:05:36 +0530

    new addition

---
+++ Problems/2090. K Radius Subarray Averages.java
@@ -0,0 +1,61 @@
+
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        
+        //System.out.println(nums.length);
+        
+        long sum=0;
+        int ans[]=new int[nums.length];
+        
+        int pointer=k, window=(2*k)+1;
+        
+        for(int i=0;i<k && i<nums.length ;i++){
+            ans[i]=-1;
+            ans[nums.length-1-i]=-1;
+        }
+        
+        int i=0;
+        
+        for(;i<=k;i++){
+                        
+            if(i>nums.length){
+                Arrays.fill(ans,-1);
+                return ans;
+            }
+            
+            sum+=nums[i];
+            
+        }
+        
+        for(;i<=2*k;i++){
+            
+            if(i>nums.length-1){
+                Arrays.fill(ans,-1);
+                return ans;
+            }
+            
+            sum+=nums[i];
+            
+        }
+        
+        
+        
+        //System.out.println(sum);
+        
+        
+        if(pointer<nums.length)
+            ans[pointer]=(int)Math.floor((sum/window));
+        
+        //System.out.println(ans[pointer]);
+        
+        for(;i<nums.length;i++){
+            
+            sum+=nums[i];
+            sum-=nums[pointer-k];
+            ans[++pointer]=(int)(sum/window);
+        }        
+        
+        return ans;
+        
+    }
+}

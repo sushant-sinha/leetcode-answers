74736442ec2cae4f5ef4910a884c302a1f47134d
sushant-sinha <sinhasushant04@gmail.com>
2021-12-04 18:53:25 +0530

    new addition

---
+++ Problems/2053. Kth Distinct String in an Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        
+        ArrayList<String> seen=new ArrayList<>();
+        
+        ArrayList<String> distinct=new ArrayList<>();
+        
+        for(String s:arr){
+            
+            if(seen.contains(s)){
+                distinct.remove(s);
+            }
+            
+            else{
+                distinct.add(s);
+                seen.add(s);
+            }
+            
+        }
+        
+        if(distinct.size()<k)
+            return "";
+        
+        else
+            return distinct.get(k-1);
+        
+    }
+}

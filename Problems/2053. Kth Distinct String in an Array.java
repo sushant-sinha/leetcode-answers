fb2b3ee2e95c804984ab5af29dd9746cbea3e393
sushant-sinha <sinhasushant04@gmail.com>
2021-12-04 18:56:58 +0530

    new addition

--- Problems/2053. Kth Distinct String in an Array.java
+++ Problems/2053. Kth Distinct String in an Array.java
@@ -1,6 +1,6 @@
 // SUSHANT SINHA
 
-// 59ms ( 8.11% ) 38.3mb ( 99.72% )
+// 59ms ( 8.11% ) 38.3mb ( 99.72% ) or { 57ms ( 8.35% ) 38.4mb ( 96.56% )}
 
 class Solution {
     public String kthDistinct(String[] arr, int k) {
@@ -28,5 +28,29 @@ class Solution {
         else
             return distinct.get(k-1);
         
+    }
+}
+
+// best solution from submission tab
+// 3ms ( 100% ) 38.7mb ( 82.67% )
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        boolean[] distinct = new boolean[arr.length];
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                distinct[i] = false;
+                distinct[map.get(arr[i])] = false;
+            } else {
+                distinct[i] = true;
+                map.put(arr[i], i);
+            }
+        }
+        for (int i = 0; i < distinct.length; i++) {
+            if (distinct[i]) k--;
+            if (k == 0) return arr[i];
+        }
+        return "";
     }
 }

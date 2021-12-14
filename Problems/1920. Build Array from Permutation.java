3a51e6257fdf397cb91e946e208c7998b7bdc87a
sushant-sinha <sinhasushant04@gmail.com>
2021-12-15 00:15:51 +0530

    new addition

--- Problems/1920. Build Array from Permutation.java
+++ Problems/1920. Build Array from Permutation.java
@@ -1,6 +1,6 @@
 // SUSHANT SINHA
 
-// 1ms 39.5mb (not enough accepted submission to show distribution)
+// 1ms ( 99.96%) 39.5mb ( 72.66%  )
 
 class Solution {
     public int[] buildArray(int[] nums) {
@@ -12,5 +12,30 @@ class Solution {
         
         return ans;
         
+    }
+}
+
+// best solution
+// 1ms ( 99.96% ) 52mb ( 6.07% )
+
+class Solution {
+    int []  recMeth(int[] retArr, int [] nums, int index ){
+        int len  =  nums.length;
+        if(index<len){
+             retArr[index]  =  nums[nums[index]];
+            index++;
+          retArr  = recMeth(retArr, nums, index);
+        }
+        return retArr;
+    }
+    public int[] buildArray(int[] nums) {
+        int len  =  nums.length;
+        int [] retArr =  new int[len];
+        // for(int i=0;i<len;i++)
+        //     {
+        //         retArr[i]  =  nums[nums[i]];
+        //     }
+        retArr = recMeth(retArr, nums, 0);
+        return retArr;
     }
 }

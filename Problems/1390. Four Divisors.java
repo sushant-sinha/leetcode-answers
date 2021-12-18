bf6d4259f0b13a95a76136df0b6aac6800f9fd7c
sushant-sinha <sinhasushant04@gmail.com>
2021-12-18 13:19:19 +0530

    new addition

--- Problems/1390. Four Divisors.java
+++ Problems/1390. Four Divisors.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 89ms ( 22.07% ) 28.9mb ( 88.28% )
+
 class Solution {
     public int sumFourDivisors(int[] nums) {
         
@@ -8,17 +12,20 @@ class Solution {
             
             int divcount=0,tempsum=0;
 
+            // finding divisors till i/2 gives tle.... so iterate till the square root
+
             for(int j=1;j<=Math.sqrt(i);j++){
                 
                 if(i%j==0){
-
+                    
                     tempsum+=j;
 
+                    // if j is the square root... we will get the same number two times.. so we need to use an if condition to recitify this situation
                     if(j*j!=i){
                         tempsum+=i/j;
                         divcount++;
                     }
-                    
+
                     divcount++;
                 }
                 

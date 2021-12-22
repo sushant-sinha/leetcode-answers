304ddb701aef14e4f01ba5a88c007bbe90f43453
sushant-sinha <sinhasushant04@gmail.com>
2021-12-23 00:04:52 +0530

    new addition

---
+++ Problems/143. Reorder List.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// to do... below solution was presented by https://leetcode.com/ashu25/
+
+// https://leetcode.com/problems/reorder-list/discuss/1640806/Javaor2-Approach-or-2-Pointer-approach
+// https://leetcode.com/problems/reorder-list/discuss/1640597/C%2B%2BJavaPython-2-Solutions-oror-Visualized-Explanation-oror-Beginner-Friendly
+

3eb6eea16c3e995edeb9ebf58c94639335b1bac8
sushant-sinha <sinhasushant04@gmail.com>
2021-12-27 18:49:20 +0530

    new addition

---
+++ Problems/2114. Maximum Number of Words Found in Sentences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        
+        int ans=0;
+        
+        for(String s:sentences){
+            
+            String temp[]=s.split(" ");
+            
+            ans = ans > temp.length ? ans : temp.length;
+            
+        }
+        
+        return ans;
+        
+    }
+}

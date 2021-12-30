2ca63677ef22d5ee0c941c2c7ab02d98717d37b5
sushant-sinha <sinhasushant04@gmail.com>
2021-12-30 12:15:31 +0530

    new addition

--- Problems/2120. Execution of All Suffix Instructions Staying in a Grid.java
+++ Problems/2120. Execution of All Suffix Instructions Staying in a Grid.java
@@ -54,3 +54,57 @@ class Solution {
         
     }
 }
+
+
+// inconsistent runtime ... don't know why ¯\_(ツ)_/¯
+// 26ms ( 84.48% ) 39.3mb ( 90.74% )
+
+class Solution {
+    public int[] executeInstructions(int n, int[] startPos, String s) {
+        
+        int ans[]=new int[s.length()];
+        
+        for(int i=0;i<s.length();i++){
+            
+            int x=startPos[0]; int y=startPos[1];
+            
+            for(int j=i;j<s.length();j++){
+                
+                switch(s.charAt(j)){
+                        
+                    case 'R':
+                        y++;
+                        break;
+                        
+                    case 'L':
+                        y--;
+                        break;
+                    
+                    case 'U':
+                        x--;
+                        break;
+                        
+                    case 'D':
+                        x++;
+                        break;
+                        
+                };
+                
+                //System.out.println(Arrays.toString(pos));
+                
+                
+                if(x==n || x==-1 || y==n || y==-1){
+                    break;
+                }
+                
+                else
+                    ans[i]++;
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

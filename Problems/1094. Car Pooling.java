b6e2268367bc910358c604490eea20a67c65a444
sushant-sinha <sinhasushant04@gmail.com>
2022-01-07 00:45:33 +0530

    new addition

---
+++ Problems/1094. Car Pooling.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {        
+        
+        int passcount[]=new int[1001];
+        
+        for(int i[]:trips){
+            
+            passcount[i[1]] += i[0];
+            passcount[i[2]] -= i[0];
+            
+        }
+        
+        int curpass=0;
+        
+        for(int i:passcount){
+            
+            curpass+=i;
+            
+            if(curpass>capacity)
+                return false;
+            
+        }
+        
+        return true;
+        
+    }
+}

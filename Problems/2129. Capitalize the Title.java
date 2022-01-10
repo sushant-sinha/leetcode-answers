e2f0d2bd13ea4d4feac669af54e6e3ca71df6319
sushant-sinha <sinhasushant04@gmail.com>
2022-01-10 14:15:00 +0530

    new addition

---
+++ Problems/2129. Capitalize the Title.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+
+        StringBuffer ans=new StringBuffer();
+        
+        for(String s: title.split(" ")){
+
+            StringBuffer temp=new StringBuffer();
+
+            if(s.length()<=2){
+
+                for(char c:s.toCharArray()){
+
+                    if(c>=65 && c<=91)
+                        temp.append((char)(c+32));
+
+                    else
+                        temp.append(c);
+
+                }
+
+            }
+
+            else{
+
+                if(s.charAt(0)>=65 && s.charAt(0)<=91)
+                    temp.append(s.charAt(0));
+
+                else
+                    temp.append((char)(s.charAt(0)-32));
+
+                for(int i=1;i<s.length();i++){
+
+                    if(s.charAt(i)>=65 && s.charAt(i)<=91)
+                        temp.append((char)(s.charAt(i)+32));
+
+                    else
+                        temp.append(s.charAt(i));
+
+                }
+
+            }
+            
+            ans.append(temp+" ");
+
+        }
+        
+        
+
+
+        return String.valueOf(ans).substring(0,title.length());
+
+    }
+}

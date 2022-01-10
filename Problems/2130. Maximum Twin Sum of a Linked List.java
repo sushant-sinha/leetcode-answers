3286923316b05799b36a8b966c41b7fcba0cf4e1
sushant-sinha <sinhasushant04@gmail.com>
2022-01-11 00:55:47 +0530

    new addition

--- Problems/2130. Maximum Twin Sum of a Linked List.java
+++ Problems/2130. Maximum Twin Sum of a Linked List.java
@@ -2,6 +2,27 @@
 
 // 20ms ( 14.29% ) 126.1mb ( 14.29% )
 
+/* 
+
+to find the mid of the linkedlist....
+
+i will make a pointer which traverses the linkedlist with x2 speed
+meanwhile... another pointer whill traverse the list with x1 speed and store the visited node's value in an arraylist
+
+when the speedX2 pointer reaches the end.. this means the speed pointer is currently in the middle of the list..
+
+---------------
+
+from now on... we will make another while loop..
+it will use the speed pointer to traverse the remaining half of the linkedlist
+
+simulataneously when the speed pointer traverses the second half.. we will generate the sum by adding speed.val with the reverse arraylist
+
+return the max sum
+
+*/
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {

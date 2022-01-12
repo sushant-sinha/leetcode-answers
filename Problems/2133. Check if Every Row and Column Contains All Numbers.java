2be1fa8781fd353ba4bbdff35dcc5da9ca3829b8
sushant-sinha <sinhasushant04@gmail.com>
2022-01-13 00:16:31 +0530

    new addition

--- Problems/2133. Check if Every Row and Column Contains All Numbers.java
+++ Problems/2133. Check if Every Row and Column Contains All Numbers.java
@@ -53,3 +53,35 @@ class Solution {
         
     }
 }
+
+// optimal solution
+
+// 4ms ( 97.18% ) ( claims to be 1ms ) 83.9mb ( 79.58% )
+
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int len = matrix.length, xor = 0;
+        for(int i = 1; i <= len; i++){
+            xor ^= i;
+        }
+        for(int[] arr : matrix){
+            int x = xor;
+            for(int n : arr){
+                x ^= n;
+            }
+            if(x != 0){
+                return false;
+            }
+        }
+        for(int c = 0; c < len; c++){
+            int x = xor;
+            for(int r = 0; r < len; r++){
+                x ^= matrix[r][c];
+            }
+            if(x != 0){
+                return false;
+            }
+        }
+        return true;    
+    }
+}

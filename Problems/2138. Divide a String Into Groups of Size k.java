0c7b867c76ce803e4cd1f54982432e483c077540
sushant-sinha <sinhasushant04@gmail.com>
2022-01-17 16:50:26 +0530

    new addition

---
+++ Problems/2138. Divide a String Into Groups of Size k.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        
+        String ans[]=new String[(s.length()%k==0 ? s.length()/k : ((s.length()/k)+1))];
+        
+        int arIndex=0;
+        
+        for(int i=0;i<ans.length;i++){
+            
+            StringBuffer temp=new StringBuffer();
+            
+            int start=k*i, limit=start+k;
+            
+            while(start<s.length() && start<limit){
+                temp.append(s.charAt(start));
+                start++;
+            }
+            
+            ans[i]=String.valueOf(temp);
+            
+        }
+        
+        if(s.length()%k!=0){
+            
+            StringBuffer temp;;
+            
+            while(ans[ans.length-1].length()!=k){
+                
+                temp=new StringBuffer();
+                
+                temp.append(ans[ans.length-1]);
+                temp.append(fill);
+                ans[ans.length-1]=String.valueOf(temp);
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

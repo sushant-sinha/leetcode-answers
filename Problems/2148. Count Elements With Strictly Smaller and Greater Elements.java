272791b9f211541b0b3695694b9da7ccc1c3466a
sushant-sinha <sinhasushant04@gmail.com>
2022-01-27 06:40:12 +1400

    new addition

---
+++ Problems/2148. Count Elements With Strictly Smaller and Greater Elements.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countElements(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int ans=0;
+        boolean small=false, large=false;
+        
+        for(int i=1; i<nums.length-1;i++){
+            
+            //int cur=nums[i];
+            
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i])
+                    small=true;
+            }
+            
+            for(int j=nums.length-1;j>i;j--){
+                if(nums[j]>nums[i])
+                    large=true;
+            }
+            
+            if(large && small)
+                ans++;
+            
+            large=false;
+            small=false;
+            
+            
+        }
+        
+        return ans;
+        
+    }
+}

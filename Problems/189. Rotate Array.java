ce6a39c71e404ef52b0377873be22adcd3fe132f
sushant-sinha <sinhasushant04@gmail.com>
2022-01-31 00:05:19 +0530

    new addition

--- Problems/189. Rotate Array.java
+++ Problems/189. Rotate Array.java
@@ -1,3 +1,19 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
+}
+
 // below solution worked on 23-december-2020 (410ms), but now gives TLE
 
 class Solution {

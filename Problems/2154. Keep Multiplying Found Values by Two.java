13ea70b88eb26e0e7a7ecb09ec4f0af89fd9a5ca
sushant-sinha <sinhasushant04@gmail.com>
2022-02-03 01:04:50 +1400

    new addition

--- Problems/2154. Keep Multiplying Found Values by Two.java
+++ Problems/2154. Keep Multiplying Found Values by Two.java
@@ -1,5 +1,7 @@
 // SUSHANT SINHA
 
+// last solution has the best runtime
+
 // 2ms ( 87.13% ) 44.1mb ( 76.82% )
 
 class Solution {
@@ -42,5 +44,19 @@ class Solution {
         
         return original;
         
+    }
+}
+
+// recursion
+// 1ms ( 96.99% ) 44.5mb ( 62.39% )
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == original) {
+                return findFinalValue(nums, original*2);
+            }
+        }
+        return original;
     }
 }

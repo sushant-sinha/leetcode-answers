91b5af11c396b26f918be08417a6fb8cea18a012
sushant-sinha <sinhasushant04@gmail.com>
2022-02-06 22:07:05 +0530

    new addition

---
+++ Problems/2164. Sort Even and Odd Indices Independently.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        
+        ArrayList<Integer> temp=new ArrayList<>();
+        
+        int index=0;
+        
+        // for indices
+        for(int i=1;i<nums.length;i++){
+            
+            temp.add(nums[i]);
+            i++;
+            
+        }
+        
+        Collections.sort(temp, Collections.reverseOrder());
+        
+        for(int i=1;i<nums.length;i++){
+            
+            nums[i]=temp.get(index++);
+            i++;
+            
+        }
+        
+        temp.clear();
+        
+        // for even indices
+        
+        index=0;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            temp.add(nums[i]);
+            i++;
+            
+        }
+        
+        Collections.sort(temp);
+        
+        for(int i=0;i<nums.length;i++){
+            
+            nums[i]=temp.get(index++);
+            i++;
+            
+        }
+        
+        return nums;
+        
+    }
+}

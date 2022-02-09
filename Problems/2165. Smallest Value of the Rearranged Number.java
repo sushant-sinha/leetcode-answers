828a1700a453de3b79ac219bb882e072707496ee
sushant-sinha <sinhasushant04@gmail.com>
2022-02-09 21:51:27 +0530

    new addition

---
+++ Problems/2165. Smallest Value of the Rearranged Number.java
@@ -0,0 +1,46 @@
+class Solution {
+    public long smallestNumber(long num) {
+        
+        if(num==0)
+            return num;
+        
+        String s=Long.toString(num);
+        
+        char ar[]=s.toCharArray();
+        
+        Arrays.sort(ar);
+        
+        int i=0;
+        
+        if(ar[0]=='-'){
+            
+            // reusing variable s
+            
+            s="";
+            i=ar.length-1;
+            
+            for(;i>0;i--)
+                s+=ar[i];
+            
+            s='-'+s;
+            
+            return Long.parseLong(s);
+            
+        }
+        
+        if(ar[0]=='0'){
+            
+            while(i<ar.length && ar[i]=='0')
+                i++;
+            
+            ar[0]=ar[i];
+            ar[i]='0';
+            
+            return Long.parseLong(new String(ar));
+        }
+        
+        else
+            return Long.parseLong(new String(ar));
+        
+    }
+}

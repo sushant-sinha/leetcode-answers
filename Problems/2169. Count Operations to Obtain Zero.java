942fe22e86e22bb2b7f6e15bf54212926313d222
sushant-sinha <sinhasushant04@gmail.com>
2022-02-14 00:10:15 +0530

    new addition

--- Problems/2169. Count Operations to Obtain Zero.java
+++ Problems/2169. Count Operations to Obtain Zero.java
@@ -1,9 +1,16 @@
+// SUSHANT SINHA
+
+// 2ms ( 33.33% ) 41.2mb ( 33.33% )
+
 class Solution {
     public int countOperations(int num1, int num2) {
         
         if(num1==0 || num2==0)
             return 0;
         
+        if(num1==num2)
+            return 1;
+
         // making num1 to always have the bigger value
         if(num1<num2){
             int temp=num2;
@@ -12,9 +19,6 @@ class Solution {
         }
         
         //System.out.println(num1+" "+num2);
-        
-        if(num1==num2)
-            return 1;
         
         if(num1%num2==0)
             return num1/num2;

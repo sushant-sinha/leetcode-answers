0445ce8cf5f843b98a9a3e7b4d90c5b9d78784c9
sushant-sinha <sinhasushant04@gmail.com>
2022-02-19 22:48:48 +0530

    new addition

---
+++ Problems/2176. Count Equal and Divisible Pairs in an Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        
+        int ans=0;
+        
+        for(int i=0;i<nums.length-1;i++){
+            
+            for(int j=i+1;j<nums.length;j++){
+                
+                if(nums[i]==nums[j] && (i*j)%k==0)
+                    ans++;
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

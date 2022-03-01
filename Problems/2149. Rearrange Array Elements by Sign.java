12224acc307e92827120dec528c16a94c04f86c1
sushant-sinha <sinhasushant04@gmail.com>
2022-03-01 10:59:11 +0530

    new addition

---
+++ Problems/2149. Rearrange Array Elements by Sign.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        
+        int pos[]=new int[nums.length/2];
+        int neg[]=new int[nums.length/2];
+        
+        int posIndex=0, negIndex=0;
+        
+        for(int i:nums){
+            
+            if(i>0){
+                pos[posIndex++]=i;
+            }
+            
+            else{
+                neg[negIndex++]=i;
+            }
+            
+        }
+        
+        posIndex=0; negIndex=0;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            nums[i]=pos[posIndex++];
+            nums[++i]=neg[negIndex++];
+            
+        }
+
+        // instead of the above for loop, could have used this
+        
+        /*
+        for (int i =0; i < nums.length/2; i++) {
+            nums[i * 2] = p[i];
+            nums[(i * 2) + 1] = n[i];
+        }
+        */
+        
+        return nums;
+        
+    }
+}

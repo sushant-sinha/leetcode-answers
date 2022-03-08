d5ed1190a300f6e7304eddc4d96fe5799142999e
Sushant Sinha <sinhasushant04@gmail.com>
2022-03-08 07:24:46 +0530

    new addition

---
+++ Problems/2190. Most Frequent Number Following Key In an Array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int mostFrequent(int[] nums, int key) {
+        
+        int ans=0;
+        
+        HashMap<Integer, Integer> counter=new HashMap<>();
+        
+        for(int i=0;i<nums.length-1;i++){
+            
+            if(nums[i]==key){
+                
+                if(counter.containsKey(nums[i+1])){
+                    counter.put(nums[i+1],counter.get(nums[i+1])+1);
+                }
+                
+                else{
+                    counter.put(nums[i+1],1);
+                }
+            }
+            
+        }
+        
+        int min=0;
+        
+        // code for traversing the HashMap: https://www.geeksforgeeks.org/iterate-map-java/
+        for(Map.Entry<Integer, Integer> m: counter.entrySet()){
+            if(m.getValue()>min){
+                min=m.getValue();
+                ans=m.getKey();
+            }
+        }
+        
+        return ans;
+        
+    }
+}

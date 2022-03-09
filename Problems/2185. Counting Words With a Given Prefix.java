adc7ca6a111e2e96a542edac75c5f8352e364456
sushant-sinha <sinhasushant04@gmail.com>
2022-03-10 03:31:05 +1400

    new addition

--- Problems/2185. Counting Words With a Given Prefix.java
+++ Problems/2185. Counting Words With a Given Prefix.java
@@ -32,3 +32,39 @@ class Solution {
         
     }
 }
+
+
+// when initializing the flag variable in every iteration
+// 3ms ( 16.67% ) 43.5mb ( 50.00% )
+
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        
+        int ans=0, len=pref.length();
+        
+        // removed the flag variable and is being initialzed in the for loop every iteration
+        // initializing the flag variable every time in the for loop itself gives better speed
+
+        for(String s:words){
+            
+            boolean flag=true;
+            
+            if(s.length()<len)
+                continue;
+            
+            for(int i=0;i<len;i++){
+                if(s.charAt(i)!=pref.charAt(i)){
+                    flag=false;
+                    break;
+                }
+            }
+            
+            if(flag)
+                ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

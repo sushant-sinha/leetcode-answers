4c3dc1edf520709ab0d2426b4e0895e00ef16ccc
sushant-sinha <sinhasushant04@gmail.com>
2022-03-21 16:51:59 +0530

    new addition

---
+++ Problems/202. Happy Number.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        int temp=0;
+        
+        ArrayList<Integer> alreadyVisited=new ArrayList<>();
+        
+        alreadyVisited.add(n);
+        
+        while(n!=1){
+            
+            temp=calcSum(n);
+            
+            if(alreadyVisited.contains(temp))
+                return false;
+            
+            else
+                alreadyVisited.add(temp);
+            
+            n=temp;
+        }
+        
+        return true;
+        
+    }
+    
+    int calcSum(int n){
+        
+        int sum=0;
+        
+        while(n!=0){
+            
+            sum+=Math.pow(n%10,2);
+            n/=10;
+            
+        }
+        
+        return sum;
+    }
+}

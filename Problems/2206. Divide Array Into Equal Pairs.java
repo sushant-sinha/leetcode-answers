aa6e9b65398f383c18f11a9f9aa16af396ce4164
sushant-sinha <sinhasushant04@gmail.com>
2022-03-23 19:24:33 +0530

    new addition

---
+++ Problems/2206. Divide Array Into Equal Pairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        
+        int n=nums.length;
+        
+        int counter[][]=new int[501][1];
+        
+        for(int i:nums){
+            
+            counter[i][0]++;
+            
+        }
+        
+        for(int i[]:counter){
+            
+            if(i[0]%2!=0)
+                return false;
+            
+        }
+        
+        return true;
+        
+    }
+}

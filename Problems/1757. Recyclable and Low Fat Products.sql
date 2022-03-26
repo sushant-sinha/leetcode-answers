8a4ce1faa9d2304bf5ef93b6b83108c3145b6ad4
sushant-sinha <sinhasushant04@gmail.com>
2022-03-26 15:42:21 +0530

    new addition

--- Problems/1757. Recyclable and Low Fat Products.sql
+++ Problems/1757. Recyclable and Low Fat Products.sql
@@ -1,2 +1,7 @@
+# SUSHANT SINHA
+
+# 463ms ( 77.77% )
+# metrics of runtime isn't constant and cannot be relied on
+
 # Write your MySQL query statement below
 select product_id from products where recyclable='Y' and low_fats='Y';

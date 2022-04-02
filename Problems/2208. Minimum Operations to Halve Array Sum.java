006e94273c5bd4841715555ea5681747376777c7
sushant-sinha <sinhasushant04@gmail.com>
2022-04-02 11:30:20 +0530

    new addition

--- Problems/2208. Minimum Operations to Halve Array Sum.java
+++ Problems/2208. Minimum Operations to Halve Array Sum.java
@@ -1,6 +1,11 @@
+// SUSHANT SINHA
+
+// 200ms ( 92.80% ) 59.2mb ( 90.88% )
+
 class Solution {
     public int halveArray(int[] nums) {
         
+        // defining reverse order to maintain the maximum number at the head
         PriorityQueue<Double> pq=new PriorityQueue<>(Collections.reverseOrder());
         
         double tot=0;
@@ -18,18 +23,18 @@ class Solution {
         
         while(cursum>tot/2){
             
+            // get the maximum number (optimal)
             double max=pq.poll();
             
             max/=2;
             
+            // put back the max number divided by 2 in the queue
             pq.add(max);
             
             cursum-=max;
             ans++;
             
         }
-        
-        
         
         return ans;
         

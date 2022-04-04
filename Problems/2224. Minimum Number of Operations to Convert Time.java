6187b007bd26da9e1ee68419807c78c4d3107f68
sushant-sinha <sinhasushant04@gmail.com>
2022-04-05 00:04:56 +0530

    new addition

---
+++ Problems/2224. Minimum Number of Operations to Convert Time.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int convertTime(String current, String correct) {
+        
+        int timeCurrent=convertToMinutes(current);
+        int timeCorrect=convertToMinutes(correct);
+        
+        int diff=Math.abs(timeCurrent-timeCorrect);
+        
+        int ans=0;
+        
+        while(diff!=0){
+            
+            if(diff>=60)
+                diff-=60;
+            
+            else if(diff>=15)
+                diff-=15;
+            
+            else if(diff>=5)
+                diff-=5;
+            
+            else if(diff>=1)
+                diff-=1;
+            
+            ans++;
+        }
+        
+        return ans;
+        
+        
+    }
+    
+    int convertToMinutes(String s){
+        
+        int hour=Integer.parseInt(s.substring(0,2))*60;
+        int min=Integer.parseInt(s.substring(3,5));
+        
+        return hour+min;
+        
+    }
+}

b9b29d9505cb38320be01f24343d92fec80a3137
sushant-sinha <sinhasushant04@gmail.com>
2022-04-08 07:11:40 +1400

    new addition

---
+++ Problems/2221. Find Triangular Sum of an Array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        
+        ArrayList<Integer> temp=new ArrayList<>();
+        
+        for(int i : nums){
+            temp.add(i);
+        }
+        
+        while(temp.size()!=1){
+            
+            temp=generator(temp);
+            
+        }
+        
+        return temp.get(0);
+        
+    }
+    
+    ArrayList<Integer> generator(ArrayList<Integer> var){
+        
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        for(int i=0;i<var.size()-1;i++){
+            
+            ans.add( (var.get(i)+var.get(i+1))%10 );
+            
+        }
+        
+        
+        return ans;
+        
+    }
+    
+}

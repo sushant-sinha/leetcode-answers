92482bf5fb7605707cb15bd924174c1793009692
sushant-sinha <sinhasushant04@gmail.com>
2022-04-14 00:58:25 +0530

    new addition

---
+++ Problems/2810. Faulty Keyboard.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String finalString(String s) {
+        
+        StringBuffer sb=new StringBuffer();
+        for(char c:s.toCharArray()){
+            if(c=='i'){
+                sb.reverse();
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+        
+    }
+}

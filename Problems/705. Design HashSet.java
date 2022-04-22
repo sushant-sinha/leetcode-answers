a95d7a854ac8aa4016f497a4294b2c951741a5cc
sushant-sinha <sinhasushant04@gmail.com>
2022-04-22 16:49:48 +0530

    new addition

---
+++ Problems/705. Design HashSet.java
@@ -0,0 +1,38 @@
+class MyHashSet {
+    
+    List<Integer> arr;
+    
+    public MyHashSet() {
+        
+        arr=new ArrayList<Integer>();
+    }
+    
+    public void add(int key) {
+        
+        if(!arr.contains(key)){
+            arr.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        
+        int index=arr.indexOf(key);
+        
+        if(index!=-1){
+            arr.remove(index);
+        }
+    }
+    
+    public boolean contains(int key) {
+        
+        return arr.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

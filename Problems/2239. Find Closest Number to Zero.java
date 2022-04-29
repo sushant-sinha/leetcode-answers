21fdb4f9a9ba78901f82d319f09f3829955921b0
sushant-sinha <sinhasushant04@gmail.com>
2022-04-29 21:01:46 +0200

    new addition

--- Problems/2239. Find Closest Number to Zero.java
+++ Problems/2239. Find Closest Number to Zero.java
@@ -1,3 +1,16 @@
+// SUSHANT SINHA
+
+// 7ms ( 10.87% ) 51.5mb ( 20% )
+
+/*
+
+tried using something other than a basic for loop iteration (backfired as it had worse performance)
+
+sorting the array and then iterating it from the maximum value (because we need the maximum value with the least distance)
+when we see that the distance has increased from that of the previous element, we will break, since the array is sorted and going down the array will only lead to increase in the distance from 0
+
+*/
+
 class Solution {
     public int findClosestNumber(int[] nums) {
         
@@ -37,5 +50,27 @@ class Solution {
         
         return ans[0];
         
+    }
+}
+
+// (simple) best solution from the submission tab
+// 1ms ( 100% ) 42mb ( 91.12% )
+
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int ans=Integer.MIN_VALUE;
+        int minDiff=Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            int currDiff=Math.abs(nums[i]);
+            
+            if(currDiff<minDiff){
+                minDiff=currDiff;
+                ans=nums[i];
+            }else if(currDiff==minDiff){
+                ans=Math.max(ans,nums[i]);
+            }
+        }
+        return ans;
     }
 }

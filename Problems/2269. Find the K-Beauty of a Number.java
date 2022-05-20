a42e4baf3eef4cf6470e5954b2be5e301c588e04
sushant-sinha <sinhasushant04@gmail.com>
2022-05-20 21:31:40 +0530

    new addition

---
+++ Problems/2269. Find the K-Beauty of a Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        
+        int ans=0;
+        
+        String s=""+num;
+        
+        for(int i=0;i<=s.length()-k;i++){
+            
+            String sub=s.substring(i,i+k);
+            
+            // System.out.println(sub);
+            
+            int n=Integer.parseInt(sub);
+            
+            if(n!=0 && num%n==0)
+                ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

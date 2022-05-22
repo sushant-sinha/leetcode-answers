57320c4b968c095adcc9be61ca9dca68457cc809
sushant-sinha <sinhasushant04@gmail.com>
2022-05-22 23:53:02 +0530

    new addition

--- Problems/1302. Deepest Leaves Sum.java
+++ Problems/1302. Deepest Leaves Sum.java
@@ -1,3 +1,23 @@
+// do again (because it uses poll function)
+// https://leetcode.com/problems/deepest-leaves-sum/discuss/463239/JavaC%2B%2BPython-Level-Traversal
+
+// 12ms ( 18.92% ) 58.8mb ( 32.09% )
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
      

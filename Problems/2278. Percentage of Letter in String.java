4176d9f84326e57c0796e46b9785425c71d8bb2e
sushant-sinha <sinhasushant04@gmail.com>
2022-05-24 00:00:55 +0530

    new addition

--- Problems/2278. Percentage of Letter in String.java
+++ Problems/2278. Percentage of Letter in String.java
@@ -1,16 +1,15 @@
+// SUSHANT SINHA
+
+// 0ms ( 100% ) 41.6mb ( 50.00% )
+
 class Solution {
     public int percentageLetter(String s, char letter) {
         
         int counter=0;
         
-        for(char c:s.toCharArray()){
-            
-            System.out.println(c==letter);
-            
+        for(char c:s.toCharArray())
             if(c==letter)
                 counter++;
-            
-        }
         
         return counter*100/s.length();
         

d3b2c6e1e9a23cd5ea92b032bbdc0342da8dafc4
sushant-sinha <sinhasushant04@gmail.com>
2022-05-27 19:55:42 +0530

    new addition

---
+++ Problems/2161. Partition Array According to Given Pivot.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        
+        ArrayList<Integer> less=new ArrayList<>();
+        ArrayList<Integer> more=new ArrayList<>();
+        
+        int[] ans=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            
+            if(nums[i]<pivot)
+                less.add(nums[i]);
+            
+            else if(nums[i]>pivot)
+                more.add(nums[i]);
+            
+        }
+        
+        int index=0;
+        
+        for(;index<less.size();index++)
+            ans[index]=less.get(index);
+        
+        int same=nums.length-less.size()-more.size();
+        
+        while(same!=0){
+            ans[index++]=pivot;
+            same--;
+        }
+        
+        int curlen=nums.length-more.size();
+        
+        
+        for(;index<nums.length;index++){
+            ans[index]=more.get(index-curlen);
+        }
+        
+        return ans;
+            
+        
+    }
+}

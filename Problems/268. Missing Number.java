4a302064dd5674a51449e746bb76f04b06a7f444
sushant-sinha <sinhasushant04@gmail.com>
2022-05-29 12:57:35 +0530

    new addition

--- Problems/268. Missing Number.java
+++ Problems/268. Missing Number.java
@@ -1,25 +1,42 @@
 // SUSHANT SINHA
 
-// 0-1ms ( 100% ) 50.5mb ( 68.83% )
+// 0ms-1ms ( 100%-65.97% ) 50.5mb-49.5mb ( 68.83%-71.25% )
 
 class Solution {
     public int missingNumber(int[] nums) {
         
-        // USING SUM FASTEST
+        // USING BIT MANIPULATION FASTEST
+        // from the submission tab
+                
+        
+        int ans = nums.length;
+        
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        
+        return ans;
+        
+        /* USING SUM FASTEST
+        
         
         int ans=0;
         for(int i=0;i<nums.length;i++)ans+=nums[i];
-
         return ((nums.length)*(nums.length+1)/2)-ans;
         
-        // SORTING
+        */
         
-        // Arrays.sort(nums);
+        /* SORTING
         
-        // for(int i=0;i<nums.length;i++){
-        // if(nums[i]!=i)return i;
-        // }
-        // return nums.length;
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length;i++){
+        if(nums[i]!=i)return i;
+        }
+        return nums.length;
+        
+        */
         
     }
 }

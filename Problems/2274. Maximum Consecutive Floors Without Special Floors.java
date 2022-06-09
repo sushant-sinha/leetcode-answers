54fa90544b69b56222f300ff1b0e797df99191b9
sushant-sinha <sinhasushant04@gmail.com>
2022-06-10 00:18:17 +0530

    new addition

---
+++ Problems/2274. Maximum Consecutive Floors Without Special Floors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxConsecutive(int bottom, int top, int[] special) {
+        
+        Arrays.sort(special);
+        
+        int ans=0, old=0;
+        int i=0;
+        
+        while(bottom<=top && i<special.length){
+            
+            int len=special[i]-bottom;
+            //System.out.println("bottom"+bottom+" spe "+special[i]+" len "+len+" ans "+ans);
+            ans=len>ans ? len : ans;
+            
+            bottom=special[i]+1;
+            i++;
+            
+        }
+        
+        ans=(top-bottom+1)>ans ? (top-bottom+1) : ans;
+        
+        //System.out.println(i);
+        
+        return ans;
+        
+    }
+}

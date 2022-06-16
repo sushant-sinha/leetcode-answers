9d826ea6b51dc24e1c8d909b5cd274a336e4808f
sushant-sinha <sinhasushant04@gmail.com>
2022-06-16 23:19:55 +0530

    new addition

--- Problems/2303. Calculate Amount Paid in Taxes.java
+++ Problems/2303. Calculate Amount Paid in Taxes.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 1ms ( 92.73% ) 46.1mb ( 49.19% )
+
 class Solution {
     public double calculateTax(int[][] brackets, int income) {
         
@@ -66,5 +70,27 @@ class Solution {
         
         return ans;
         
+    }
+}
+
+// claims to be 0ms but never got the same result
+// looks cleaner tho
+
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
+        int pre=0,sum=0;
+        double ans =0;
+        
+        for(int a[]:brackets){
+            int tmp =a[0]-pre;
+            sum+=tmp;
+            if(sum>income) {
+                ans+= 0.01*(tmp-sum+income)*a[1];
+                break;
+            }
+            ans+= 0.01*tmp*a[1];
+            pre=a[0];
+        }
+        return ans;
     }
 }

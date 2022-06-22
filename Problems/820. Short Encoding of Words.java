642ff2603f3cbbb8e12b177a0a34e7012fc888e0
sushant-sinha <sinhasushant04@gmail.com>
2022-06-22 23:58:39 +0530

    new addition

--- Problems/820. Short Encoding of Words.java
+++ Problems/820. Short Encoding of Words.java
@@ -1,5 +1,48 @@
 // SUSHANT SINHA
 
+
+// trie solution ( also the expected solution )
+// from leetcode's solution tab *to do*
+
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        TrieNode trie = new TrieNode();
+        Map<TrieNode, Integer> nodes = new HashMap();
+
+        for (int i = 0; i < words.length; ++i) {
+            String word = words[i];
+            TrieNode cur = trie;
+            for (int j = word.length() - 1; j >= 0; --j)
+                cur = cur.get(word.charAt(j));
+            nodes.put(cur, i);
+        }
+
+        int ans = 0;
+        for (TrieNode node: nodes.keySet()) {
+            if (node.count == 0)
+                ans += words[nodes.get(node)].length() + 1;
+        }
+        return ans;
+
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    int count;
+    TrieNode() {
+        children = new TrieNode[26];
+        count = 0;
+    }
+    public TrieNode get(char c) {
+        if (children[c-'a'] == null) {
+            children[c-'a'] = new TrieNode();
+            count++;
+        }
+        return children[c - 'a'];
+    }
+}
+
 // initial approach
 // this will be followed by the trie approach (optimal ig)... it has better speed at the cost of space
 

81f3b681ca5ab4cb6df8d2a2f1f62fa423813f50
sushant-sinha <sinhasushant04@gmail.com>
2022-06-23 23:04:59 +0530

    new addition

---
+++ Problems/575. Distribute Candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        
+        int allowedCandies=candyType.length/2;
+        
+        Arrays.sort(candyType);
+        
+        int unique=1;
+        
+        for(int i=1;i<candyType.length;i++){
+            
+            if(candyType[i]!=candyType[i-1])
+                unique++;
+            
+        }
+        
+        return unique<allowedCandies ? unique : allowedCandies   ;
+        
+    }
+}

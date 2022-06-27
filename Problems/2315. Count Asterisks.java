9d1a336d911ae74b707f8c0b8cfd198b3ebc42d1
sushant-sinha <sinhasushant04@gmail.com>
2022-06-27 23:33:38 +0530

    new addition

---
+++ Problems/2315. Count Asterisks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countAsterisks(String s) {
+        
+        boolean start=false;
+        
+        int ans=0;
+        
+        for(char c:s.toCharArray()){
+            
+            if(c=='|'){
+                start=!start;
+                continue;
+            }
+            
+            if(!start && c=='*')
+                ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

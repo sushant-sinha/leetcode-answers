0e2ff949a47ac8ffb38adb4ff2d9e65dc9d4b21a
sushant-sinha <sinhasushant04@gmail.com>
2022-07-03 00:08:26 +0530

    update

--- Problems/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.java
+++ Problems/1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts.java
@@ -1,3 +1,8 @@
+// SUSHANT SINHA
+
+// my solution (one condition is reduntant-> the if and else condition i guess))
+// 26ms ( 16.41% ) 61.7mb ( 6.75% )
+
 class Solution {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
         
@@ -6,46 +11,40 @@ class Solution {
         
         long hMaxDiff=horizontalCuts[0], vMaxDiff=verticalCuts[0];
         
-        if(horizontalCuts.length>1){
             
-            for(int i=0;i<horizontalCuts.length-1;i++){
-                
-                hMaxDiff=hMaxDiff>horizontalCuts[i+1]-horizontalCuts[i] ? hMaxDiff : horizontalCuts[i+1]-horizontalCuts[i];
-                
-            }
+        for(int i=0;i<horizontalCuts.length-1;i++){
             
-            hMaxDiff= hMaxDiff> h-horizontalCuts[horizontalCuts.length-1] ? hMaxDiff : h-horizontalCuts[horizontalCuts.length-1];
+            hMaxDiff=hMaxDiff>horizontalCuts[i+1]-horizontalCuts[i] ? hMaxDiff : horizontalCuts[i+1]-horizontalCuts[i];
             
-        }
-        
-        else
-            hMaxDiff= hMaxDiff>h-horizontalCuts[0] ? hMaxDiff : h-horizontalCuts[0];
         
         
-        if(verticalCuts.length>1){
+        hMaxDiff= hMaxDiff> h-horizontalCuts[horizontalCuts.length-1] ? hMaxDiff : h-horizontalCuts[horizontalCuts.length-1];
+        
+    
+    
+    
+        hMaxDiff= hMaxDiff>h-horizontalCuts[0] ? hMaxDiff : h-horizontalCuts[0];
+    
+        
+        for(int i=0;i<verticalCuts.length-1;i++){
             
-            for(int i=0;i<verticalCuts.length-1;i++){
-                
-                vMaxDiff=vMaxDiff>verticalCuts[i+1]-verticalCuts[i] ? vMaxDiff : verticalCuts[i+1]-verticalCuts[i];
-                
-            }
-            
-            vMaxDiff= vMaxDiff>w-verticalCuts[verticalCuts.length-1] ? vMaxDiff : w-verticalCuts[verticalCuts.length-1];
+            vMaxDiff=vMaxDiff>verticalCuts[i+1]-verticalCuts[i] ? vMaxDiff : verticalCuts[i+1]-verticalCuts[i];
             
         }
         
-        else
-            vMaxDiff= vMaxDiff>w-verticalCuts[0] ? vMaxDiff : w-verticalCuts[0];
+        vMaxDiff= vMaxDiff>w-verticalCuts[verticalCuts.length-1] ? vMaxDiff : w-verticalCuts[verticalCuts.length-1];
+        
+        vMaxDiff= vMaxDiff>w-verticalCuts[0] ? vMaxDiff : w-verticalCuts[0];
         
         
-        //System.out.println(hMaxDiff+" "+vMaxDiff);
+        // System.out.println(hMaxDiff+" "+vMaxDiff);
 
-//         if have used long instead of int ... this would have worked
-//         int ans=1;
-//         ans=(ans*hMaxDiff)%1000000007;
-//         ans=(ans*vMaxDiff)%1000000007;
+        // if have used long instead of int ... this would have worked
+        // int ans=1;
+        // ans=(ans*hMaxDiff)%1000000007;
+        // ans=(ans*vMaxDiff)%1000000007;
         
-//         return ans;
+        // return ans;
         
         
         long ans=((hMaxDiff%1000000007)*(vMaxDiff%1000000007))%(1000000007);

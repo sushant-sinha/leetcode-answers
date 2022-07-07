3e3014beff9e625cfbef890418caf046067b00af
sushant-sinha <sinhasushant04@gmail.com>
2022-07-07 23:53:25 +0530

    new addition

---
+++ Problems/2319. Check if Matrix Is X-Matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        
+        for(int i=0;i<grid.length;i++){
+            
+            for(int j=0;j<grid.length;j++){
+                
+                if(i==j && grid[i][j]==0)
+                    return false;
+                
+                else if(i+j==grid.length-1 && grid[i][j]==0)
+                    return false;
+                
+                else if(i!=j && i+j!=grid.length-1 && grid[i][j]!=0)
+                    return false;
+            }
+            
+        }
+        
+        return true;
+        
+    }
+}

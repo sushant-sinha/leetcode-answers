a5d85221dc489b7ff92ab7a735371aa9b83bbb7a
sushant-sinha <sinhasushant04@gmail.com>
2022-07-08 23:02:02 +0530

    new addition

--- Problems/2320. Count Number of Ways to Place Houses.java
+++ Problems/2320. Count Number of Ways to Place Houses.java
@@ -1,3 +1,17 @@
+// intuition: using fibonacci series to calculate the number of possible arrangements on one side of the road, and then squaring it
+
+// the number of possible arrangements on the one side of the road can be calculated using the fibonacci series..
+// the same combination will be for the arrangements on the opposite side of the road
+
+// if n==1: then the number of possible combinations of houses on one side of the road->2
+// if n==2: then the number of possible combinations of houses on one side of the road->3
+// if n==3: then the number of possible combinations of houses on one side of the road->5
+// if n==4: then the number of possible combinations of houses on one side of the road->8... so on
+
+// to get the final answer.. multiply both (square)
+
+// the hint was useful (see the dp answer also)(used recusion)
+
 class Solution {
     public int countHousePlacements(int n) {
         

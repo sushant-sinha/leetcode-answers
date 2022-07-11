90e3c5352d70d2243126367d268690ad62100b6f
sushant-sinha <sinhasushant04@gmail.com>
2022-07-11 23:01:08 +0530

    new addition

---
+++ Problems/2335. Minimum Amount of Time to Fill Cups.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        
+        int ans=0;//, max1=0;max2=1;
+        
+        while(amount[0]!=0 || amount[1]!=0 || amount[2]!=0){
+            
+            Arrays.sort(amount);
+            
+            //System.out.println(Arrays.toString(amount));
+            
+            if(amount[1]!=0){
+                amount[2]--;
+                amount[1]--;
+            }
+            
+            else
+                amount[2]--;
+            
+            ans++;
+        }
+        
+        return ans;
+        
+    }
+}

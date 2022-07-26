5b36306c4bb7d4a2f07a8733bbe7dc2bdf89f17b
sushant-sinha <sinhasushant04@gmail.com>
2022-07-27 01:43:12 +0530

    new addition

--- Problems/86. Partition List.java
+++ Problems/86. Partition List.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 1ms ( 69.53% ) 43mb ( 18.23% )
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -36,6 +40,25 @@ class Solution {
         
         // ending the linkedlist
         more.next=null;
+        
+        /*
+        
+        if i will be doing (less.next=more_head;) right now and then returning the less_head...
+        
+        the solution for the first test case will be : [0,1,2,2,0,4,3,5]
+        
+        so i will have to remove the: '0' before the individual lists
+        
+        less_head=less_head.next;
+        more_head=more_head.next;
+        
+        *** cannot use this idea... because if the less_head is empty... then we will get the wrong answer....
+        
+        *** but if the more_head is empty... we wont get wrong answer... because more_head.next is pointing towards null.. see line 38
+        
+        therefor the pointing from the less list towards the more list should be careful.
+        
+        */
         
         less.next=more_head.next;
         

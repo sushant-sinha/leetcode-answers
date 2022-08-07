ba3568102b04504a55fc15d30f70339d2f8a7739
sushant-sinha <sinhasushant04@gmail.com>
2022-08-07 19:33:17 +0530

    new addition

--- Problems/2351. First Letter to Appear Twice.java
+++ Problems/2351. First Letter to Appear Twice.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 0ms ( 100% ) 42mb ( 25.00% )
+
 class Solution {
     public char repeatedCharacter(String s) {
         
@@ -10,6 +14,33 @@ class Solution {
             
             else
                 ar[c-'a']++;
+            
+        }
+        
+        return ' ';
+        
+    }
+}
+
+
+// 1ms 40.1mb
+
+class Solution {
+    public char repeatedCharacter(String s) {
+        
+        HashMap<Character, Integer> count=new HashMap<>();
+        
+        for(char c:s.toCharArray()){
+            
+            if(!count.containsKey(c))
+                count.put(c, 1);
+            
+            else{
+                if(count.get(c)==1)
+                    return c;
+                else
+                    continue;
+            }
             
         }
         

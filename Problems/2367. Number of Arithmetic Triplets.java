90d56b2edd4ba43bb0982cb4719e7fde93b326f5
sushant-sinha <sinhasushant04@gmail.com>
2022-08-12 01:55:16 +0530

    new addition

---
+++ Problems/2367. Number of Arithmetic Triplets.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        
+        /*
+        
+        // brute force
+        
+        int ans=0;
+        
+        for(int i=0;i<nums.length-2;i++){
+            
+            for(int j=i+1;j<nums.length-1;j++){
+                
+                for(int k=j+1; k<nums.length;k++){
+                    
+                    if((nums[j] - nums[i] == diff) && (nums[k] - nums[j] == diff))
+                        ans++;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+        
+        */
+        
+        // new method
+        
+        // this array is for marking the visited elements
+        // also will be needed when we will be finding the other 2 elements (i and j)
+        
+        boolean seen[]=new boolean[201];
+        
+        int ans=0;
+        
+        for(int i: nums){
+            
+            if(i>=2*diff)
+                ans += seen[i-diff] && seen[i-(2*diff)] ? 1 : 0;
+            
+            seen[i]=true;
+            
+        }
+        
+        return ans;
+        
+    }
+}

241873f7169bc63f2176563bc2d191ec3e90bbac
sushant-sinha <sinhasushant04@gmail.com>
2022-08-24 00:03:18 +0530

    new addition

--- Problems/2363. Merge Similar Items.java
+++ Problems/2363. Merge Similar Items.java
@@ -1,3 +1,12 @@
+// SUSHANT SINHA
+
+// 11ms ( >83.33% ) 43.3mb ( >16.67% )
+// most optimized solution in the bottom
+
+// see this for the sorting of HashMap on the basis of keey
+
+// https://www.javatpoint.com/how-to-sort-hashmap-in-java#:~:text=Sort%20HashMap%20by%20Keys&text=When%20we%20use%20LinkedHashMap%2C%20we,example%20Sort%20HashMap%20by%20Value.
+
 class Solution {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
         
@@ -46,5 +55,32 @@ class Solution {
         
         return ans;
         
+    }
+}
+
+// ~10ms
+
+class Solution {
+    public int[][] mergeSimilarItems(int[][] items1, int[][] items2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int [] item : items1){
+            map.put(item[0], map.getOrDefault(item[0], 0) + item[1]);
+        }
+        
+        for (int [] item : items2){
+            map.put(item[0], map.getOrDefault(item[0], 0) + item[1]);
+        }
+        
+        int [][] result = new int [map.size()][];
+        int idx = 0;
+        
+        for (int key : map.keySet()){
+            result[idx++] = new int [] {key , map.get(key)};
+        }
+        
+        Arrays.sort(result, (a , b) -> a[0] - b[0]);
+        
+        return result;
     }
 }

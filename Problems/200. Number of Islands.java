02fbfe1a25826c2efe97c6656d903e64ed242375
sushant-sinha <sinhasushant04@gmail.com>
2022-08-30 22:55:19 +0530

    new addition

---
+++ Problems/200. Number of Islands.java
@@ -0,0 +1,51 @@
+class Solution {
+   
+    char[][] flag;
+
+    public int numIslands(char[][] grid) {
+        
+        flag=grid;
+        
+        int ans=0;
+        
+        int m=grid.length;
+        int n=grid[0].length;
+        
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(grid[i][j]=='1'){
+                    
+                    ans++;
+                    
+                    fill(i, j, m, n);
+                    
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+        
+        
+    }
+    
+   void fill(int i,int j, int m, int n){
+       
+       if(i>=m || i<0 || j>=n || j<0 || flag[i][j]=='0')
+           return;
+       
+       else{
+           flag[i][j]='0';
+       }
+       
+       fill(i+1,j,m,n);
+       fill(i,j+1,m,n);
+       fill(i-1,j,m,n);
+       fill(i,j-1,m,n);
+           
+       
+   }
+}

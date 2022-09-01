a6c95bf3a3fcb3849327ee57a4bb578be8374802
sushant-sinha <sinhasushant04@gmail.com>
2022-09-01 14:45:45 +0530

    new addition

---
+++ Problems/2390. Removing Stars From a String.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeStars(String s) {
+        
+        //int star=0;
+        
+        //int deleted=0;
+        StringBuffer sbuf=new StringBuffer();
+        
+        for(int i=0;i<s.length();i++){
+            
+            sbuf.append(s.charAt(i));
+            
+            if(s.charAt(i)=='*'){
+                sbuf.deleteCharAt(sbuf.length()-1);
+                sbuf.deleteCharAt(sbuf.length()-1);
+            }
+            
+            //deleted+=2;
+                
+            
+        }
+        
+        //System.out.println(sbuf);
+        
+        return sbuf.toString();
+        
+    }
+}

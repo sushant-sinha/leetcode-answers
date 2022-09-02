7b4b2d0e7526834ffc21ae43e2ae16ebd4415e3c
sushant-sinha <sinhasushant04@gmail.com>
2022-09-03 02:40:15 +0530

    new addition

---
+++ Problems/1448. Count Good Nodes in Binary Tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    int ans=0;
+    
+    public int goodNodes(TreeNode root) {
+        
+        dfs(root, -10001);
+        
+        return ans;
+        
+    }
+    
+    void dfs(TreeNode node, int max){
+        
+        if(node==null)
+            return;
+        
+        else if(!(max>node.val)){
+            
+            ans++;
+            max=node.val;
+            
+        }
+        
+        dfs(node.left, max);
+        dfs(node.right, max);
+        
+        
+        
+    }
+}

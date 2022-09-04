2056f0296ff54a121176a47c61a462cb7008aaa4
sushant-sinha <sinhasushant04@gmail.com>
2022-09-05 00:17:05 +0530

    new addition

---
+++ Problems/2399. Check Distances Between Same Letters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        
+        int ar[]=new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(ar[s.charAt(i)-'a']==0){
+                ar[s.charAt(i)-'a']-=(i+1);
+            }
+            
+            else{
+                
+                ar[s.charAt(i)-'a']+=i;
+                
+                //System.out.println(ar[s.charAt(i)-'a']);
+                
+                if(ar[s.charAt(i)-'a']!=distance[s.charAt(i)-'a']){
+                    return false;
+                }
+                
+            }
+            
+        }
+        
+        return true;
+        
+    }
+}

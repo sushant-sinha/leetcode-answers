d4a50d4ba7bf5ea4c7722e375f1630d05e4c830b
sushant-sinha <sinhasushant04@gmail.com>
2022-09-18 22:12:36 +0530

    new addition

---
+++ Problems/42. Trapping Rain Water.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        int l=height.length;
+        
+        int maxL[]=new int[l];
+        
+        maxL[0]=0;
+        
+        int curmax=height[0];
+        
+        for(int i=1;i<l;i++){
+            
+            if(height[i]>curmax){
+                maxL[i]=0;
+                curmax=height[i];
+            }
+            
+            else
+            maxL[i]=curmax-height[i];
+            
+        }
+        
+        int maxR[]=new int[l];
+        
+        maxR[l-1]=0;
+        
+        curmax=height[l-1];
+        
+        for(int i=l-2;i>=0;i--){
+            
+            if(height[i]>curmax){
+                maxR[i]=0;
+                curmax=height[i];
+            }
+            
+            else
+            maxR[i]=curmax-height[i];
+            
+        }
+        
+        int ans=0;
+        
+        for(int i=0;i<l;i++){
+            //System.out.println(height[i]+" "+maxL[i]+" "+maxR[i]);
+            
+            ans+=Math.min(maxR[i],maxL[i]);
+        }
+        
+        return ans;
+        
+    }
+}

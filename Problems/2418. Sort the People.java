2a64372ae89cbe81502f8791e5d100c77e4d079f
sushant-sinha <sinhasushant04@gmail.com>
2022-09-27 00:01:52 +0530

    new addition

---
+++ Problems/2418. Sort the People.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        
+        // bubble sort
+        
+        int num=0, len=heights.length;
+        
+        while(num<len-1){
+            
+            int cur=heights[len-1];
+            //System.out.println(cur);
+            
+            for(int i=len-2;i>=num;i--){
+                if(heights[i]<cur){
+                    
+                    //System.out.println(heights[i]+" "+heights[i+1]+" "+names[i]+" "+names[i+1]);
+                    
+                    // swapping string
+                    String temp=names[i];
+                    names[i]=names[i+1];
+                    names[i+1]=temp;
+                    
+                    // swapping int
+                    int t=heights[i];
+                    heights[i]=heights[i+1];
+                    heights[i+1]=t;
+                    
+                }
+                
+                cur=heights[i];
+            }
+            
+            num++;
+            
+        }
+        
+        return names;
+        
+    }
+}

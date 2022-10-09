27457d7acf45f6f626b467d0b6607fe7a1055758
sushant-sinha <sinhasushant04@gmail.com>
2022-10-09 23:00:14 +0530

    new addition

---
+++ Problems/2427. Number of Common Factors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        
+        int ans=0;
+        
+        HashSet<Integer> factors=new HashSet<>();
+        
+        for(int i=1; i<=Math.sqrt(b); i++){
+            
+            if(b%i==0){
+                factors.add(i);
+                factors.add(b/i);
+            }
+            
+        }
+        
+        //System.out.println(factors);
+        
+        for(int i=1; i<=Math.sqrt(a); i++){
+            
+            if(a%i==0 && factors.contains(i))
+                ans++;
+            
+            if( a%i==0 && factors.contains(a/i) && ((a/i)!=i)  )
+                ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

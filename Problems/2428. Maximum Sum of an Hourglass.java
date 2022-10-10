2bc44ddfcadf26e68c7f87e2118c434e33f65c2d
sushant-sinha <sinhasushant04@gmail.com>
2022-10-11 00:17:57 +0530

    new addition

---
+++ Problems/2428. Maximum Sum of an Hourglass.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        
+        int sum=-1;
+        
+        for(int i=0;i<=grid.length-3;i++){
+            
+            for(int j=0;j<=grid[0].length-3;j++){
+                int temp=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+                
+                sum=sum>temp ? sum : temp;
+            }
+            
+        }
+        
+        return sum;
+        
+    }
+}

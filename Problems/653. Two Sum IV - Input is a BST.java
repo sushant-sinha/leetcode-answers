d3fc4e7cbbb3f35e83c4982a0b41dfea6e0d6820
sushant-sinha <sinhasushant04@gmail.com>
2022-10-13 02:52:37 +1300

    new addition

---
+++ Problems/653. Two Sum IV - Input is a BST.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    ArrayList<Integer> seen=new ArrayList<>();
+    
+    public boolean findTarget(TreeNode root, int k) {
+        
+        return dfs(root, k);
+        
+        
+    }
+    
+    boolean dfs(TreeNode root, int k){
+        
+        int find=k-root.val;
+        
+        if(seen.contains(find))
+            return true;
+        
+        seen.add(root.val);
+        
+        boolean left=false, right=false;
+        
+        if(root.left!=null)
+            left=dfs(root.left,k);
+        
+        if(root.right!=null)
+            right=dfs(root.right, k);
+        
+        return left||right;
+        
+    }
+    
+}

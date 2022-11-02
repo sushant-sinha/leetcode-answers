182e4c6239e5ef8b2062aef039b12c01f77fff3e
sushant-sinha <sinhasushant04@gmail.com>
2022-11-03 01:01:17 +0530

    new addition

---
+++ Problems/2413. Smallest Even Multiple.java
@@ -0,0 +1,11 @@
+// SUSHANT SINHA
+
+// 0ms ( 100% ) 39.9mb ( 86.15% )
+
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        
+        return n%2==0 ? n : n*2;
+        
+    }
+}

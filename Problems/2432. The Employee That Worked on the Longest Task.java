2b35e39d4a13e7d1f0d085643607ece5a1e8b6cc
sushant-sinha <sinhasushant04@gmail.com>
2022-11-05 01:29:53 +0530

    new addition

---
+++ Problems/2432. The Employee That Worked on the Longest Task.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int hardestWorker(int n, int[][] logs) {
+        
+        int maxTime=-1;
+        
+        int curTime=0;
+        
+        int ans=0;
+        
+        for(int i=0;i<logs.length;i++){
+            
+            //System.out.println(ans+" start "+maxTime+" "+curTime);
+            
+            if(logs[i][1]-curTime>=maxTime){
+                
+                if(logs[i][1]-curTime==maxTime){
+                    
+                    if(logs[i][0]<ans){
+                        ans=logs[i][0];
+                        curTime=logs[i][1];
+                        //System.out.println(ans+" hi "+maxTime);
+                        continue;
+                    }
+                    
+                    else{
+                        curTime=logs[i][1];
+                        //System.out.println(ans+" hi "+maxTime);
+                        continue;
+                    }
+                    
+                }
+                
+                
+                ans=logs[i][0];
+                maxTime=logs[i][1]-curTime;
+                //System.out.println(ans+" "+maxTime);
+                
+            }
+            
+            curTime=logs[i][1];
+            
+        }
+        
+        return ans;
+        
+    }
+}

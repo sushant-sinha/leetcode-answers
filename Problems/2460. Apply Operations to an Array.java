95a115db56a5c3adfd742a07c43b3715b22da4db
sushant-sinha <sinhasushant04@gmail.com>
2022-11-09 02:33:48 +1000

    new addition

---
+++ Problems/2460. Apply Operations to an Array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        
+        ArrayList<Integer> pos=new ArrayList<>();
+        
+        for(int i=0;i<nums.length-1;i++){
+            
+            if(nums[i]==nums[i+1]){
+                
+                if(nums[i]*2!=0){
+                    nums[i]*=2;
+                    pos.add(nums[i]);
+                }
+                
+                nums[i+1]=0;
+                
+            }
+            
+            else{
+                if(nums[i]!=0)
+                    pos.add(nums[i]);
+            }
+            
+        }
+        
+        if(nums[nums.length-1]!=0)
+            pos.add(nums[nums.length-1]);
+        
+        int index=0;
+        
+        for(int i:pos){
+            nums[index++]=i;
+        }
+        
+        while(index<nums.length)
+            nums[index++]=0;
+        
+        
+        return nums;
+        
+    }
+}

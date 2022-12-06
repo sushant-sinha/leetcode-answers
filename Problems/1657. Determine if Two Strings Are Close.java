e4cf8172125bd9f025dbc4864273d2b8722aa9fc
sushant-sinha <sinhasushant04@gmail.com>
2022-12-07 04:14:25 +1100

    new addition

--- Problems/1657. Determine if Two Strings Are Close.java
+++ Problems/1657. Determine if Two Strings Are Close.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 32ms ( 49.62% ) 54.2mb ( 70.45% )
+
 // first attempt
 
 /*
@@ -54,3 +58,41 @@ class Solution {
 }
 
 */
+
+// the above code passed 150/152 tc
+
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        
+        if(word1.length()!=word2.length())
+            return false;
+        
+        int ar1[]=new int[26];
+        int ar2[]=new int[26];
+        
+        for(int i=0;i<word1.length();i++){
+            ar1[word1.charAt(i)-'a']++;
+            ar2[word2.charAt(i)-'a']++;
+        }
+        
+        // characters should be present in both the strings
+        for(int i=0;i<26;i++)
+            if((ar1[i]!=0 && ar2[i]==0) || (ar1[i]==0 && ar2[i]!=0))
+                return false;
+        
+        // checking whether the equal frequencies are present in both words
+        Arrays.sort(ar1);
+        Arrays.sort(ar2);
+        
+        for(int i=0;i<26;i++){
+            
+            if(ar1[i]!=ar2[i])
+                return false;
+            
+        }
+        
+        return true;
+        
+        
+    }
+}

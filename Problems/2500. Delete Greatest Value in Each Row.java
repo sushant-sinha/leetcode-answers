94135b2e26e26bb83506ab4e7e5b6742cc073d59
sushant-sinha <sinhasushant04@gmail.com>
2022-12-14 22:06:56 +0530

    new addition

---
+++ Problems/2500. Delete Greatest Value in Each Row.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        
+        int sum=0;
+        
+        // rowwise sorted values in matrix
+        
+        int m=grid.length, n=grid[0].length;
+        
+        for(int i=0;i<m;i++){
+            
+            Arrays.sort(grid[i]);
+            
+        }
+        
+        for(int j=n-1;j>=0;j--){
+            
+            int max=0;
+            
+            for(int i=0;i<m;i++){
+                max=max<grid[i][j] ? grid[i][j] : max;
+            }
+            
+            sum+=max;
+            
+        }
+        
+        return sum;
+        
+    }
+}

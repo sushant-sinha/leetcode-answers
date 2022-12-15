ed49c3c5c32aed673d0aab92b3a4cc69a5cd87b0
sushant-sinha <sinhasushant04@gmail.com>
2022-12-16 04:30:38 +1100

    new addition

---
+++ Problems/2490. Circular Sentence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+
+        String ar[]=sentence.split(" ");
+
+        if(ar.length==1){
+            return sentence.charAt(0)==sentence.charAt(sentence.length()-1);
+        }
+
+        for(int i=0;i<ar.length-1;i++){
+
+            if( ar[i].charAt(ar[i].length()-1) != ar[i+1].charAt(0)){
+                return false;
+            }
+
+        }
+
+        if(ar[0].charAt(0) != ar[ar.length-1].charAt(ar[ar.length-1].length()-1)){
+            return false;
+        }
+
+        return true;
+        
+    }
+}

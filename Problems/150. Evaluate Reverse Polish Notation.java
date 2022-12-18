d89be9445ce017442f304d6f72cc95740eeb9b14
sushant-sinha <sinhasushant04@gmail.com>
2022-12-19 00:33:04 +0530

    new addition

--- Problems/150. Evaluate Reverse Polish Notation.java
+++ Problems/150. Evaluate Reverse Polish Notation.java
@@ -15,3 +15,47 @@ input: 10 6 9 3 + -11 * / * 17 + 5 +
 [22]
 
 */
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack=new Stack<>();
+
+        for(String s:tokens){
+
+            if(s.charAt(s.length()-1)=='+'){
+                int temp=stack.pop()+stack.pop();
+                
+                stack.push(temp);
+            }
+
+            else if(s.charAt(s.length()-1)=='-'){
+                int temp1=stack.pop();
+                // System.out.println("-"+temp);
+                stack.push(stack.pop()-temp1);
+            }
+
+            else if(s.charAt(s.length()-1)=='/'){
+                int temp1=stack.pop();
+                // System.out.println("/"+temp);
+                stack.push(stack.pop()/temp1);
+            }
+
+            else if(s.charAt(s.length()-1)=='*'){
+                int temp=stack.pop()*stack.pop();
+                // System.out.println("*"+temp);
+                stack.push(temp);
+            }
+
+            else{
+                stack.push(Integer.valueOf(s));
+                // System.out.println(s);
+            }
+
+            // System.out.println("stack"+stack);
+        }
+
+        return (int)stack.pop();
+        
+    }
+}

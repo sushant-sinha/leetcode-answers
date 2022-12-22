18429ffbf03343dc7ac72bac039ea7b70295867e
sushant-sinha <sinhasushant04@gmail.com>
2022-12-23 00:21:41 +0530

    new addition

--- Problems/2507. Smallest Value After Replacing With Sum of Prime Factors.java
+++ Problems/2507. Smallest Value After Replacing With Sum of Prime Factors.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 4ms ( 61.54% ) 39mb ( 98.69% )
+
 class Solution {
     public int smallestValue(int n) {
         
@@ -44,6 +48,42 @@ class Solution {
         // System.out.println(ans);
         
         return ans;
+        
+    }
+}
+
+// can skip the sum function and directly return the sum of the prime factors
+
+class Solution {
+    public int smallestValue(int n) {
+        
+        int oldVal=n;
+        int newVal=primeFactorsSum(n);
+        
+        while(oldVal!=newVal){
+            oldVal=newVal;
+            newVal=primeFactorsSum(oldVal);
+        }
+        
+        return newVal;
+        
+    }
+    
+    int primeFactorsSum(int n){
+        
+        int sum=0;
+
+        for(int i=2; i<=n; i++){
+            
+            if(n%i==0){
+                sum+=i;
+                n/=i;
+                i=1;
+            }
+            
+        }
+        
+        return sum;
         
     }
 }

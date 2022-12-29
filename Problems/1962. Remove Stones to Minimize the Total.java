58d4d924b13d3ec69bbaba119957250c255ad590
sushant-sinha <sinhasushant04@gmail.com>
2022-12-30 00:32:12 +0530

    new addition

--- Problems/1962. Remove Stones to Minimize the Total.java
+++ Problems/1962. Remove Stones to Minimize the Total.java
@@ -1,3 +1,8 @@
+// SUSHANT SINHA
+
+// my solution using treemap
+// 485ms ( 91.27% ) 52.9mb ( 100% )
+
 class Solution {
     public int minStoneSum(int[] piles, int k) {
 
@@ -44,3 +49,31 @@ class Solution {
         
     }
 }
+
+// best solution from the submission tab (120x better)
+// 4ms ( 100% ) 55.4mb ( 89.98% )
+
+
+//--Instead of  using PriorityQueue , we can use array to track number of piles with same heigth
+
+public class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        int sum = 0;
+        int a[] = new int[10001];
+        for (int i : piles) {
+            sum += i;
+            a[i]++;
+        }
+        for (int i = 10000; i > 0 && k > 0; i--) {
+            if (a[i] > 0) {
+                int b = i / 2;
+                sum -= Math.min(k, a[i]) * b;
+                a[i - i / 2] += a[i];
+                k -= a[i];
+            }
+        }
+        return sum;
+
+    }
+}
+// Time complexity : O(n)

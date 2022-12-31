ec1a6c9a26dcefd27340433ebdb5dd8f2794883a
sushant-sinha <sinhasushant04@gmail.com>
2022-12-31 14:03:48 +0530

    new addition

---
+++ Problems/2515. Shortest Distance to Target String in a Circular Array.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int closetTarget(String[] words, String target, int startIndex) {
+        
+        HashMap<String, ArrayList<Integer>> map=new HashMap<>();
+        
+        for(int i=0;i<words.length;i++){
+            
+            ArrayList<Integer> ar=new ArrayList<>();
+            
+            if(map.containsKey(words[i])){
+                ar=map.get(words[i]);
+                ar.add(i);
+                map.put(words[i], ar);
+            }
+            
+            else{
+                ar.add(i);
+                map.put(words[i], ar);
+            }
+            
+        }
+        
+        if(map.containsKey(target)){
+            return findMin(startIndex, map.get(target), words.length);
+        }
+        
+        return -1;
+        
+    }
+    
+    int findMin(int startIndex, ArrayList<Integer> ar, int len){
+        
+        int min=0, ans=101;
+        
+        for(int i:ar){
+            
+            if(i==startIndex)
+                return 0;
+            
+            if(startIndex>i){
+                
+                min=Math.min(len-startIndex+i, Math.abs(startIndex-i));
+                
+            }
+            
+            else{
+                
+                min=Math.min(len-i+startIndex, Math.abs(startIndex-i));
+                
+            }
+            
+            
+            ans=Math.min(ans,min);
+            // System.out.println(i+" start="+startIndex+" "+min+" first="+(len-startIndex+i)+" sec="+(Math.abs(startIndex-i)));
+            
+            
+            
+        }
+        
+        return ans;
+        
+    }
+}

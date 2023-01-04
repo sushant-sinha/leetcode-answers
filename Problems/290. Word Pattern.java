f189cc93ed8385d5522eb76c6cda7294f175e5f3
sushant-sinha <sinhasushant04@gmail.com>
2023-01-04 22:17:48 +0530

    new addition

--- Problems/290. Word Pattern.java
+++ Problems/290. Word Pattern.java
@@ -86,3 +86,26 @@ class Solution {
         return true;
     }
 }
+
+// 0ms solution
+// forgot that there is a preexisting function for map.containsValue 😥🤦‍
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        String[] animals = s.split(" ");
+        if (pattern.length() != animals.length) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String animal = animals[i];
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(animal)) return false;                
+            } else {
+                if (map.containsValue(animal)) return false;
+                map.put(c, animal);
+            }
+        }
+        return true;
+    }
+}

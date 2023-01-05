5765978c76313818bd7fd8f798ae5a8f3a6c7a1b
sushant-sinha <sinhasushant04@gmail.com>
2023-01-06 00:07:31 +0530

    new addition

--- Problems/2244. Minimum Rounds to Complete All Tasks.java
+++ Problems/2244. Minimum Rounds to Complete All Tasks.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 81ms ( 44.87% ) 104.2mb ( 39.65% )
+
 class Solution {
     public int minimumRounds(int[] tasks) {
 
@@ -39,4 +43,60 @@ class Solution {
         
     }
 
+}
+
+// first approach
+// This solution gave tle
+
+class Solution {
+    public int minimumRounds(int[] tasks) {
+
+        // mapping the tasks and their frequency
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i:tasks){
+
+            map.put(i, map.getOrDefault(i,0)+1);
+
+        }
+
+        int ans=0;
+
+        for(Map.Entry<Integer, Integer> e: map.entrySet()){
+
+            int val=e.getValue();
+
+            int x=helper(val, 0);
+
+            if(x==-1)
+            return -1;
+
+            ans+=x;
+        }
+
+        return ans;
+        
+    }
+
+    int helper(int a, int times){
+
+        // System.out.println("cur value a="+a+" times="+times);
+
+        if(a==0)
+        return times;
+
+        if(a<2)
+        return -1;
+
+        int three=helper((a-3), times+1);
+        // System.out.println("three="+three);
+        int two=helper((a-2), times+1);
+        // System.out.println("two="+two);
+
+        int ans= (three>0 && two>0 && three<two) || (three>0 && two<0) ? three : two;
+        // System.out.println(ans);
+        return ans;
+
+    }
 }

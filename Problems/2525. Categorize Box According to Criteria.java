84795a0d3345c962c553d1c9eb03b6781f830874
sushant-sinha <sinhasushant04@gmail.com>
2023-01-13 00:35:53 +0530

    new addition

---
+++ Problems/2525. Categorize Box According to Criteria.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String categorizeBox(int length, int width, int height, int mass) {
+        
+        boolean bulky=false, heavy=false;
+        long volume=(long)length*width*height;
+        
+        if(mass>=100)
+            heavy=true;
+        
+        if(length>=10000 || width>=10000 || height>=10000 || volume>=1000000000)
+            bulky=true;
+        // System.out.println(heavy+" "+bulky+" "+(volume>=1000000000)+" vol="+volume);
+        
+        if(bulky && heavy)
+            return "Both";
+        
+        if(!bulky && !heavy)
+            return "Neither";
+        
+        return bulky ? "Bulky" : "Heavy";
+        
+    }
+}

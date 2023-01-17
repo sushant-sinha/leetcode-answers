a790c8cb07803257765d27ca729ab0467923cbad
sushant-sinha <sinhasushant04@gmail.com>
2023-01-17 23:46:40 +0530

    new addition

---
+++ Problems/2522. Partition String Into Substrings With Values at Most K.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minimumPartition(String s, int k) {
+        
+        int ans=0;
+        
+        for(int i=0;i<s.length();){
+            
+            StringBuffer sb=new StringBuffer();
+            
+            sb.append(s.charAt(i));
+            
+            if(k<Integer.valueOf(sb.toString()))
+                return -1;
+            
+            ++i;
+            
+            while(i<s.length() && Integer.valueOf(sb.toString())<99999999){
+                
+                sb.append(s.charAt(i));
+                
+                if(k<Integer.valueOf(sb.toString())){
+                    break;
+                }
+                
+                i++;
+                
+            }
+            
+            // System.out.println(sb);
+            ans++;
+            
+        }
+        
+        return ans;
+        
+    }
+}

68748b2dea23f7db0933c8c95a9e6a31e5fe3237
sushant-sinha <sinhasushant04@gmail.com>
2023-01-19 22:22:10 +0530

    new addition

---
+++ Problems/2535. Difference Between Element Sum and Digit Sum of an Array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        
+        int sum=0, charSum=0;
+        
+        for(int i: nums){
+            
+            sum+=i;
+            charSum+=charSumHelper(i);
+            
+        }
+        
+        return Math.abs(sum-charSum);
+        
+    }
+    
+    int charSumHelper(int i){
+        
+        int sum=0;
+        
+        while(i!=0){
+            sum+=i%10;
+            i/=10;
+        }
+        
+        return sum;
+        
+    }
+    
+}

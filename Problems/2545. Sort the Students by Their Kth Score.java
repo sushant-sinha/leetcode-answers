951faa18541ce1cb92c479832b70a4fd389d03ac
sushant-sinha <sinhasushant04@gmail.com>
2023-01-26 00:20:49 +0530

    new addition

---
+++ Problems/2545. Sort the Students by Their Kth Score.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        
+        int ans[][]=new int[score.length][score[0].length];
+        
+        TreeMap<Integer, int[]> map=new TreeMap<>(Collections.reverseOrder());
+        
+        for(int i=0;i<score.length;i++){
+            map.put(score[i][k], score[i]);
+        }
+        
+        int i=0;
+        
+        for(Map.Entry<Integer, int[]> e: map.entrySet())
+            score[i++]=e.getValue();
+        
+        return score;
+        
+    }
+}

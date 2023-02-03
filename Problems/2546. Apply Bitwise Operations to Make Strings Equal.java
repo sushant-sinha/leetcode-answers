8e527736d426534ccb1ca708d5297e7b07931143
sushant-sinha <sinhasushant04@gmail.com>
2023-02-03 22:11:01 +0530

    new addition

---
+++ Problems/2546. Apply Bitwise Operations to Make Strings Equal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean makeStringsEqual(String s, String target) {
+
+        if(s.equals(target))
+        return true;
+
+        boolean ans1=false, ans2=false;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                ans1=true;
+            }
+            if(target.charAt(i)=='1'){
+                ans2=true;
+            }
+
+            if(ans1&&ans2)
+            return true;
+        }
+
+        return false;
+        
+    }
+}

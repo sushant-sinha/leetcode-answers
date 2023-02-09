a486ecd7812fa909a76b5074455ccb5a2f6339e5
sushant-sinha <sinhasushant04@gmail.com>
2023-02-10 01:30:02 +0530

    new addition

--- Problems/2554. Maximum Number of Integers to Choose From a Range I.java
+++ Problems/2554. Maximum Number of Integers to Choose From a Range I.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 64ms ( 38.94% ) 44mb ( 43.64% )
+
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
         
@@ -21,5 +25,25 @@ class Solution {
         
         return ans;
         
+    }
+}
+
+// 5ms solution
+// since the input size in very small.... choosing an array would've been a better choice instead of using a hashset
+
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        boolean[] f = new boolean[10001];
+        for (int e : banned) f[e] = true;
+        int sum = 0;
+        int cnt = 0;
+        for (int a = 1; a <= n; ++a) {
+            if (!f[a]) {
+                sum += a;
+                if (sum > maxSum) break;
+                ++cnt;
+            }
+        }
+        return cnt;
     }
 }

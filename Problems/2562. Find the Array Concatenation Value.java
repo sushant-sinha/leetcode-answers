83690dcee2b42ed93f9829be5b9a34559d52be73
sushant-sinha <sinhasushant04@gmail.com>
2023-02-14 22:12:20 +0530

    new addition

---
+++ Problems/2562. Find the Array Concatenation Value.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        
+        long ans=0;
+        int len=nums.length;
+        
+        for(int i=0;i<len/2;i++){
+            
+            ans+=help(nums[i], nums[len-i-1]);
+            
+        }
+        
+        if(len%2==1)
+            ans+=nums[len/2];
+        
+        return ans;
+        
+    }
+    
+    long help(int a, int b){
+        
+        return (long)((a*Math.pow(10, String.valueOf(b).length()))+b);
+        
+    }
+}

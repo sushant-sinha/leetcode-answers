d868818c8d9cd11aea5bed57f2da6eb40b62502f
sushant-sinha <sinhasushant04@gmail.com>
2023-02-25 23:25:34 +0530

    new addition

---
+++ Problems/2566. Maximum Difference by Remapping a Digit.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minMaxDifference(int num) {
+        
+        return helper(num, '9')-helper(num, '0');
+        
+    }
+    
+    int helper(int num, char y){
+        
+        char ar[]=String.valueOf(num).toCharArray();
+        
+        char x=ar[0];
+        
+        for(int i=0;i<ar.length;i++){
+            if(ar[i]!=y){
+                x=ar[i];
+                break;
+            }
+        }
+        
+        String ans="";
+        
+        for(char c:ar){
+            
+            if(c==x)
+                ans+=y;
+            
+            else
+                ans+=c;
+        }
+        
+        return Integer.parseInt(ans);
+        
+    }
+}

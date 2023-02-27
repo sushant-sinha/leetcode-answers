b4a724aa868324747efedd11ea20c0fa1b638cc9
sushant-sinha <sinhasushant04@gmail.com>
2023-02-27 21:22:29 +0530

    new addition

---
+++ Problems/2574. Left and Right Sum Differences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] leftRigthDifference(int[] nums) {
+
+        int len=nums.length;
+
+        int left[]=new int[len];
+        int right[]=new int[len];
+
+        left[0]=0;
+        right[len-1]=0;
+
+        for(int i=1;i<len;i++){
+            left[i]=left[i-1]+nums[i-1];
+            right[len-i-1]=right[len-i]+nums[len-i];
+        }
+
+        for(int i=0;i<len;i++)
+        nums[i]=Math.abs(left[i]-right[i]);
+
+        return nums;
+
+        
+    }
+}

633ddbc6eac3eac4a2f76f74984303e87eb07def
sushant-sinha <sinhasushant04@gmail.com>
2023-03-12 00:10:45 +0530

    new addition

---
+++ Problems/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+
+    HashMap<Integer, TreeNode> temp=new HashMap<>();
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+
+        help(cloned);
+
+        return temp.get(target.val);
+
+        
+    }
+
+    void help(TreeNode root){
+
+        if(root==null)
+        return ;
+
+        else
+        temp.put(root.val, root);
+
+        help(root.left);
+        help(root.right);
+    }
+
+}

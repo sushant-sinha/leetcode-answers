94dd751589ec41fd9cfe9b5e3821570df25d39f2
sushant-sinha <sinhasushant04@gmail.com>
2023-03-17 23:11:36 +0530

    to do

---
+++ Problems/208. Implement Trie (Prefix Tree).java
@@ -0,0 +1,63 @@
+// to do using HashMap as well
+
+class TrieNode {
+    boolean isCompleteWord;
+    TrieNode[] children;
+    
+    public TrieNode() {
+        isCompleteWord = false; // when the word is complete (mark that node as true)
+        children = new TrieNode[26]; // for 26 possible Childrens (for next letter)
+    }
+}
+
+class Trie {
+    TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isCompleteWord = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isCompleteWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */

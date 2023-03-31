af24603d13f51d3cff240ba4608fb173fec6e51e
sushant-sinha <sinhasushant04@gmail.com>
2023-03-31 20:48:10 +0530

    new addition

---
+++ Problems/2600. K Items With the Maximum Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        
+        if(k<=numOnes)
+            return k;
+        
+        if(k<=(numOnes+numZeros))
+            return numOnes;
+        
+        else{
+            return (numOnes-(k-numOnes-numZeros));
+        }
+        
+    }
+}

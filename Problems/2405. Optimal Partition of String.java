944ddd3180d2eeb151661d8e30bf6a2e8a272175
sushant-sinha <sinhasushant04@gmail.com>
2023-04-11 23:09:11 +0530

    new addition

--- Problems/2405. Optimal Partition of String.java
+++ Problems/2405. Optimal Partition of String.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 39ms ( 36.13% ) 43.2mb ( 53% )
+
 class Solution {
     public int partitionString(String s) {
         int ans = 1;
@@ -15,5 +19,23 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+// best solution using bit manipulation
+
+class Solution {
+    public int partitionString(String s) {
+
+        int map = 0;
+        int count = 1;
+        for(char c : s.toCharArray()){
+            if((map & (1<<c)) != 0) {
+                count++;
+                map = 0;
+            }
+            map ^= (1<<c);
+        }
+        return count;
     }
 }

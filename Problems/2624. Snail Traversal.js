b6b23f501a63ab3c573fb9b64a78216bcb6471ce
sushant-sinha <sinhasushant04@gmail.com>
2023-04-16 20:57:34 +0530

    new addition

--- Problems/2624. Snail Traversal.js
+++ Problems/2624. Snail Traversal.js
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 220ms ( 57.89% ) 56.7mb ( 88.16% )
+
 /**
  * @param {number} rowsCount
  * @param {number} colsCount
@@ -37,6 +41,32 @@ Array.prototype.snail = function(rowsCount, colsCount) {
 }
 
 /**
+ * const arr = [1,2,3,4];
+ * arr.snail(1,4); // [[1,2,3,4]]
+ */
+
+// maths solution from the Solutions section
+
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if(this.length !==  rowsCount * colsCount) return [];
+    let result = Array(rowsCount).fill([]).map(() => []);
+    for(let j = 0; j < this.length; j++){
+        const i = Math.floor(j / rowsCount);
+        if(i % 2 === 0){
+            result[j % rowsCount][i] = this[j];
+        }else{
+            result[rowsCount - j % rowsCount - 1][i] = this[j];
+        }
+    }
+    return result;
+}
+
+/**     
  * const arr = [1,2,3,4];
  * arr.snail(1,4); // [[1,2,3,4]]
  */

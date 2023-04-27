64c18e6c23f2794212b91a0ea9d23dff62f081f9
sushant-sinha <sinhasushant04@gmail.com>
2023-04-28 02:09:44 +0530

    new addition

--- Problems/319. Bulb Switcher.java
+++ Problems/319. Bulb Switcher.java
@@ -1,6 +1,12 @@
+// SUSHANT SINHA
+
+// 0ms
+// check this article for fun: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+
 class Solution {
     public int bulbSwitch(int n) {
-
-        return (int)Math.sqrt((int)Math.sqrt(n)*(int)Math.sqrt(n));
+        
+        // return (int)Math.sqrt((int)Math.sqrt(n)*(int)Math.sqrt(n));
+        return (int)Math.sqrt(n);
     }
 }

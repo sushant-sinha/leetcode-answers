1921a2a82a170efe6d4454f9a73ea15ff402a17f
sushant-sinha <sinhasushant04@gmail.com>
2023-04-29 23:23:03 +0530

    new addition

---
+++ Problems/2656. Maximum Sum With Exactly K Elements .java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        
+        Arrays.sort(nums);
+        
+        int max=nums[nums.length-1];
+        
+        // for(int i=0;i<k;i++){
+        
+        //     ans+=max++;
+            
+        // }
+
+        if(k==1)
+        return max;
+
+        if(k==2)
+        return max*2+1;
+        
+        return (max*k)+((k-1)*(k)/2);
+        
+    }
+}

c63e1e2cf968a91a7bdd26386b488ec3bf69c2c9
sushant-sinha <sinhasushant04@gmail.com>
2023-05-04 02:33:32 +0530

    new addition

--- Problems/987. Vertical Order Traversal of a Binary Tree.java
+++ Problems/987. Vertical Order Traversal of a Binary Tree.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 3ms ( 82.9% ) 43mb ( 18.50% )
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,6 +19,9 @@
  */
 class Solution {
 
+    // this treemap will map the y coordinate with: another treemap holding the sum of x and y coordinates
+    // the second treemap will be used for checking when there are multiple nodes with the same index
+    // since the sum of coordinates at a level (y coordinate) is always unique, we can use this as sum as key
     TreeMap<Integer, TreeMap<Integer,ArrayList<Integer>> > map=new TreeMap<>();
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
@@ -23,24 +30,20 @@ class Solution {
 
         List<List<Integer>> ans= new ArrayList<>();
 
-        // ArrayList<Integer> temp;
-
-        // traversing treemap
+        // traversing outer treemap
 
         for (Map.Entry<Integer, TreeMap<Integer,ArrayList<Integer>>> mapElement : map.entrySet()) {
-            // String key = mapElement.getKey();
 
             TreeMap<Integer, ArrayList<Integer>> temp=map.get(mapElement.getKey());
 
             ArrayList<Integer> intermediateAns= new ArrayList<>();
 
+            // traversing inner treemap
             for (Map.Entry<Integer, ArrayList<Integer>> mapElement1 : temp.entrySet()){
 
                 intermediateAns.addAll(temp.get(mapElement1.getKey()));
 
             }
-
-            // Collections.sort(temp);
 
             ans.add(intermediateAns);
  
@@ -57,15 +60,20 @@ class Solution {
 
         if(map.containsKey(y)){
             TreeMap<Integer, ArrayList<Integer>> temp=map.get(y);
-            
+
+            // if there are two elements at the same index
             if(temp.containsKey(x+y)){
+
                 ArrayList<Integer> temp1= temp.get(x+y);
                 temp1.add(root.val);
+
+                // sorting since elements are at the same index
                 Collections.sort(temp1);
                 
                 temp.put(x+y, temp1);
             }
 
+            // if there are no two elements at the same index, simply put the sum of x+y mapped to its root.val
             else{
                 ArrayList<Integer> temp1=new ArrayList<>();
                 temp1.add(root.val);
@@ -75,15 +83,15 @@ class Solution {
             map.put(y, temp);
         }
 
+        // if there is no existing element at the given y coordinate
         else{
+
             TreeMap<Integer, ArrayList<Integer>> temp=new TreeMap<>();
             ArrayList<Integer> temp1=new ArrayList<>();
             temp1.add(root.val);
             temp.put(x+y, temp1);
             map.put(y, temp);
         }
-
-        // System.out.println(root.val+" "+map);
 
         helper(root.left, x+1, y-1);
         helper(root.right, x+1, y+1);

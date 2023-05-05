b7915a7758a2343bce498c7df7aa5733ff72e776
sushant-sinha <sinhasushant04@gmail.com>
2023-05-06 02:28:14 +0530

    new addition

--- Problems/2657. Find the Prefix Common Array of Two Arrays.java
+++ Problems/2657. Find the Prefix Common Array of Two Arrays.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 5ms ( 74.88% ) 43.5mb ( 72.28% )
+
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
         
@@ -49,5 +53,31 @@ class Solution {
         
         return A;
         
+    }
+}
+
+// best solution from the submission tab
+
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int ans[] = new int[n];
+        int freq[] = new int[n+1];
+        int count = 0;
+        for(int i = 0;i<n;i++)
+        {
+            freq[A[i]]++;
+             if(freq[A[i]]==2)count++;
+            freq[B[i]]++;
+            if(freq[B[i]]==2)count++;
+             
+
+            ans[i] = count;
+
+
+        }
+
+
+        return ans; 
     }
 }

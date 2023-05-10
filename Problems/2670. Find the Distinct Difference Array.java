0dc50b0b6c7cde836d5d5c56a9ea7d9daf263a85
sushant-sinha <sinhasushant04@gmail.com>
2023-05-11 00:01:27 +0530

    new addition

---
+++ Problems/2670. Find the Distinct Difference Array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] distinctDifferenceArray(int[] nums) {
+        
+        int len=nums.length;
+        
+        int prefixAr[]=new int[len];
+        int suffixAr[]=new int[len];
+        
+        HashSet<Integer> ltor = new HashSet<>();
+        HashSet<Integer> rtol = new HashSet<>();
+        
+        for(int i=0;i<len-1;i++){
+            
+            ltor.add(nums[i]);
+            prefixAr[i]=ltor.size();
+            
+            rtol.add(nums[len-i-1]);
+            suffixAr[len-i-2]=rtol.size();
+            
+        }
+        
+        ltor.add(nums[len-1]);
+        prefixAr[len-1]=ltor.size();
+        
+        for(int i=0;i<len;i++){
+            // System.out.println(prefixAr[i]+" "+suffixAr[i]);
+            nums[i]=prefixAr[i]-suffixAr[i];
+            
+        }
+        
+        return nums;
+        
+    }
+}

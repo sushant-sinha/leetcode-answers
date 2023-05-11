57bb1c63526925c7fb9ccc1b9e144f5070317532
sushant-sinha <sinhasushant04@gmail.com>
2023-05-12 02:04:46 +0530

    new addition

--- Problems/1456. Maximum Number of Vowels in a Substring of Given Length.java
+++ Problems/1456. Maximum Number of Vowels in a Substring of Given Length.java
@@ -1,63 +1,36 @@
 // SUSHANT SINHA
 
-/*
-
-	my code has technically no errors
-	just doesnt passes the test cases with k > 999
-	but k > 999 is not allowed to run, but k > 999 is being tested when submitted
-
-*/
-
-// my solution
+// 14ms ( 82.33% ) 43mb ( 76.32% )
 
 class Solution {
     public int maxVowels(String s, int k) {
-        int ans=0;
-        
-        for(int i=0;i<=s.length()-k;i++){
-            
-            int temp = len(s.substring(i,i+k));
-            
-            ans = ans > temp ? ans : temp;
-            
-            if(ans==k)return ans;
+
+        int ans=0, cur=0;
+
+        for(int i=0;i<s.length();++i){
+
+            if((i-k>=0) && isVowel(s.charAt(i-k)) ){
+                cur--;
+            }
+
+            if(isVowel(s.charAt(i))){
+                cur++;
+            }
+
+            ans = cur>ans ? cur : ans;
+
         }
-        
+
         return ans;
         
     }
-    
-    static int len(String s){
-        
-        int l=0;
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='i')l++;
-        }
-        
-        return l;
-    }
-}
 
+    boolean isVowel(char c){
 
-// solutions given in the discuss tab
-// 8ms ( 95.49% ) 39.5mb ( 73.25% )
+        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+        return true;
 
-class Solution {
-    public int maxVowels(String s, int k) {
-        int vowel = 0;
-        for(int i =0; i< k; i++)
-            if(isVowel(s.charAt(i))) vowel++;
-        int max = vowel;
-        for(int i = k; i < s.length(); i++){
-            if(isVowel(s.charAt(i))) vowel++;
-            if(isVowel(s.charAt(i-k))) vowel--;
-            max = Math.max(max, vowel);
-        }
-        return max;
-    }
-    private boolean isVowel(char c){
-        if((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) return true;
         return false;
+
     }
 }

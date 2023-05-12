ab32d1bf144c5b10054685dc89a641309737c7f8
sushant-sinha <sinhasushant04@gmail.com>
2023-05-13 01:07:22 +0530

    new addition

---
+++ Problems/2671. Frequency Tracker.java
@@ -0,0 +1,97 @@
+class FrequencyTracker {
+
+    int ar[];
+    int freq[];
+
+    public FrequencyTracker() {
+        ar=new int[100001];
+        freq=new int[100001];
+    }
+    
+    public void add(int number) {
+        freq[ar[number]]--;
+        ar[number]++;
+        freq[ar[number]]++;
+    }
+    
+    public void deleteOne(int number) {
+
+        if(ar[number]>0){
+            
+            freq[ar[number]]--;
+            ar[number]--;
+            freq[ar[number]]++;
+            
+        }
+        
+    }
+    
+    public boolean hasFrequency(int frequency) {
+        
+        return freq[frequency] > 0 ;
+    }
+}
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker obj = new FrequencyTracker();
+ * obj.add(number);
+ * obj.deleteOne(number);
+ * boolean param_3 = obj.hasFrequency(frequency);
+ */
+
+ /*
+
+class FrequencyTracker {
+    
+    HashMap<Integer, Integer> freq;
+
+    public FrequencyTracker() {
+        
+        freq=new HashMap<>();
+        
+    }
+    
+    public void add(int number) {
+        
+        if(freq.containsKey(number)){
+            freq.put(number, freq.get(number)+1);
+        }
+        
+        else
+            freq.put(number, 1);
+        
+    }
+    
+    public void deleteOne(int number) {
+        
+        if(freq.containsKey(number)){
+            int val = freq.get(number);
+            
+            if(val==1)
+                freq.remove(number);
+            
+            else
+                freq.put(number, freq.get(number)-1);
+        }
+        
+    }
+    
+    public boolean hasFrequency(int frequency) {
+        
+        if(freq.containsValue(frequency)){
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker obj = new FrequencyTracker();
+ * obj.add(number);
+ * obj.deleteOne(number);
+ * boolean param_3 = obj.hasFrequency(frequency);
+ */

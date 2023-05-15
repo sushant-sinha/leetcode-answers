958af35ee633fe2a0fbe4caf809dc11beeb2ec79
sushant-sinha <sinhasushant04@gmail.com>
2023-05-16 00:22:01 +0530

    new addition

---
+++ Problems/2679. Sum in a Matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int matrixSum(int[][] nums) {
+        
+        int ans=0;
+        
+        for(int i[]:nums)
+            Arrays.sort(i);
+        
+        for(int i=nums[0].length-1;i>=0;i--){
+            
+            int tempMax=0;
+            
+            for(int j[]:nums)
+                tempMax= tempMax>j[i] ? tempMax : j[i];
+            
+            ans+=tempMax;
+        }
+        
+        return ans;
+        
+    }
+}

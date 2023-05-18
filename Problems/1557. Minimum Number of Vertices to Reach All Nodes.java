94c1fea345de9eeabb551df69ca23af36aeab2e0
sushant-sinha <sinhasushant04@gmail.com>
2023-05-18 23:25:15 +0530

    new addition

---
+++ Problems/1557. Minimum Number of Vertices to Reach All Nodes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+
+        List<Integer> ans= new ArrayList<>();
+        HashSet<Integer> seen = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            seen.add(i);
+        }
+
+        Integer temp=new Integer(0);
+
+        for(int i=0;i<edges.size();i++){
+            
+            // ans.remove(Integer.valueOf(edges.get(i).get(1)));
+            // above or this
+            temp=edges.get(i).get(1);
+            if(seen.contains(temp))
+            seen.remove(temp);
+
+        }
+
+        for(int i:seen)
+            ans.add(i);
+
+        return ans;
+        
+    }
+}

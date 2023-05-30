b96d6d98a9bc26b2c740741ebc5578848fce1e39
sushant-sinha <sinhasushant04@gmail.com>
2023-05-31 02:19:35 +0530

    new addition

--- Problems/705. Design HashSet.java
+++ Problems/705. Design HashSet.java
@@ -1,43 +1,32 @@
 // SUSHANT SINHA
-// 1107ms ( 5.02% ) 57.1mb ( 38.19% )
 
-// using arraylist
+// 17ms ( 66.8% ) 48.3mb ( 93.87% )
 
 class MyHashSet {
-    
-    List<Integer> arr;
-    
+
+    boolean set[];
+
     public MyHashSet() {
+
+        set=new boolean[1000001];
         
-        arr=new ArrayList<Integer>();
     }
     
     public void add(int key) {
+
+        set[key]=true;
         
-        if(!arr.contains(key)){
-            arr.add(key);
-        }
     }
     
     public void remove(int key) {
+
+        set[key]=false;
         
-        int index=arr.indexOf(key);
-        
-        if(index!=-1){
-            arr.remove(index);
-        }
     }
     
     public boolean contains(int key) {
+
+        return set[key];
         
-        return arr.contains(key);
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */

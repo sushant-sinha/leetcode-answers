6f72d86ab64e8f9723822c35d28351d7277cd1f1
sushant-sinha <sinhasushant04@gmail.com>
2023-06-05 01:23:34 +0530

    new addition

--- Problems/2710. Remove Trailing Zeros From a String.java
+++ Problems/2710. Remove Trailing Zeros From a String.java
@@ -1,3 +1,22 @@
 // SUSHANT SINHA
 
 // 1ms ( 100% ) 43.9mb ( 60.12% )
+
+class Solution {
+    public String removeTrailingZeros(String num) {
+        
+        int index=num.length();
+        
+        for(int i=index-1 ;i>=0 ;i--){
+            if(num.charAt(i)=='0'){
+                index=i;
+            }
+            
+            else
+                break;
+        }
+        
+        return num.substring(0,index);
+        
+    }
+}

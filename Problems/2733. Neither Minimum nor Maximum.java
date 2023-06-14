be55d3409e2f0c52f49e1f99017c818c5fec4513
sushant-sinha <sinhasushant04@gmail.com>
2023-06-14 22:27:59 +0530

    new addition

--- Problems/2733. Neither Minimum nor Maximum.java
+++ Problems/2733. Neither Minimum nor Maximum.java
@@ -1,18 +1,21 @@
+// SUSHANT SINHA
+
+// 13ms ( 59.99% ) 44mb ( 33.75% )
+
+// instead of using Arrays.sort do this 👇🏻
+// iterate through the entire nums
+// in one pass we can find the min and max
+// in the same pass itself, we can maintain another variable which has the value smaller than max (aka largest value smaller than max) or larger than min (aka smallest value larger than min)
+
 class Solution {
     public int findNonMinOrMax(int[] nums) {
         
-        if(nums.length==2)
+        if(nums.length<=2)
             return -1;
         
         Arrays.sort(nums);
         
-        for(int i=1; i<nums.length-1; i++){
-            if(nums[i]!=nums[0] || nums[i]!=nums[nums.length-1])
-                return nums[i];
-        }
-        
-        
-        return -1;
+        return nums[1];
         
     }
 }

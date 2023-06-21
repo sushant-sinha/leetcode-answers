fe57036498cc663016315f41bed081e357483ea8
sushant-sinha <sinhasushant04@gmail.com>
2023-06-21 23:59:56 +0530

    new addition

---
+++ Problems/2739. Total Distance Traveled.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int distanceTraveled(int mainTank, int additionalTank) {
+        
+        int totalDist=0;
+        
+        while(mainTank>=5){
+            
+            totalDist+=50;
+            
+            if(additionalTank>0){
+                mainTank-=4;
+                additionalTank--;
+            }
+            
+            else{
+                mainTank-=5;
+            }
+            
+        }
+        
+        totalDist+=mainTank*10;
+        
+        return totalDist;
+        
+    }
+}

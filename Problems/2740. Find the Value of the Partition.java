04a0ada6f308abb57b7ea26bf2160d3a97778b4d
sushant-sinha <sinhasushant04@gmail.com>
2023-06-24 03:02:24 +0530

    new addition

--- Problems/2740. Find the Value of the Partition.java
+++ Problems/2740. Find the Value of the Partition.java
@@ -2,3 +2,20 @@
 
 // we are not concerned about the subarrays
 // we just want to find the pair of numbers which will have the least diff
+
+class Solution {
+    public int findValueOfPartition(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        int diff=Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.length-1;i++){
+            int temp= Math.abs(nums[i]-nums[i+1]);
+            diff = diff > temp ? temp : diff;
+        }
+        
+        return diff;
+        
+    }
+}

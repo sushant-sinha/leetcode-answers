44f16555edf8192cdfa3805dfad360e47734ef9c
sushant-sinha <sinhasushant04@gmail.com>
2023-06-26 21:00:39 +0530

    new addition

--- Problems/2744. Find Maximum Number of String Pairs.java
+++ Problems/2744. Find Maximum Number of String Pairs.java
@@ -3,3 +3,31 @@
 // 8ms ( 33.33% ) 43.5mb ( 33.33% )
 
 // string concatenation could be optimized
+
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        
+        int ans=0;
+        
+        HashMap<String, Integer> map=new HashMap<>();
+        
+        for(String s: words){
+            
+            String rev=""+(s.charAt(1))+(s.charAt(0));
+            // System.out.println(rev);
+            
+            if(map.containsKey(rev) && map.get(rev)==1){
+                map.put(rev, 2);
+                ans++;
+            }
+            
+            else{
+                map.put(s, 1);
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

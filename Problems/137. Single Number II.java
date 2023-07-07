7992d16c8310db9e58a9522f2ea1cac61e7d4e02
sushant-sinha <sinhasushant04@gmail.com>
2023-07-07 19:57:32 +0530

    new addition

---
+++ Problems/137. Single Number II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        HashMap<Integer, Integer> map=new HashMap<>();
+
+        for(int i:nums){
+
+            if(map.containsKey(i)){
+                if(map.get(i)==2){
+                    map.remove(new Integer(i));
+                }
+                else{
+                    map.put(i, map.get(i)+1);
+                }
+            }
+
+            else{
+                map.put(i,1);
+            }
+
+        }
+
+        Map.Entry<Integer, Integer> entry = map.entrySet().iterator().next();
+        
+        return entry.getKey();
+    }
+}

8301df7ed3f77f7638622c4b82b6f4ec03227f24
sushant-sinha <sinhasushant04@gmail.com>
2023-07-09 23:33:12 +0530

    new addition

---
+++ Problems/2765. Longest Alternating Subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int alternatingSubarray(int[] nums) {
+        
+        int ans=-1;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            int tempans=-1;
+            int power=0;
+            
+            for(int j=i+1;j<nums.length;j++){
+                
+                // System.out.println(j+"="+nums[j]+" "+(j-1)+"="+nums[j-1]+" "+Math.pow(-1, power)+" condition="+(nums[j]-nums[j-1]==Math.pow(-1, power)));
+                if(nums[j]-nums[j-1]==Math.pow(-1, power)){
+                    tempans++;
+                    power++;
+                }
+                
+                else
+                    break;
+            }
+            
+            ans = tempans > ans ? tempans : ans;
+            
+        }
+        
+        return ans==-1 ? -1 : ans+2;
+        
+    }
+}

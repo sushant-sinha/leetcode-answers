aa5c9f30431507b7f61cbad6dd0c8c2cc8d40051
sushant-sinha <sinhasushant04@gmail.com>
2023-07-14 14:42:04 +0530

    new addition

---
+++ Problems/2433. Find The Original Array of Prefix Xor.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findArray(int[] pref) {
+
+        if(pref.length==0)
+            return pref;
+
+        int ans[]=new int[pref.length];
+
+        for(int i=1;i<pref.length;i++){
+            ans[i]=pref[i-1]^pref[i];
+        }
+
+        ans[0]=pref[0];
+
+        return ans;
+        
+    }
+}

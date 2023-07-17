551791275a775151e864c36620856c430608221a
sushant-sinha <sinhasushant04@gmail.com>
2023-07-17 07:19:40 +0530

    new addition

---
+++ Problems/2778. Sum of Squares of Special Elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        
+        int ans=0, n=nums.length;
+        
+        for(int i=0;i<n;i++){
+            if(n%(i+1)==0)
+                ans+=(nums[i]*nums[i]);
+        }
+        
+        return ans;
+        
+    }
+}

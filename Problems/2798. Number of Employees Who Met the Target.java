35fa35b7b600a907f85873c1b43351adab5d2059
sushant-sinha <sinhasushant04@gmail.com>
2023-07-31 02:39:19 +0530

    new addition

---
+++ Problems/2798. Number of Employees Who Met the Target.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        
+        Arrays.sort(hours);
+        
+        int ans=0;
+        
+        for(int i=hours.length-1; i>=0; i--){
+            
+            if(hours[i]>=target)
+                ans++;
+            else
+                break;
+        }
+        
+        return ans;
+        
+    }
+}

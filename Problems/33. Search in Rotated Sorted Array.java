30ac4027fd940d3a6354b8228abb14b8e849f2e1
sushant-sinha <sinhasushant04@gmail.com>
2023-08-10 12:13:50 +0530

    new addition

--- Problems/33. Search in Rotated Sorted Array.java
+++ Problems/33. Search in Rotated Sorted Array.java
@@ -1,17 +1,69 @@
-// SUSHANT SINHA
-
-// 0ms ( 100% ) 37.9mb ( 99.11% )
-// followup part is important
-
 class Solution {
     public int search(int[] nums, int target) {
-                
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target)
-                return i;
+
+        if(nums.length==1){
+            if(nums[0]==target)
+                return 0;
+            else
+                return -1;
         }
+
+        // find the starting point of the array
+
+        int start=0;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
+                start=i;
+                break;
+            }
+        }
+
+        // start binary search for the broken two sets
+
+        // both start and end are inclusive ranges for the function helper
+        // from [0 - start]
+        int index=helper(0, start, nums, target);
+
+        if(index>-1)
+            return index;
+
+        // from [start+1 - end]
+        index=helper(start+1, nums.length-1, nums, target);
         
-        return -1;
+        return index;
         
+    }
+
+    int helper(int start, int end, int[] nums, int target){
+
+        if(nums[start]==target){
+            return start;
+        }
+
+        if(end-start==1){
+            if(nums[start]==target)
+                return start;
+
+            if(nums[end]==target)
+                return end;
+
+            else 
+                return -1;
+        }
+
+        int mid=(start+end)/2;
+        int ans=-1;
+
+        if(nums[mid]>=target && nums[start]<=target){
+            ans=helper(start, mid, nums, target);
+        }
+
+        else if(nums[mid]<target && nums[end]>=target){
+            ans=helper(mid+1, end, nums, target);
+        }
+
+        return ans;
+
     }
 }

5cb302bbbb829d2d40beef4f2aa7dd16711e6bb4
sushant-sinha <sinhasushant04@gmail.com>
2023-08-25 21:43:23 +0530

    new addition

---
+++ Problems/767. Reorganize String.java
@@ -0,0 +1,64 @@
+class Solution {
+    public String reorganizeString(String s) {
+
+        StringBuilder ans=new StringBuilder();
+
+        int count[]=new int[26];
+
+        int maxFreq=0;
+
+        for(char c:s.toCharArray()){
+
+            count[c-'a']++;
+
+            if(maxFreq<count[c-'a']){
+
+                maxFreq=count[c-'a'];
+
+                ans.delete(0,1);
+                ans.append(c);
+            }
+
+        }
+
+        // System.out.println(ans);
+
+        count[ans.charAt(0)-'a']--;
+
+        while(ans.length()!=s.length()){
+
+            char c=findMaxNotEqualToC(count, ans.charAt(ans.length()-1));
+
+            if(c==' ')
+                return "";
+
+            else{
+                ans.append(c);
+                count[c-'a']--;
+            }
+
+        }
+
+        // System.out.println(Arrays.toString(count));
+        return String.valueOf(ans);
+        
+    }
+
+    char findMaxNotEqualToC(int count[], char c){
+
+        char ans=' ';
+        int maxFreq=0;
+
+        for(int i=0;i<26;i++){
+
+            if(maxFreq<count[i] && (char)(i+'a')!=c){
+                maxFreq=count[i];
+                ans=(char)(i+'a');
+            }
+
+        }
+
+        return ans;
+
+    }
+}

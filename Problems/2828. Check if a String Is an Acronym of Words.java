e3740a8f30d761d7be44677d72824894db4c2732
sushant-sinha <sinhasushant04@gmail.com>
2023-08-31 00:02:41 +0530

    new addition

--- Problems/2828. Check if a String Is an Acronym of Words.java
+++ Problems/2828. Check if a String Is an Acronym of Words.java
@@ -1,3 +1,23 @@
 // SUSHANT SINHA
 
 // 1ms ( 100% ) 43.5mb ( 88.95% )
+
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        
+        int len=s.length();
+        
+        if(words.size()!=len)
+            return false;
+        
+        for(int i=0;i<len;i++){
+            
+            if(words.get(i).charAt(0)!=s.charAt(i))
+                return false;
+            
+        }
+            
+        return true;
+        
+    }
+}

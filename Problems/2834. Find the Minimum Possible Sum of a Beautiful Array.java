8aade9c47e4dcc98843839e65b6829950d88f86e
sushant-sinha <sinhasushant04@gmail.com>
2023-09-05 10:29:19 -0400

    new addition

---
+++ Problems/2834. Find the Minimum Possible Sum of a Beautiful Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long minimumPossibleSum(int n, int target) {
+
+        if(n==1)
+            return 1;
+
+        HashSet<Long> seen=new HashSet<>();
+        seen.add((long)1);
+
+        long sum=1, cur=1;
+
+        for(int i=1;i<n;i++){
+            cur++;
+
+            cur=findNext(seen, cur, target);
+            seen.add(cur);
+
+            sum+=cur;
+
+        }
+
+        return sum;
+        
+    }
+
+    long findNext(HashSet<Long> seen, long cur, int target){
+
+        if(!seen.contains((target-cur)))
+            return cur;
+
+        while(seen.contains(target-cur)){
+            cur++;
+        }
+
+        return cur;
+    }
+}

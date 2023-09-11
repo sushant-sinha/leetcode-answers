c1c192e96e5dd15b78ee4e9265a5e6a6a1434ac7
sushant-sinha <sinhasushant04@gmail.com>
2023-09-11 16:28:21 -0400

    new addition

--- Problems/2349. Design a Number Container System.java
+++ Problems/2349. Design a Number Container System.java
@@ -1,3 +1,55 @@
+// using arraylist was my initial intuition, but the couldn't have used more brain cells
+// found the solution which uses TreeSet instead of ArrayList
+
+class NumberContainers {
+
+    // mapping of the number and the indexes
+    
+    HashMap<Integer, ArrayList<Integer>> map;
+    
+    // this is the HashMap that maps the indexes with the values tored within it
+    HashMap<Integer, Integer> indnum;
+    
+    public NumberContainers() {
+        map=new HashMap<>();
+        indnum=new HashMap<>();
+    }
+    
+    public void change(int index, int number) {
+        
+        
+        if(indnum.containsKey(index)){
+            
+            int num=indnum.get(index);
+            ArrayList<Integer> i=map.get(num);
+            
+            i.remove(index);
+            
+            if(i.size()==0){
+                map.remove(num);
+            }
+            
+            else{
+                map.put(num, i);
+            }
+        }
+        
+        ArrayList<Integer> i=map.put
+        
+    }
+    
+    public int find(int number) {
+        
+        if(map.containsKey(number)){
+            
+            return map.get(number).get(0);
+            
+        }
+        
+        return -1;
+    }
+}
+
 /**
  * Your NumberContainers object will be instantiated and called as such:
  * NumberContainers obj = new NumberContainers();
@@ -7,7 +59,9 @@
 
 // found this solution from the discuss section
 
-// 173.5mb ( 23.19% )
+/*
+
+// 173.5mb ( 58.53% ) 173.5mb ( 23.19% )
 
 
 class NumberContainers {
@@ -42,3 +96,5 @@ class NumberContainers {
         return -1;
     }
 }
+
+*/

4bef54ebdabb78b75327dbbc3d642a3d4cc5de9d
sushant-sinha <sinhasushant04@gmail.com>
2023-09-14 01:19:42 -0400

    new addition

---
+++ Problems/2848. Points That Intersect With Cars.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfPoints(List<List<Integer>> nums) {
+        
+        HashSet<Integer> seen=new HashSet<>();
+        
+        for(List<Integer> i: nums){
+            
+            for(int x=i.get(0); x<=i.get(1); x++)
+                seen.add(x);
+        }
+        
+        // System.out.println(seen);
+        
+        return seen.size();
+        
+    }
+}

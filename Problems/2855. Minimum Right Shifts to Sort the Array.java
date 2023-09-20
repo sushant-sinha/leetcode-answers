b019446629912888f47cea7b7c15e31ca6d05535
sushant-sinha <sinhasushant04@gmail.com>
2023-09-20 01:33:22 -0400

    new addition

---
+++ Problems/2855. Minimum Right Shifts to Sort the Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumRightShifts(List<Integer> nums) {
+        
+        if(nums.size()==1)
+            return 0;
+        
+        // find the point from where the array should be sorted. i.e find the minimum element in the entire array
+        
+        int minIndex=0;
+        int minVal=Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.size();i++)
+            if(minVal>nums.get(i)){
+                minVal=nums.get(i);
+                minIndex=i;
+            }
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums.get((minIndex+i)%nums.size())<nums.get((minIndex+i-1)%nums.size()) )
+                return -1;
+        }
+        
+        return (nums.size()-minIndex)%nums.size();
+    }
+}

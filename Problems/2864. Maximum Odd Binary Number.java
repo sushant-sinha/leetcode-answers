2b8126b65fee9a63b14b41ff195a0048cbf19ae6
sushant-sinha <sinhasushant04@gmail.com>
2023-09-25 21:44:11 -0400

    new addition

---
+++ Problems/2864. Maximum Odd Binary Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        
+        int count1=0;
+        
+        for(char c:s.toCharArray())
+            if(c=='1')
+                count1++;
+        
+        int count0=s.length()-count1;
+        
+        StringBuilder ans=new StringBuilder();
+        
+        for(int i=0;i<count1-1;i++){
+            ans.append('1');
+        }
+        for(int i=0;i<count0;i++){
+            ans.append('0');
+        }
+        
+        ans.append('1');
+        
+        return ans.toString();
+        
+    }
+}

69087b0630f5ab753e5ded094cf81002a724da24
sushant-sinha <sinhasushant04@gmail.com>
2023-10-11 00:13:15 -0400

    new addition

--- Problems/2873. Maximum Value of an Ordered Triplet I.java
+++ Problems/2873. Maximum Value of an Ordered Triplet I.java
@@ -1,3 +1,10 @@
+// SUSHANT SINHA
+
+// brute force solution (input size too small)
+// check 2874. Maximum Value of an Ordered Triplet II for optimised solution
+
+// 3ms ( 66.47% ) 40.7mb ( 54.8% )
+
 class Solution {
     public long maximumTripletValue(int[] nums) {
         

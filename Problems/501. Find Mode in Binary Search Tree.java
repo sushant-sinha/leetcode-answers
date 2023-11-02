a11e370cf8135281f3f92f71b025186500a3274c
sushant-sinha <sinhasushant04@gmail.com>
2023-11-01 22:04:16 -0400

    new addition

---
+++ Problems/501. Find Mode in Binary Search Tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int ar[]=new int[200001];
+
+    public int[] findMode(TreeNode root) {
+
+        helper(root);
+
+        int maxFreq=0;
+
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        for(int i=0;i<ar.length;i++){
+
+            if(ar[i]>maxFreq){
+                ans.clear();
+                ans.add(i-100000);
+                maxFreq=ar[i];
+            }
+
+            else if(ar[i]==maxFreq){
+                ans.add(i-100000);
+            }
+        }
+
+        // System.out.println(ans);
+
+        int ret[]=new int[ans.size()];
+
+        for(int i=0;i<ret.length;i++)
+            ret[i]=ans.get(i);
+
+        return ret;
+        
+    }
+
+    void helper(TreeNode root){
+
+        if(root==null)
+        return;
+
+        else{
+            ar[root.val+100000]++;
+            helper(root.right);
+            helper(root.left);
+
+        }
+
+    }
+}

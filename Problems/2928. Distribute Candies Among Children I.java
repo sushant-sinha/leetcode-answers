9a11b259af8aa6b669c909e78f95730865ba2377
sushant-sinha <sinhasushant04@gmail.com>
2023-11-11 22:18:18 -0400

    new addition

---
+++ Problems/2928. Distribute Candies Among Children I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int distributeCandies(int n, int limit) {
+        
+        int ans=0;
+        
+        for(int i=0;i<=limit && i<=n;i++){
+            
+            for(int j=0;j<=limit && (j<=n-i); j++){
+                
+                if(n-i-j<=limit){
+                    ans++;
+                    // System.out.println(i+" "+j+" "+(n-j-i));
+                }
+                
+            }
+        }
+        
+        return ans;
+        
+    }
+}

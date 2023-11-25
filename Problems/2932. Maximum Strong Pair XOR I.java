d1d673a71b734a8544cb9245f88e6fb686754db5
sushant-sinha <sinhasushant04@gmail.com>
2023-11-24 20:49:28 -0400

    new addition

---
+++ Problems/2932. Maximum Strong Pair XOR I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        
+        int ans=0;
+        
+        for(int i=0;i<nums.length;i++){
+            
+            for(int j=i;j<nums.length;j++){
+                
+                if(Math.abs(nums[i]-nums[j]) <= Math.min(nums[i], nums[j])){
+                    ans = Math.max(ans, nums[i]^nums[j]);
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+}

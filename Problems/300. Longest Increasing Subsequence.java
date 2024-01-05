3a71c4a2f0d702d361360205b6835c813fb8a4e8
sushant-sinha <sinhasushant04@gmail.com>
2024-01-05 19:59:08 -0400

    new addition

---
+++ Problems/300. Longest Increasing Subsequence.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+
+        TreeMap<Integer, Integer> map=new TreeMap<>();
+
+        int ans=1;
+
+        map.put(nums[0], 1);
+
+        for(int i=1; i<nums.length; i++){
+
+            if(map.lowerKey(nums[i])!=null){
+                int key=map.lowerKey(nums[i]);
+                // System.out.println("cur key= "+nums[i]+" just min key= "+key);
+
+                int val=0;
+
+                for(Map.Entry<Integer, Integer> e: map.entrySet()){
+
+                    val=Math.max(e.getValue(), val);
+
+                    if(e.getKey()==key)
+                    break;
+
+
+                }
+
+                val++;
+
+                ans=Math.max(ans, val);
+
+                map.put(nums[i], val);
+            }
+
+            else{
+                map.put(nums[i], 1);
+            }
+
+        }
+
+        return ans;
+        
+    }
+}

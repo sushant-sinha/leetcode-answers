4cf6a1efdfe3ba11289fcd82e2b29f7a71be4432
sushant-sinha <sinhasushant04@gmail.com>
2024-01-07 08:08:54 -0400

    new addition

--- Problems/300. Longest Increasing Subsequence.java
+++ Problems/300. Longest Increasing Subsequence.java
@@ -1,6 +1,7 @@
 // SUSHANT SINHA
 
 // 172ms ( 12.41% ) 44.80mb ( 15.24% )
+// not O(n lgn) because of the for loop needed for traversing the map
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
@@ -13,16 +14,20 @@ class Solution {
 
         for(int i=1; i<nums.length; i++){
 
+            // finding the key lower than the current val in array and which has the maximum number of elements smaller than it
             if(map.lowerKey(nums[i])!=null){
 
+                // greatest key strictly smaller than the current array element
                 int key=map.lowerKey(nums[i]);
 
                 int val=0;
 
+                // finding the element which has the maximum value and smaller than the 'key'
                 for(Map.Entry<Integer, Integer> e: map.entrySet()){
 
                     val=Math.max(e.getValue(), val);
 
+                    // break when the biggest key is seen
                     if(e.getKey()==key)
                         break;
 

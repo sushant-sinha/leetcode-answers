da2f35b7d04fe1651e92711167a5d4b14255860f
sushant-sinha <sinhasushant04@gmail.com>
2024-01-12 00:50:10 -0400

    new addition

---
+++ Problems/763. Partition Labels.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        int index[]=new int[26];
+
+        for(int i=0;i<s.length();i++)
+            index[s.charAt(i)-'a']=i;
+
+        for(int i=0;i<s.length();i++){
+
+            int end=index[s.charAt(i)-'a'];
+
+            for(int j=i;j<end;j++){
+                int newEnd=index[s.charAt(j)-'a'];
+
+                if(newEnd>end)
+                    end=newEnd;
+            }
+
+            ans.add(end-i+1);
+            i=end;
+
+        }
+
+        return ans;
+
+    }
+}

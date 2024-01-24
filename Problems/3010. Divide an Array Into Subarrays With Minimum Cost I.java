5dae03c84472c940c9a50dcc2b133b17f672c641
sushant-sinha <sinhasushant04@gmail.com>
2024-01-24 13:13:53 -0400

    new addition

--- Problems/3010. Divide an Array Into Subarrays With Minimum Cost I.java
+++ Problems/3010. Divide an Array Into Subarrays With Minimum Cost I.java
@@ -22,3 +22,29 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int minimumCost(int[] nums) {
+        
+        int ans=nums[0];
+        
+        int min1=Integer.MAX_VALUE;
+        int min2=min1;
+        
+        for(int i=1;i<nums.length;i++){
+
+            if(nums[i]<=min1){
+                min2=min1;
+                min1=nums[i];
+            }
+
+            else if(nums[i]<=min2){
+                min2=nums[i];
+            }
+
+        }
+        
+        return ans+min1+min2;
+        
+    }
+}

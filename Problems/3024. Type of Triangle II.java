6306248c7e6929b9709519ed925bf6ee602638f2
sushant-sinha <sinhasushant04@gmail.com>
2024-02-11 00:08:54 -0400

    new addition

---
+++ Problems/3024. Type of Triangle II.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        
+        if(!isValidTriangle(nums))
+            return "none";
+        
+        if(nums[0]==nums[   1] && nums[1]==nums[2])
+                return "equilateral";
+        
+        if(nums[0]==nums[1] || nums[1]==nums[2] || nums[2]==nums[0])
+            return "isosceles";
+        
+        else
+            return "scalene";
+        
+    }
+    
+    boolean isValidTriangle(int ar[]){
+        
+        boolean flag=true;
+        
+        flag = flag && (ar[0]+ar[1]>ar[2]);
+        flag = flag && (ar[1]+ar[2]>ar[0]);
+        flag = flag && (ar[0]+ar[2]>ar[1]);
+        
+        flag = flag && (Math.abs(ar[0]-ar[1])<ar[2]);
+        flag = flag && (Math.abs(ar[1]-ar[2])<ar[0]);
+        flag = flag && (Math.abs(ar[0]-ar[2])<ar[1]);
+        
+        return flag;
+        
+    }
+}

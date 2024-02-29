e8cbc3e970d880273b5a710e5c2af22a6be38256
sushant-sinha <sinhasushant04@gmail.com>
2024-02-29 00:15:43 -0400

    new addition

---
+++ Problems/513. Find Bottom Left Tree Value.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    int depth=0;
+
+    int val=0;
+
+    public int findBottomLeftValue(TreeNode root) {
+
+        val=root.val;
+
+        helper(root, 0);
+
+        return val;
+        
+    }
+
+    void helper(TreeNode root, int level){
+        
+        // System.out.println("val = "+val+" level = "+level);
+
+        if(level>depth){
+            depth++;
+            val=root.val;
+        }
+
+        
+        if(root.left!=null)
+            helper(root.left, level+1);
+
+        if(root.right!=null)
+            helper(root.right, level+1);
+
+    }
+}

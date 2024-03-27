bcd13215531b7d98faddcb3a2bf59f5ac5193efc
sushant-sinha <sinhasushant04@gmail.com>
2024-03-26 22:16:25 -0400

    new addition

---
+++ Problems/3079. Find the Sum of Encrypted Integers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int sumOfEncryptedInt(int[] nums) {
+
+        int sum = 0;
+
+        for(int i: nums)
+            sum+=encrypt(i);
+
+        return sum;
+        
+    }
+
+    int encrypt(int i){
+
+        int len=0, max=-1;
+
+        while(i!=0){
+            max = max > i%10 ? max : i%10;
+            len++;
+            i/=10;
+        }
+
+        int ans=0;
+
+        while(len!=0){
+            ans*=10;
+            ans+=max;
+            len--;
+        }
+
+        return ans;
+
+    }
+}

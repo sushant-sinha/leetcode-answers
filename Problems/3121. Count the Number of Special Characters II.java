45f30032a6feb0220982a332cfc36003b968993f
sushant-sinha <sinhasushant04@gmail.com>
2024-04-23 17:50:28 -0400

    new addition

---
+++ Problems/3121. Count the Number of Special Characters II.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+
+        // HashSet<Integer> seenUpperCase=new HashSet<>();
+
+        int firstPositionOfUpperCase[]=new int[26];
+        Arrays.fill(firstPositionOfUpperCase, -1);
+
+        int lastPositionOfLowerCase[]=new int[26];
+        Arrays.fill(lastPositionOfLowerCase, -1);
+
+        int ans=0;
+
+        char ar[]=word.toCharArray();
+
+        for(int i=0;i<ar.length;i++){
+
+            char c=ar[i];
+
+            if(Character.isUpperCase(c)){
+                if(firstPositionOfUpperCase[c-'A']==-1)
+                    firstPositionOfUpperCase[c-'A']=i;
+            }
+
+            else{
+                // if it is lowerCase
+                lastPositionOfLowerCase[c-'a']=i;
+
+            }
+
+        }
+
+        // System.out.println(Arrays.toString(lastPositionOfLowerCase));
+        // System.out.println(Arrays.toString(firstPositionOfUpperCase));
+
+        for(int i=0;i<26;i++){
+
+            if(lastPositionOfLowerCase[i]!=-1 && firstPositionOfUpperCase[i]!=-1 && lastPositionOfLowerCase[i]<firstPositionOfUpperCase[i])
+                ans++;
+        }
+        return ans;
+        
+    }
+}

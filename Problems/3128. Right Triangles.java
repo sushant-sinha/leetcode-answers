fd62ebab452d14d3a9424d1f7e386883fd2b52e6
sushant-sinha <sinhasushant04@gmail.com>
2024-05-07 08:58:17 -0400

    new addition

--- Problems/3128. Right Triangles.java
+++ Problems/3128. Right Triangles.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 16ms ( 66.67% ) 146.14mb ( 33.33% )
+
 class Solution {
     public long numberOfRightTriangles(int[][] grid) {
 
@@ -14,6 +18,7 @@ class Solution {
             }
         }
 
+        // be careful with the return type.... int wont accomodate all the combinations
         long ans=0;
 
         for(int i=0;i<grid.length;i++){
@@ -21,6 +26,11 @@ class Solution {
             for(int j=0;j<grid[0].length;j++){
 
                 if(grid[i][j]==1){
+
+                    // a right angle can be formed with any 1 in the same row and col.
+                    // they dont need to be adjacent. Therefore, any 1 can be paired with any 1 in the same rorw and col
+                    // first example is misleading and makes you think that they have to be adjacent
+                    // third example makes you think that wraparound is allowed😂
                     ans+=(rowOneCount[i]-1)*(colOneCount[j]-1);
                 }
             }

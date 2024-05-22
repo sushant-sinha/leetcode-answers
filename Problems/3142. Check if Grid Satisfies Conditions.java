a934dd8d7a5055e060b736a7bfcd2ca5b541281e
sushant-sinha <sinhasushant04@gmail.com>
2024-05-22 18:25:55 -0400

    new addition

---
+++ Problems/3142. Check if Grid Satisfies Conditions.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean satisfiesConditions(int[][] grid) {
+
+        for(int i=0;i<grid.length;i++){
+
+            for(int j=0;j<grid[0].length;j++){
+
+                if(i+1<grid.length && grid[i][j]!=grid[i+1][j]){
+                    return false;
+                }
+
+                if(j+1<grid[0].length && grid[i][j]==grid[i][j+1])
+                    return false;
+            }
+        }
+
+        return true;
+        
+    }
+}

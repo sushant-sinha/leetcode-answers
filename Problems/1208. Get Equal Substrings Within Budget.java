99722f5ec3f87d74214c5ba74a16af839a2f5559
sushant-sinha <sinhasushant04@gmail.com>
2024-06-01 17:24:30 -0400

    new addition

--- Problems/1208. Get Equal Substrings Within Budget.java
+++ Problems/1208. Get Equal Substrings Within Budget.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 5ms ( 97.71% ) 43.21mb ( 31.10% )
+
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
 
@@ -7,16 +11,25 @@ class Solution {
             cost[i]=Math.abs(s.charAt(i)-t.charAt(i));
         }
 
+        // System.out.println(Arrays.toString(cost));
+
+        // sliding window (window between start and i of for loop)
         int ans=0, cur=0, start=0;
 
         for(int i=0;i<cost.length;i++){
 
             cur+=cost[i];
             
+            // if addition of this element exceeds the maxCost
             if(cur>maxCost){
+                // start removing elements from the beginning of the array
                 while(cur>maxCost){
                     cur-=cost[start];
                     start++;
+
+                    // when we hit the current element, that implies that the current element itself is causing the problem
+                    // i.e. this element is having cost more than maxCost(ofc) and we have to remove this element too
+                    // reset cost and start the window from start+1
                     if(start==i){
                         start++;
                         cur=0;

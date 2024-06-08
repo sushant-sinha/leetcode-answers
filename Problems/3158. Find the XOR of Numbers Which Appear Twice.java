e639bf49176945f2e9efca48ae9859fa80a014e8
sushant-sinha <sinhasushant04@gmail.com>
2024-06-08 00:27:43 -0400

    new addition

--- Problems/3158. Find the XOR of Numbers Which Appear Twice.java
+++ Problems/3158. Find the XOR of Numbers Which Appear Twice.java
@@ -1,3 +1,13 @@
+// SUSHANT SINHA
+
+// 2ms ( 74.54% ) 43.47mb ( 18.48% )
+
+/*
+could have used more memory efficient way of hashing:
+    1) boolean array
+    2) int array, as 1<=nums[i]<=50
+*/
+
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
 

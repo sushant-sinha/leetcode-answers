f3a6334bde337b837e7217b021c477f53d6fc5d9
sushant-sinha <sinhasushant04@gmail.com>
2024-07-01 00:24:56 -0400

    new addition

--- Problems/3192. Minimum Operations to Make Binary Array Elements Equal to One II.java
+++ Problems/3192. Minimum Operations to Make Binary Array Elements Equal to One II.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 8ms ( 12.50% ) 56.82mb ( 66.08% )
+
 class Solution {
     public int minOperations(int[] nums) {
 
@@ -18,3 +22,17 @@ class Solution {
         
     }
 }
+
+/*
+
+input: [0,1,1,0,1]
+
+flip at index 0: 1 0 0 1 0-> flipCounter: 1
+
+flip at index 1: 1 1 1 0 1-> flipCounter: 2
+
+flip at index 3: 1 1 1 1 0-> flipCounter: 3
+
+flip at index 4: 1 1 1 1 1-> flipCounter: 4
+
+*/

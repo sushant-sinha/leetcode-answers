9e5999d259f98592182fde75100d9590085b4073
sushant-sinha <sinhasushant04@gmail.com>
2024-07-09 21:39:46 -0400

    new addition

--- Problems/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.java
+++ Problems/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 15ms ( 96.06% ) 56.79mb ( 83.99% )
+
 class Solution {
     public int minDifference(int[] nums) {
 
@@ -10,9 +14,25 @@ class Solution {
 
         int ans=Integer.MAX_VALUE;
 
+        /*
+
+            i will iterate through the number reductions we are doing from the starting of the array
+            i.e. number of values we have matched with the neighboring values (from start).
+            This value can be maximum of 3
+
+        */
+
+
         for(int i=0;i<=3;i++){
             
             int maxReduceCounter=3-i;
+
+            /*
+
+                j will iterate through the remaining number of reductions(maxReduceCounter) we can do from the back of the array
+                i.e. number of values we have matched with the neighboring values (from back).
+                This value depends on the number of reductions ALREADY done before, only spare moves can be used
+            */
 
             for(int j=0;j<=maxReduceCounter;j++){
 

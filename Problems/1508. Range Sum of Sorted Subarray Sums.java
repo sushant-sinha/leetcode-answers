9e1f0567c0931a6b72d34ddca8f5f471903f8183
Sushant Sinha <sinhasushant04@gmail.com>
2024-08-05 18:54:02 -0400

    new addition

---
+++ Problems/1508. Range Sum of Sorted Subarray Sums.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+
+        int[] newAr=new int[n*(n+1)/2+1];
+
+        int index=0;
+
+        for(int i=0;i<n;i++){
+
+            int tempSum=0;
+
+            for(int j=i;j<n;j++){
+
+                tempSum+=nums[j];
+                newAr[index++]=tempSum;
+
+            }
+        }
+        
+        Arrays.sort(newAr);
+
+        int ans=0;
+
+        for(;left<=right;left++){
+
+            ans=(ans+newAr[left])%1000000007;
+        }
+
+        return ans;
+    }
+}

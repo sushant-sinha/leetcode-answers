a96d1c51dd3d19109527412b949cf3d923a5d3bd
sushant-sinha <sinhasushant04@gmail.com>
2024-08-15 19:30:22 -0400

    new addition

---
+++ Problems/125. Valid Palindrome.java
@@ -0,0 +1,25 @@
+// solution from Nov 24, 2020😂
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null) return false;
+        if(s.equals("")) return true;
+        s = s.toLowerCase();
+        int f = -1;
+        int e = s.length();
+        boolean is = true;
+        while(f < e){
+            while(!Character.isLetter(s.charAt(++f)) && !Character.isDigit(s.charAt(f)) && f < s.length() - 1);
+            while(!Character.isLetter(s.charAt(--e)) && !Character.isDigit(s.charAt(e)) && e > 0);
+            
+            if(!Character.isLetter(s.charAt(e)) && !Character.isDigit(s.charAt(e))
+                && !Character.isLetter(s.charAt(f)) && !Character.isDigit(s.charAt(f))) break;
+            else if(s.charAt(f) != s.charAt(e)){
+                is = false;
+                break;
+            }  
+        }
+        return is;
+        
+    }
+}

ceecc4a2bdec17e9ac42264d0ae05af6db59df96
sushant-sinha <sinhasushant04@gmail.com>
2024-08-16 23:38:58 -0400

    new addition

--- Problems/125. Valid Palindrome.java
+++ Problems/125. Valid Palindrome.java
@@ -1,3 +1,42 @@
+// SUSHANT SINHA
+
+// 6ms ( 44.61% ) 48.31mb ( 5.50% )
+// part of TUF recursion
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuffer sb=new StringBuffer();
+
+        for(char c:s.toCharArray()){
+
+            if(Character.isLetter(c) || Character.isDigit(c)){
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        // System.out.println(sb);
+
+        return isRecurPalindrome(sb, 0, sb.length());
+        
+    }
+
+    boolean isRecurPalindrome(StringBuffer sb, int curIndex, int len){
+
+        if(curIndex==len/2)
+            return true;
+
+        if(sb.charAt(curIndex)!=sb.charAt(len-curIndex-1))
+            return false;
+
+        else{
+            boolean rightCheck=isRecurPalindrome(sb, curIndex+1, len);
+
+            return true && rightCheck;
+        }
+    }
+}
+
 // solution from Nov 24, 2020😂
 
 class Solution {

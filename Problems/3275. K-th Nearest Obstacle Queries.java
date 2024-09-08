88d25ed9b42b1691511cd3807f8bda4e5862882c
sushant-sinha <sinhasushant04@gmail.com>
2024-09-08 00:07:30 -0400

    new addition

--- Problems/3275. K-th Nearest Obstacle Queries.java
+++ Problems/3275. K-th Nearest Obstacle Queries.java
@@ -1,6 +1,6 @@
 // SUSHANT SINHA
 
-// 124ms ( 11.94% ) 145.50mb ( 41.99% )
+// 113ms ( 52.89% ) 146.76mb ( 40.57% )
 
 class Solution {
     public int[] resultsArray(int[][] queries, int k) {
@@ -9,41 +9,28 @@ class Solution {
 
         int[] ans=new int[queries.length];
 
-        int maxValueInWindow=-1;
-        int sizeOfQueue=0;
-
-        int index=0;
+        int maxValueInWindow=-1, sizeOfQueue=0, kthValue=-1, index=0;
 
         for(int i[]: queries){
 
             int tempDist=Math.abs(i[0])+Math.abs(i[1]);
 
-            if(sizeOfQueue<k){
+            if(++sizeOfQueue<=k){
                 pq.add(tempDist);
                 
-                sizeOfQueue++;
                 maxValueInWindow=Math.max(maxValueInWindow, tempDist);
-                // System.out.println("adding "+tempDist+ " size= "+sizeOfQueue);
+                kthValue=sizeOfQueue==k ? maxValueInWindow : -1;
             }
 
             else if(maxValueInWindow>=tempDist){
                 pq.add(tempDist);
                 pq.poll();
-                maxValueInWindow=Math.max(maxValueInWindow, tempDist);
-            }
-
-            if(sizeOfQueue==k){
-                ans[index++]=pq.peek();
                 
+                maxValueInWindow=pq.peek();
+                kthValue=maxValueInWindow;
             }
 
-            else
-                ans[index++]=-1;
-
-            // System.out.println("peek= "+pq.peek());
-            // System.out.println("pq.size= "+pq.size());
-
-            // System.out.println("-----------------------------------------------------");
+            ans[index++]=kthValue;
             
         }
 

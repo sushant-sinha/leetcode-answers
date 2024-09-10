4a47b67f8c5db1550438865666d8d5dc30c90674
sushant-sinha <sinhasushant04@gmail.com>
2024-09-09 21:29:54 -0400

    new addition

---
+++ Problems/3280. Convert Date to Binary.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+
+        StringBuffer sb=new StringBuffer();
+
+        String[] split=date.split("-");
+
+        int day=Integer.parseInt(split[2]), month=Integer.parseInt(split[1]), year=Integer.parseInt(split[0]);
+
+        sb.append(intToBinary(year));
+        sb.append("-");
+        sb.append(intToBinary(month));
+        sb.append("-");
+        sb.append(intToBinary(day));
+
+        return sb.toString();
+        
+    }
+
+    String intToBinary(int date){
+
+        StringBuffer sb=new StringBuffer();
+
+        while(date!=0){
+            sb.append(date%2);
+            date/=2;
+        }
+
+        // System.out.println(sb);
+
+        return sb.reverse().toString();
+    }
+}

1d33468730165f7a169569ccd02a27dca05a328f
sushant-sinha <sinhasushant04@gmail.com>
2024-09-25 00:17:19 -0400

    new addition

--- Problems/731. My Calendar II.java
+++ Problems/731. My Calendar II.java
@@ -2,6 +2,39 @@
 
 // 44ms ( 90.66% ) 45.33mb ( 86.35% )
 
+/*
+
+    Found a good way of comparing intervals and identifying iverlaps:
+
+    Math.max(start, event[0]) < Math.min(end, event[1])
+    
+    The maximum of the start of the two events must be smaller than the minimum of the end of two events.
+
+    This condition ensure all 3 cases (there are actually 6 cases, the other 3 are when the newer event is replace by the older event and vice-versa):
+    1) when the start of the new event is after the start of the older event(), and the end of the older event is after the start of the newer event. But, the end of the newer event is after the end of the older event.
+    example:
+
+    old: [10,20]
+    new: [12,24]
+
+    2) when the end of the new event is after the start of the older event, and the start of the newer event is before the start of the older event. But, the end of the older event is after the end of the newer event.
+
+    example:
+
+    old: [10,20]
+    new: [8,13]
+
+    3) when the new event is completely engulfed by the older event
+
+    example:
+
+    old: [10,20]
+    new: [12,15]
+
+*/
+
+
+
 class MyCalendarTwo {
 
     // all the new events are going to be added to this list.

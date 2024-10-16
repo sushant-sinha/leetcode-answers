947af93605486c00cf17d100bb9d88e807057d52
sushant-sinha <sinhasushant04@gmail.com>
2024-10-15 21:29:38 -0400

    new addition

--- Problems/1545. Find Kth Bit in Nth Binary String.java
+++ Problems/1545. Find Kth Bit in Nth Binary String.java
@@ -15,15 +15,11 @@ class Solution {
             bitFlip--;
         }
 
-        
-
         return (char)(x+'0');
 
     }
 
     int recur(int n, int k, int len){
-
-        // System.out.println("in for n="+n+ " "+k);
 
         if(n==1)
             return 0;
@@ -31,13 +27,19 @@ class Solution {
         if(k==len/2+1)
             return 1;
 
-        // first half
+        // if index falls in the first half
         else if(k<=len/2){
             return recur(n-1, k, len/2);
         }
 
+        // if it falls in the second half
         else{
+        	// handle inverse
             bitFlip++;
+
+            // since its reversed, the new index in the second half (from front) will be len/2 - a + 1 (a is the index from front)
+            // index from front will be k-len/2-1 (remove half string from front, and 1 that we appended)
+
             return recur(n-1, len/2-(k-len/2-1)+1, len/2);
         }
     }

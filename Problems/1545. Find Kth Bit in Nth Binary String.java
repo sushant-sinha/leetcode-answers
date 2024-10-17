994bcda0ad6aefd63e86779a17795399f198d8c3
sushant-sinha <sinhasushant04@gmail.com>
2024-10-17 00:10:55 -0400

    new addition

--- Problems/1545. Find Kth Bit in Nth Binary String.java
+++ Problems/1545. Find Kth Bit in Nth Binary String.java
@@ -1,3 +1,21 @@
+/*
+
+for char to int and int to char conversions.
+
+https://www.scaler.com/topics/int-to-char-in-java/
+
+    int num = 4;
+
+    //add '0' to convert int to char in Java
+    char c = (char)(num + '0');
+
+    System.out.println(c);
+
+Output => 4
+
+*/
+
+// 0ms ( 100% ) 41.21mb ( 56.45% )
 class Solution {
 
     int bitFlip=0;

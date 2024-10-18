c92f070436b60ccb7fed8ee1104d3bb0e9dd3282
sushant-sinha <sinhasushant04@gmail.com>
2024-10-18 18:49:15 -0400

    new addition

---
+++ Problems/3304. Find the K-th Character in String Game I.java
@@ -0,0 +1,34 @@
+class Solution {
+    public char kthCharacter(int k) {
+
+        char c=recur(new StringBuffer("a"), k, 1);
+
+        return c;
+        
+    }
+
+    char recur(StringBuffer s, int k, int len){
+
+        if(len>=k){
+            // System.out.println(s);
+            return s.charAt(k-1);
+        }
+
+        else{
+            for(int i=0;i<len;i++){
+                s.append(next(s.charAt(i)));
+            }
+
+            return recur(s, k, len*2);
+        }
+    }
+
+    char next(char c){
+
+        if(c=='z')
+            return 'a';
+
+        else
+            return (char)((int)(c+1));
+    }
+}

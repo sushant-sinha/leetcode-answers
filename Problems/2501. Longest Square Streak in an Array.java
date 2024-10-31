b57eef776d4a1b9c43adb244ed14446958251a6c
sushant-sinha <sinhasushant04@gmail.com>
2024-10-30 22:02:23 -0400

    new addition

--- Problems/2501. Longest Square Streak in an Array.java
+++ Problems/2501. Longest Square Streak in an Array.java
@@ -1,3 +1,7 @@
+// SUSHANT SINHA
+
+// 55ms ( 34.22% ) 62.05mb ( 23.85% )
+
 class Solution {
     public int longestSquareStreak(int[] nums) {
 
@@ -51,3 +55,62 @@ class Solution {
 
     }
 }
+
+
+
+/*
+
+// initially submitted this, which gave wrong answer for the last second test case.
+// this was because I was using int for storing the products instead of Long.
+// fixing this passed all test cases
+
+class Solution {
+    public int longestSquareStreak(int[] nums) {
+
+        // Arrays.sort(nums);
+
+        HashSet<Integer> set=new HashSet<>();
+
+        int ans=0;
+
+        for(int i: nums)
+            set.add(i);
+
+        for(int i: nums){
+
+            if(!hasSquareRoot(i, set)){
+
+                int cur=i, temp=0;
+
+                while(set.contains(cur)){
+                    temp++;
+                    cur*=cur;
+                }
+
+                ans=Math.max(ans, temp);
+
+            }
+        }
+
+        return ans==1 ? -1 : ans;
+        
+    }
+
+    boolean hasSquareRoot(int i, HashSet<Integer> nums){
+
+        int sq=(int)Math.sqrt(i);
+
+        if(sq*sq!=i)
+            return false;
+
+        else{
+            if(nums.contains(sq))
+                return true;
+
+            else return false;
+        }
+
+    }
+}
+
+*/

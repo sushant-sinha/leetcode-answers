2912cbfe3740a3cd54f791c5700c9258123d2a3f
sushant-sinha <sinhasushant04@gmail.com>
2024-12-06 22:03:24 -0500

    new addition

---
+++ Problems/3361. Shift Distance Between Two Strings.java
@@ -0,0 +1,70 @@
+class Solution {
+    public long shiftDistance(String s, String t, int[] nextCost, int[] previousCost) {
+
+        // create a map for converting every character to every other character
+
+        long ans=0;
+
+        long[][] cost=new long[26][26];
+
+        for(int i=0;i<26;i++){
+
+            for(int j=0;j<26;j++){
+
+                if(i==j){
+                    cost[i][j]=0;
+                    continue;
+                }
+
+                else{
+
+                    long costFront=calculateCostFront(i, j, nextCost);
+                    long costBack=calculateCostFront(j, i, previousCost);
+                    costBack+=previousCost[i]-previousCost[j];
+
+                    cost[i][j]=Math.min(costFront, costBack);
+                }
+            }
+        }
+
+        // for(long i[]: cost){
+        //     System.out.println(Arrays.toString(i));
+        // }
+
+        for(int i=0;i<s.length();i++){
+
+            // System.out.println("adding "+cost[s.charAt(i)-'a'][t.charAt(i)-'a']);
+            ans+=cost[s.charAt(i)-'a'][t.charAt(i)-'a'];
+        }
+
+        // System.out.println("final answer="+ans);
+
+        return ans;
+        
+    }
+
+    long calculateCostFront(int start, int target, int[] cost){
+
+        long sum=0;
+
+        while(start!=target){
+            sum+=cost[start];
+            start++;
+            start%=26;
+        }
+
+        return sum;
+    }
+
+    // long costBack(int start, int target, int[] cost){
+
+    //     long sum=0;
+
+    //     while(start!=target){
+    //         sum+=cost[start];
+    //         (++start)%26;
+    //     }
+
+    //     return sum;
+    // }
+}

d54323f8218f1fd069fc1941bd890e42174e1549
sushant-sinha <sinhasushant04@gmail.com>
2024-12-14 23:01:24 -0500

    new addition

--- Problems/2593. Find Score of an Array After Marking All Elements.java
+++ Problems/2593. Find Score of an Array After Marking All Elements.java
@@ -83,3 +83,27 @@ class Solution {
         
     }
 }
+
+// expected solution from the editorial
+// TC: O(N)
+
+class Solution {
+
+    public long findScore(int[] numbers) {
+        long ans = 0;
+        for (int i = 0; i < numbers.length; i += 2) {
+            int currentStart = i;
+            while (i + 1 < numbers.length && numbers[i + 1] < numbers[i]) {
+                i++;
+            }
+            for (
+                int currentIndex = i;
+                currentIndex >= currentStart;
+                currentIndex -= 2
+            ) {
+                ans += numbers[currentIndex];
+            }
+        }
+        return ans;
+    }
+}

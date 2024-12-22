713d55da094aaedeaef1767caa62f786eb7a2692
sushant-sinha <sinhasushant04@gmail.com>
2024-12-21 21:04:04 -0500

    new addition

--- Problems/3380. Maximum Area Rectangle With Point Constraints I.java
+++ Problems/3380. Maximum Area Rectangle With Point Constraints I.java
@@ -1,3 +1,10 @@
+// SUSHANT SINHA
+
+// bunch of edge cases which I missed on.
+// had 20% accuracy
+
+// 7ms ( 80.74% ) 45.36MB ( 24.37% )
+
 class Solution {
     public int maxRectangleArea(int[][] points) {
 
@@ -35,7 +42,7 @@ class Solution {
 
             // bottom left
             int x=i[0], y=i[1];
-// System.out.println("---------\np0 "+x+" "+y);
+            // System.out.println("---------\np0 "+x+" "+y);
             // check if we have points above this, parallel to y-axis, top left
             TreeSet<Integer> lockedX=sortedX.get(x);
 
@@ -47,19 +54,19 @@ class Solution {
             else
                 continue;
 
-// System.out.println("p1 "+x1+" "+y1);
+            // System.out.println("p1 "+x1+" "+y1);
             // bottom right
 
             int y2=y, x2=-1;
 
             TreeSet<Integer> lockedY=sortedY.get(y2);
-// System.out.println("lockedY for y= "+y+" : "+lockedY+" ceiling= "+lockedY.ceiling(x+1));
+            // System.out.println("lockedY for y= "+y+" : "+lockedY+" ceiling= "+lockedY.ceiling(x+1));
             if(lockedY.ceiling(x+1)!=null)
                 x2=lockedY.ceiling(x+1);
             
             else
                 continue;
-// System.out.println("p2 x2="+x2+" "+y2);
+            // System.out.println("p2 x2="+x2+" "+y2);
 
             int topRightX=x2, topRightY=y1;
 
@@ -67,9 +74,9 @@ class Solution {
 
             if(sortedX.containsKey(topRightX) && sortedX.get(topRightX).contains(topRightY)){
                 // check if there is a point within this rectangle
-// System.out.println("p3 "+x2+" "+y1);
+            // System.out.println("p3 "+x2+" "+y1);
 
-boolean flag=false;
+            boolean flag=false;
                 // find an X coordinate which is >x but <x2
                 for(Map.Entry<Integer, TreeSet<Integer>> e: sortedX.entrySet()){
 
@@ -108,5 +115,47 @@ boolean flag=false;
 
         return area;
         
+    }
+}
+
+// fastest solution
+// feel my solution looks good, atleast fomat and execution wise
+
+class Solution {
+    public int maxRectangleArea(int[][] points) {
+        return sln1(points);
+    }
+
+    private int sln1(int[][] ps){
+        int n = ps.length;
+        int res = -1;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(j==i) continue;
+                if(ps[i][1] != ps[j][1]) continue;
+                for(int k = 0;k<n;k++){
+                    if(k==i || k==j) continue;
+                    if(ps[i][0] != ps[k][0]) continue;
+                    for(int l = k+1; l<n;l++){
+                        if(l==i || l==j || l==k) continue;
+                        if(ps[l][0] != ps[j][0] || ps[l][1] != ps[k][1]) continue;
+                        int hl = Math.min(ps[i][0], ps[j][0]);
+                        int hr = Math.max(ps[i][0], ps[j][0]);
+                        int vt = Math.min(ps[i][1], ps[k][1]);
+                        int vb = Math.max(ps[i][1], ps[k][1]);
+                        boolean valid = true;
+                        for(int a = 0;a<n;a++){
+                            if(a==i || a==j || a==k || a==l) continue;
+                            if(hl <= ps[a][0] && ps[a][0] <= hr && vt <= ps[a][1] && ps[a][1]<=vb){
+                                valid = false;
+                                break;
+                            }
+                        }
+                        if(valid)  res = Math.max(res, (hr-hl)*(vb-vt));
+                    }
+                }
+            }
+        }
+        return res;
     }
 }

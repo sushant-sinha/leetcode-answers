888752843ed0a5827fa2c8a80db8b8d9e99f4637
sushant-sinha <sinhasushant04@gmail.com>
2025-01-03 02:23:48 -0500

    new addition

---
+++ Problems/2559. Count Vowel Strings in Ranges.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+
+        int[] prefix=new int[words.length+1];
+
+        prefix[0]=0;
+
+        for(int i=0;i<words.length;i++){
+
+            prefix[i+1]=prefix[i];
+
+            if(isStartingAndEndingWithVowel(words[i]))
+                prefix[i+1]+=1;
+
+        }
+
+        // System.out.println(Arrays.toString(prefix));
+
+        int[] ans=new int[queries.length];
+
+        for(int i=0;i<queries.length;i++){
+
+            ans[i]=prefix[queries[i][1]+1]-prefix[queries[i][0]];
+        }
+
+        return ans;
+        
+    }
+
+    boolean isStartingAndEndingWithVowel(String s){
+
+        return isVowel(s.charAt(0)) && isVowel(s.charAt(s.length()-1));
+    }
+
+    boolean isVowel(char c){
+
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+}

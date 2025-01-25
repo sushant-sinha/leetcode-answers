487fd20c7f1436b2209c3b36362928203806176a
sushant-sinha <sinhasushant04@gmail.com>
2025-01-24 22:20:54 -0500

    new addition

--- Problems/1047. Remove All Adjacent Duplicates In String.java
+++ Problems/1047. Remove All Adjacent Duplicates In String.java
@@ -64,24 +64,38 @@ class Solution {
     }
 }
 
-// gievn solution
-// 3ms ( 99.72% ) 39.4mb ( 74.65% )
+/*
+given solution
+3ms ( 99.72% ) 39.4mb ( 74.65% )
+
+two pointer, end holds the end of the new string while i scans the string
+replace the end with the current i
+if at any point, end is same as i, we will move back the end (because we are removing the last character)
+*/
 
 class Solution {
-    public String removeDuplicates(String S) {
-        char[] in = S.toCharArray();
-        int j = 0;
-        for (int i = 0; i < in.length; i++) {
-            in[j] = in[i];
-            
-            
-            if (j != 0 && in[j] == in[j - 1]) {
-                j = j - 1;
-            } else {
-                j++;
-            }
+    public String removeDuplicates(String s) {
+
+        char[] ar=s.toCharArray();
+
+        int end=1;
+
+        for(int i=1;i<s.length();i++){
+
+            // replace the end with the current character (i)
+
+            ar[end]=ar[i];
+
+            // check if the two match (charat end and charat end-1)
+            if(end!=0 && ar[end]==ar[end-1])
+                end--;
+            else
+                end++;
+
+            // System.out.println(ar);
         }
+
+        return new String(ar, 0, end);
         
-        return new String(in, 0, j);
     }
 }
